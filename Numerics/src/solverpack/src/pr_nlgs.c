113ea05768436c0c15a0005daa1d86a2de3e51ea
nineb <nineb@bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5>
2005-09-22 15:31:30 +0000

    Addition of an argument for chattering, and some comments.
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@251 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Numerics/src/solverpack/src/pr_nlgs.c
+++ Numerics/src/solverpack/src/pr_nlgs.c
@@ -1,96 +1,136 @@
+
 /*!\file pr_nlgs.c
 
-  This subroutine allows the primal resolution of contact problems with friction.
+
+This subroutine allows the primal resolution of relay problems.
 
    Try \f$(z,w)\f$ such that:
 \f$
 \left\lbrace
 \begin{array}{l}
-M z- w=q\\
--w \in \partial\psi_{[-a, a]}(z)\\
+M z +q = w\\
+-w \in \partial\psi_{[-b, a]}(z)\\
 \end{array}
 \right.
 \f$
 
  here M is an n by n  matrix, q an n-dimensional vector, z an n-dimensional  vector and w an n-dimensional vector.
 
-\fn  pr_nlgs(double vec[],double *qq,int *nn,double a[],int * itermax, double * tol,double z[],double w[],int *it_end,double * res,int *info)
+*/
 
-   pr_nlgs is a specific nlgs (Gauss Seidel Non Linear)solver for relay problems.
 
-   \param vec On enter a double vector containing the components of the double matrix with a fortran90 allocation.
-   \param qq On enter a pointer over doubles containing the components of the double vector.
-   \param nn On enter a pointer over integers, the dimension of the second member.
-   \param a On enter a pointer over doubles, the bound.
-   \param itermax On enter a pointer over integers, the maximum iterations required.
-   \param tol On enter a pointer over doubles, the tolerance required.
-   \param it_end On enter a pointer over integers, the number of iterations carried out.
-   \param res On return a pointer over doubles, the error value.
-   \param z On return double vector, the solution of the problem.
-   \param w On return double vector, the solution of the problem.
-   \param info On return a pointer over integers, the termination reason (0 is successful otherwise 1).
+/*!\fn int  pr_nlgs(double vec[], double *q, int *nn, double a[], double b[], int * itermax, double * tol, int *chat, double z[], double w[], int *it_end, double * res, int *info)
+
+   pr_nlgs is a specific nlgs(non linear Gauss-Seidel) solver for primal relay problems.
+
+
+
+   \param vec       On enter a double vector containing the components of the matrix with a fortran storage.
+   \param qq        On enter a pointer over doubles containing the components of the vector.
+   \param nn        On enter a pointer over integers, the dimension of the second member.
+   \param a         On enter a pointer over doubles, the upper bound.
+   \param b         On enter a pointer over doubles, the down bound.
+   \param itermax   On enter a pointer over integers, the maximum iterations required.
+   \param tol       On enter a pointer over doubles, the tolerance required.
+   \param chat      On enter a pointer over integer, the output log identifiant
+                    0 > =  no output
+                    0 < =  active screen output
+
+
+   \param it_end    On return a pointer over integers, the number of iterations carried out.
+   \param res       On return a pointer over doubles, the error value.
+   \param z         On return double vector, the solution of the problem.
+   \param w         On return double vector, the solution of the problem.
+   \param info      On return a pointer over integers, the termination reason
+                    0 = convergence,
+        1 = no convergence,
+        2 = Nul diagonal term
+
 
    \author Nineb Sheherazade.
  */
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include "blaslapack.h"
 
-pr_nlgs(double vec[], double *q, int *nn, double a[], double b[], int * itermax, double * tol, double z[], double w[], int *it_end, double * res, int *info)
+
+void pr_nlgs(double vec[], double *q, int *nn, double a[], double b[], int * itermax, double * tol, int *chat, double z[], double w[], int *it_end, double * res, int *info)
 {
 
-  FILE *f101;
-  int i, j, iter1, k;
+
+
+  int i, j, iter1, k, ispeak = *chat;
   int n = *nn, incx = 1, incy = 1, itt = *itermax;
+
+
   double errmax = *tol, alpha, beta, mina;
-  double err1, num, den, avn, avt, apn, apt, xn;
+  double err1, num, den, avn, apn, xn;
   double *zt, *wnum1;
-  char trans = 'T';
-  /*  double M[n][n];*/
-  double(*M)[n];
+
+  char trans = 'N';
 
 
-  f101 = fopen("resultat_nlgs.dat", "w+");
 
-  M =  malloc(n * n * sizeof(double));
-
-  for (i = 0; i < n; i++)
-    for (j = 0; j < n; j++)
-      M[i][j] = vec[i * n + j];
 
   wnum1 = (double*) malloc(n * sizeof(double));
-  zt = (double*) malloc(n * sizeof(double));
+  zt    = (double*) malloc(n * sizeof(double));
+
+
 
   for (i = 0; i < n; i++)
   {
-    w[i] = 0.;
-    z[i] = 0.;
-    zt[i] = 0.;
+    w[i]     = 0.0;
+    z[i]     = 0.0;
+    zt[i]    = 0.0;
+    wnum1[i] = 0.0;
   }
 
+
   iter1 = 0;
-  err1 = 1.;
+  err1  = 1.;
+
+
+
+
 
   while ((iter1 < itt) && (err1 > errmax))
   {
     iter1 = iter1 + 1;
-    /*  dcopy_( &n, q, &incx, wnum1, &incy);
-    alpha = 1.;
-    beta = -1.;
-    dgemv_( &trans, &n, &n, &alpha, M, &n, z, &incx, &beta, wnum1, &incy);*/
 
     for (i = 0; i < n; i++)
     {
       avn = 0.;
       apn = 0.;
+
       for (j = 0; j <= i - 1; j++)
-        avn = avn + M[i][j] * z[j];
-      for (k = i + 1; k < n; k++)
-        apn = apn + M[i][k] * z[k];
-      xn = q[i] - avn - apn;
-      zt[i] = 1 / M[i][i] * xn;
+        avn = avn + vec[j * n + i] * z[j];
+
+      for (k = i + 1 ; k < n ; k++)
+        apn = apn + vec[k * n + i] * z[k];
+
+      xn = -q[i] - avn - apn;
+
+
+      if (fabs(vec[i * n + i]) < 1e-16)
+      {
+        if (ispeak > 0)
+          printf("\n Warning nul diagonal term of M \n");
+
+
+        free(zt);
+        free(wnum1);
+
+        *info = 2;
+
+        return;
+
+      }
+      else
+        zt[i] = 1 / vec[i * n + i] * xn;
 
       if (a[i] < zt[i])
       {
@@ -109,29 +149,30 @@ pr_nlgs(double vec[], double *q, int *nn, double a[], double b[], int * itermax,
       {
         z[i] = -b[i];
       }
-      w[i] = M[i][i] * z[i] - xn;
+
+      w[i] = vec[i * n + i] * z[i] - xn;
+
     }
 
-    /* ///////// convergence criterium ///////////// */
+    /*          Convergence criterium      */
 
     dcopy_(&n, w, &incx, wnum1, &incy);
-    alpha = 1.;
+
+    alpha = -1.;
     daxpy_(&n, &alpha, q, &incx, wnum1, &incy);
+
     alpha = 1.;
     beta = -1.;
-    dgemv_(&trans, &n, &n, &alpha, M, &n, z, &incx, &beta, wnum1, &incy);
+    dgemv_(&trans, &n, &n, &alpha, vec, &n, z, &incx, &beta, wnum1, &incy);
+
     num = ddot_(&n, wnum1, &incx, wnum1, &incy);
+
     den = ddot_(&n, q, &incx, q, &incy);
 
     err1 = sqrt(num) / sqrt(den);
+
     *it_end = iter1;
     *res = err1;
-
-    for (i = 0; i < n; i++)
-    {
-      /*result_gs[i][iter1-1] = z[i]; */
-      fprintf(f101, "%d  %d  %14.7e \n", iter1 - 1, i, z[i]);
-    }
 
 
 
@@ -140,18 +181,24 @@ pr_nlgs(double vec[], double *q, int *nn, double a[], double b[], int * itermax,
 
   if (err1 > errmax)
   {
-    printf("no convergence after %d iterations, the residue is %g\n", iter1, err1);
+    if (ispeak > 0)
+      printf("No convergence after %d iterations, the residue is %g\n", iter1, err1);
+
     *info = 1;
   }
   else
   {
-    printf("there is convergence after %d iterations, the residue is %g \n", iter1, err1);
+    if (ispeak > 0)
+      printf("Convergence after %d iterations, the residue is %g \n", iter1, err1);
+
     *info = 0;
   }
 
+
+
   free(wnum1);
   free(zt);
-  free(M);
-  fclose(f101);
+
+
 
 }
