c265247713efb5010eef7c71ca4a8c35390a9c3b
nineb <nineb@bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5>
2005-09-23 11:56:52 +0000

    Files cleaned, tests ok.
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@265 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Numerics/src/solverpack/test/main_dr.c
+++ Numerics/src/solverpack/test/main_dr.c
@@ -1,66 +1,125 @@
-/*!
- *  This main file allows the primal resolution of contact problems with friction:
- *  try (z,w) such that:
- *
- *
- *
- *                    M z  - w = q                 (1)
- *                    0<= zn , 0<= wn , zn.wn= O   (2) (Signorini contact law)
- *                    -wt in diff(PSI)   (zt)      (3) (Coulomb friction law)
- *                                 [-mu*zn, mu*zn]
- *
- *
- *  here M is an n by n  matrix, q an n-dimensional vector, z an n-dimensional*  vector and w an n-dimensional vector.
- *
- *  This system of equations and inequalities is solved thanks to dr subroutines:
- *
- *         dr_nlgs( M,q,n,mu,itermax,tol,z,w,it_end,res,info)
- *        dr_latin( M,q,n,k_latin,mu,itermax,tol,z,w,it_end,res,info)
- *
- *  For more information about the methods see the chapter 4 of the Siconos manual theory.
- *
- *  The subroutine's call is due to the function dr_solver:
- *
- */
+///////////////////////////////////////////////////////////////////////////
+// This main file allows the dual resolution of relay problems:
+//  try (z,w) such that:
+//
+//
+//
+//                    M z  +q = w                      (1)
+//                    -z in diff(PSI) (w)              (2)
+//                               [-b,a]
+//
+//
+//  here M is an n by n  matrix, q an n-dimensional vector, z,w,b,a an n-dimensional vectors.
+//
+//  This system of equations and inequalities is solved thanks to rp_subroutine:
+//        dr_gsnl  ( M, q, n, a, b, itermax, tol, chat, z, w, it_end, res, info)
+//        dr_latin ( M, q, n, k_latin,  a, b, itermax, tol, chat, z, w, it_end, res, info)
+//
+//
+//  where _ itermax      is the maximum iterations required, it's an integer
+//        _ res          is the residue, it's a float (positive float)
+//        _ it_end       is the number of iterations carried out, it's an integer
+//        _ tol          is the tolerance value desired, it's a positive float (if you make it non positive then you force the convergence)
+//        _ chat         is the output log identifiant
+//        _ k_latin      is the parameter research of the latin, it's a float (strictly positive)
+//        _ a            is the upper bound, it's a vector of floats
+//        _ b            is the down bound, it's a vector of floats
+//        _ z and w      are the solutions of the problem
+//        _ info         shows the termination reason,0 is successful (otherwise see the termination reason of the solver used), it's an integer.
+//
+//
+//    For more information about the methods see the chapter 4 of the Siconos manual theory.
+//
+//
+//
+//  The subroutine's call is due to the function solve_rd:
+//
+//  int solve_dr ( double *M, double *q, int n, methode *pt, double *z, double *w )
+//
+//  where M       is an n by n matrix,
+//        q       an n-dimensional vector,
+//        n       is the row dimension of M,
+//        pt      a pointer other a union ( methode see "solverpack.h").
+//        z and w are n-dimensional  vectors solution.
+//
+//        methode is a variable with a union type; in this union you find the structure (method_rd) that gives to the function
+//  solve_dr, the name and the parameters (itermax, tol, k_latin, a, b, chat) of the method we want to use.
+//  This function return an interger:  0 successful .
+//
+//
+///////////////////////////////////////////////////////////////////////////////
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "solverpack.h"
 
-int main(void)
+#define CHAT
+
+
+void test_mmc(void)
 {
+  FILE *f1, *f2, *f3, *f4;
+  int i, j, nl, nc, nll, n = 40;
+  int info1 = -1, info2 = -1;
+  int nonsymmetric;
 
-  FILE *f1, *f2, *f5, *f6;
-  int i, j, nl, nc, nll, info, n = 40, dimM = n;
-  double *q, *z, *w, *vec, *a, *b, *zt;
-  double **M;
+  double *q, *z1, *w1, *z2, *w2, *vec, *a, *b;
   double qi, Mij;
-  char val[14], vall[50];
 
-  method meth_dr;
 
-  M = (double **)malloc(dimM * sizeof(double*));
-  for (i = 0; i < n; i++) M[i] = (double*)malloc(dimM * sizeof(double));
+  char val[50], vall[50];
+  method meth_dr1, meth_dr2;
 
-  strcpy(meth_dr.dr.name , "Latin");
 
-  meth_dr.dr.itermax = 1000;
-  meth_dr.dr.tol = 0.0001;
-  meth_dr.dr.k_latin = 0.007; //0.9;//0.00005;
+  printf("\n* *** ******************** *** * \n");
+  printf("* ***        TEST MMC      *** * \n");
+  printf("* *** ******************** *** * \n");
 
 
 
-  if ((f1 = fopen("DATA/M_relay_dr.dat", "r")) == NULL)
+  /*           Allocations                     */
+
+  q   = (double*)malloc(n * sizeof(double));
+  z1  = (double*)malloc(n * sizeof(double));
+  w1  = (double*)malloc(n * sizeof(double));
+  z2  = (double*)malloc(n * sizeof(double));
+  w2  = (double*)malloc(n * sizeof(double));
+  a   = (double*) malloc(n * sizeof(double));
+  b   = (double*)malloc(n * sizeof(double));
+  vec = (double*) malloc(n * n * sizeof(double));
+
+
+  /*     Data loading of M , q , a and b           */
+
+  if ((f1 = fopen("DATA/M_relay_rd2.dat", "r")) == NULL)
   {
     perror("fopen 1");
     exit(1);
   }
 
 
+  if ((f2 = fopen("DATA/q_relay_rd2.dat", "r")) == NULL)
+  {
+    perror("fopen 2");
+    exit(2);
+  }
 
 
-  vec = (double*)malloc(dimM * dimM * sizeof(double));
+  if ((f3 = fopen("DATA/a_relay_rd2.dat", "r")) == NULL)
+  {
+    perror("fopen 5");
+    exit(5);
+  }
+
+
+  if ((f4 = fopen("DATA/b_relay_rd2.dat", "r")) == NULL)
+  {
+    perror("fopen 6");
+    exit(6);
+  }
+
 
 
   while (!feof(f1))
@@ -70,61 +129,10 @@ int main(void)
     fscanf(f1, "%s", val);
     Mij = atof(val);
 
-    /////////////       on met la transpos       ////////////////
-    //printf("nl=%i", nl);
-    //printf("nc=%i", nc);
 
-
-    M[nl - 1][nc - 1] = Mij;
-    //*(*(M+nc-1)+nl-1)=Mij;
-
-    //////////////         fin transpos         ////////////////////
+    vec[(nc - 1)*n + nl - 1 ] = Mij;
 
   }
-
-  //// valeurs du tableau dans vec (compatibilite allocation memoire f90)///
-  for (i = 0; i < dimM; i++)
-  {
-    for (j = 0; j < dimM; j++)
-    {
-      vec[j * dimM + i] = M[i][j];
-
-    }
-  }
-  ////////////////////////////////////////////////////////////////////////
-
-
-
-
-  if ((f2 = fopen("DATA/q_relay_dr.dat", "r")) == NULL)
-  {
-    perror("fopen 2");
-    exit(2);
-  }
-
-
-  if ((f5 = fopen("DATA/a_relay_dr.dat", "r")) == NULL)
-  {
-    perror("fopen 5");
-    exit(5);
-  }
-
-
-  if ((f6 = fopen("DATA/b_relay_dr.dat", "r")) == NULL)
-  {
-    perror("fopen 6");
-    exit(6);
-  }
-
-
-
-  q = malloc(dimM * sizeof(double));
-  z = malloc(dimM * sizeof(double));
-  zt = malloc(dimM * sizeof(double));
-  w = malloc(dimM * sizeof(double));
-  a = malloc(dimM * sizeof(double));
-  b = malloc(dimM * sizeof(double));
-
 
 
 
@@ -134,66 +142,152 @@ int main(void)
     fscanf(f2, "%d", &nll);
     fscanf(f2, "%s", vall);
     qi = atof(vall);
-    printf("nll=%i\n", nll);
-    q[nll - 1] = qi;
+    q[nll - 1] = -qi;
+
   }
 
 
-  while (!feof(f5))
+  while (!feof(f3))
   {
-    fscanf(f5, "%d", &nll);
-    fscanf(f5, "%s", vall);
+    fscanf(f3, "%d", &nll);
+    fscanf(f3, "%s", vall);
     qi = atof(vall);
-    fprintf(f5, "%d %.14e\n", nll, qi);
     a[nll - 1] = qi;
   }
 
 
-  while (!feof(f6))
+  while (!feof(f4))
   {
-    fscanf(f6, "%d", &nll);
-    fscanf(f6, "%s", vall);
+    fscanf(f4, "%d", &nll);
+    fscanf(f4, "%s", vall);
     qi = atof(vall);
-    fprintf(f6, "%d %.14e\n", nll, qi);
     b[nll - 1] = qi;
   }
 
 
+  fclose(f2);
+  fclose(f4);
+  fclose(f1);
+  fclose(f3);
 
-  meth_dr.dr.a = (double*)malloc(dimM * sizeof(double));
-  meth_dr.dr.b = (double*)malloc(dimM * sizeof(double));
+
+
+  /*           End od the data loading        */
+
+
+  nonsymmetric = 0;
+
+
+  /*           Is M symmetric ?                */
+
+  for (i = 0 ; i < n ; ++i)
+  {
+    for (j = 0 ; j < i ; ++j)
+    {
+      if (abs(vec[i * n + j] - vec[j * n + i]) > 1e-16)
+      {
+        nonsymmetric = 1;
+        break;
+      }
+    }
+  }
+
+
+
+#ifdef CHAT
+  if (nonsymmetric) printf("\n !! WARNING !!\n M is a non symmetric matrix \n \n");
+  else printf(" \n M is a symmetric matrix \n \n");
+#endif
+
+
+
+
+
+  /*           Initialization                 */
+
+  strcpy(meth_dr1.dr.name, "NLGS");
+  meth_dr1.dr.itermax  =  1000;
+  meth_dr1.dr.tol      =  0.000001;
+  meth_dr1.dr.chat  =  0;
+
+
+  strcpy(meth_dr2.dr.name, "Latin");
+  meth_dr2.dr.itermax  =  5000;
+  meth_dr2.dr.tol      =  0.0000000000001;
+  meth_dr2.dr.k_latin  =  0.09;
+  meth_dr2.dr.chat  =  0;
+
+
+
+
+  meth_dr1.dr.a = (double*)malloc(n * sizeof(double));
+  meth_dr1.dr.b = (double*)malloc(n * sizeof(double));
+  meth_dr2.dr.a = (double*)malloc(n * sizeof(double));
+  meth_dr2.dr.b = (double*)malloc(n * sizeof(double));
 
   for (i = 0; i <= n - 1; i++)
   {
-    meth_dr.dr.a[i] = a[i];
-    meth_dr.dr.b[i] = -b[i];
+
+    meth_dr1.dr.a[i] =  a[i];
+    meth_dr1.dr.b[i] = -b[i];
+    meth_dr2.dr.a[i] =  a[i];
+    meth_dr2.dr.b[i] = -b[i];
+
+
   }
 
-  printf("\n\n we go in the function \n\n");
-
-  info = dr_solver(vec, q, &n, &meth_dr, zt, w);
 
 
 
-  printf("\n\n we go out the function and info is %d\n", info);
+#ifdef CHAT
+  printf("**** NLGS TEST ****\n \n");
+#endif
 
-  fclose(f2);
-  fclose(f1);
-  fclose(f5);
-  fclose(f6);
+  info1 = dr_solver(vec, q, &n, &meth_dr1, z1, w1);
 
-  for (i = 0; i < dimM; i++) free(M[i]);
-  free(M);
+#ifdef CHAT
+  printf("\n**** LATIN TEST ***\n \n");
+#endif
+
+  info2 = dr_solver(vec, q, &n, &meth_dr2, z2, w2);
+
+
+#ifdef CHAT
+  printf(" *** ************************************** ***\n");
+  for (i = 0 ; i < n ; ++i)
+    printf(" NLGS RESULT : %14.7e %14.7e   LATIN RESULT : % 14.7e  %14.7e \n" , z1[i] , w1 [i], z2[i], w2[i]);
+
+  printf("\n -- SOLVEUR ------ ITER ----- ERR\n");
+  printf(" \n NLGS RESULT : %d   % 14.7e \n" , meth_dr1.dr.iter , meth_dr1.dr.err);
+  printf("   LATIN RESULT : %d   % 14.7e \n" , meth_dr2.dr.iter , meth_dr2.dr.err);
+
+
+
+
+
+  printf(" *** ************************************** ***\n");
+#endif
 
 
   free(vec);
   free(q);
-  free(z);
-  free(zt);
-  free(w);
+  free(z1);
+  free(w1);
+  free(z2);
+  free(w2);
   free(a);
   free(b);
-  free(meth_dr.dr.a);
-  free(meth_dr.dr.b);
-  return 0;
+  free(meth_dr1.dr.a);
+  free(meth_dr1.dr.b);
+  free(meth_dr2.dr.a);
+  free(meth_dr2.dr.b);
 }
+
+
+int main(void)
+{
+  test_mmc();
+  return 1;
+}
+
+
--- Numerics/src/solverpack/test/main_pr.c
+++ Numerics/src/solverpack/test/main_pr.c
@@ -1,86 +1,124 @@
-/*!
- *  This main file allows the primal resolution of contact problems with friction:
- *  try (z,w) such that:
- *
- *
- *
- *                    M z  - w = q                 (1)
- *                    0<= zn , 0<= wn , zn.wn= O   (2) (Signorini contact law)
- *                    -wt in diff(PSI)   (zt)      (3) (Coulomb friction law)
- *                                 [-mu*zn, mu*zn]
- *
- *
- *  here M is an n by n  matrix, q an n-dimensional vector, z an n-dimensional*  vector and w an n-dimensional vector.
- *
- *  This system of equations and inequalities is solved thanks to cfp_subroutine:
- *        cfp_gsnl (M,q,n,mu,itermax,tol,z,w,it_end,res,info)
- *        cfp_gcp (M,q,n,mu,itermax,tol,z,w,it_end,res,info)
- *        cfp_latin (M,q,n,k_latin,mu,itermax,tol,z,w,it_end,res,info)
- *
- *  where _ itermax is the maximum iterations required, it's an integer
- *        _ mu is the friction coefficient, it's a float
- *        _ res is the residue, it's a float
- *        _ it_end is the number of iterations carried out, it's an integer
- *        _ tol is the tolerance value desired, it's a float
- *        _ k_latin is the parameter research of the latin, it's a float
- *        _ z and w are the solutions of the problem
- *        _ info shows the termination reason,0 is successful otherwise 1, it's an integer.
- *
- *
- *    For more information about the methods see the chapter 4 of the Siconos manual theory.
- *
- *
- *
- *  The subroutine's call is due to the function solve_cfp:
- *
- *  int solve_cfp (double (*M)[maxcols],double *q,int n,method *pt, double *z,double *w)
- *
- *  where M is an n by n matrix, q an n-dimensional vector, n is the row dimension
- *  of M, and pt a pointer other a structure ( method). z and w are n-dimensional
- *  vectors solution.
- *  method is a variable with a structure type; this structure gives to the function
- * solve_pcf, the name and the parameters (itermax, tol, k_latin,...) of the method we want to use.
- *  This function return an interger:  0 successful return otherwise 1.
- *
- *
- */
+// This main file allows the primal resolution of relay problems:
+//  try (z,w) such that:
+//
+//
+//
+//                    M z  + q = w                     (1)
+//                    -w in diff(PSI) (z)              (2)
+//                               [-b,a]
+//
+//
+//  here M is an n by n  matrix, q an n-dimensional vector, z,w,b,a an n-dimensional vectors.
+//
+//  This system of equations and inequalities is solved thanks to rp_subroutine:
+//        pr_gsnl  ( M, q, n, a, b, itermax, tol, chat, z, w, it_end, res, info)
+//        pr_latin ( M, q, n, k_latin, a, b, itermax, tol, chat, z, w, it_end, res, info)
+//
+//
+//  where _ itermax    is the maximum iterations required, it's an integer
+//        _ res        is the residue, it's a float (positive float)
+//        _ it_end     is the number of iterations carried out, it's an integer
+//        _ tol        is the tolerance value desired, it's a positive float (if you make it non positive then you force the convergence)
+//        _ k_latin    is the parameter research of the latin, it's a float (strictly positive)
+//        _ a          is the upper bound, it's a vector of floats
+//        _ b          is the down bound, it's a vector of floats
+//        _ chat       is the output log identifiant
+//        _ z and w    are the solutions of the problem
+//        _ info       shows the termination reason,0 is successful (otherwise see the termination reason of the solver used), it's an integer.
+//
+//
+//    For more information about the methods see the chapter 4 of the Siconos manual theory.
+//
+//
+//
+//  The subroutine's call is due to the function solve_rp:
+//
+//  int pr_solver ( double *M, double *q, int n, methode *pt, double *z, double *w )
+//
+//  where M        is an n by n matrix,
+//        q        an n-dimensional vector,
+//        n        is the row dimension of M,
+//        pt       a pointer other a union ( methode see "solverpack.h").
+//        z and w are n-dimensional vectors solution.
+//
+//  methode is a variable with a union type; in this union you find the structure (method_rp) that gives to the function
+// rp_solver, the name and the parameters ( itermax, tol, k_latin, a, b, chat) of the method we want to use.
+//  This function return an interger:  0 for successful
+//
+//
+///////////////////////////////////////////////////////////////////////////////
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "solverpack.h"
 
+#define CHAT
 
-void test1(void)
+
+void test_mmc(void)
 {
+  FILE *f1, *f2, *f3, *f4;
+  int i, j, nl, nc, nll, n = 15;
+  int info1 = -1, info2 = -1;
+  int nonsymmetric;
 
-  FILE *f1, *f2, *f5, *f6;
-  int i, j, nl, nc, nll, n = 40, dimM = n;
-  double *q, *z, *w, *vec, *a, *b, *zt;
-  int info;
-  double **M;
+  double *q, *z1, *w1, *z2, *w2, *vec, *a, *b;
   double qi, Mij;
+
+
   char val[50], vall[50];
-
-  method meth_pr;
-
-  strcpy(meth_pr.pr.name , "NLGS");
-  meth_pr.pr.itermax = 100000;
-  meth_pr.pr.tol = 0.0000001;
-  meth_pr.pr.k_latin = 0.003; //0.00005;
-
-  M = (double **)malloc(dimM * sizeof(double*));
-  for (i = 0; i < n; i++)
-    M[i] = (double*)malloc(dimM * sizeof(double));
+  method meth_pr1, meth_pr2;
 
 
-  if ((f1 = fopen("DATA/M_relay.dat", "r")) == NULL)
+  printf("\n* *** ******************** *** * \n");
+  printf("* ***        TEST MMC      *** * \n");
+  printf("* *** ******************** *** * \n");
+
+
+
+  /*           Allocations                     */
+
+  q   = (double*)malloc(n * sizeof(double));
+  z1  = (double*)malloc(n * sizeof(double));
+  w1  = (double*)malloc(n * sizeof(double));
+  z2  = (double*)malloc(n * sizeof(double));
+  w2  = (double*)malloc(n * sizeof(double));
+  a   = (double*) malloc(n * sizeof(double));
+  b   = (double*)malloc(n * sizeof(double));
+  vec = (double*) malloc(n * n * sizeof(double));
+
+
+  /*     Data loading of M , q , a and b           */
+
+  if ((f1 = fopen("DATA/M_relay1.dat", "r")) == NULL)
   {
     perror("fopen 1");
     exit(1);
   }
 
-  vec = (double*)malloc(dimM * dimM * sizeof(double));
+
+  if ((f2 = fopen("DATA/q_relay1.dat", "r")) == NULL)
+  {
+    perror("fopen 2");
+    exit(2);
+  }
+
+
+  if ((f3 = fopen("DATA/a_relay1.dat", "r")) == NULL)
+  {
+    perror("fopen 5");
+    exit(5);
+  }
+
+
+  if ((f4 = fopen("DATA/b_relay1.dat", "r")) == NULL)
+  {
+    perror("fopen 6");
+    exit(6);
+  }
+
 
 
   while (!feof(f1))
@@ -90,57 +128,10 @@ void test1(void)
     fscanf(f1, "%s", val);
     Mij = atof(val);
 
-    /////////////       on met la transpos       ////////////////
 
-    M[nl - 1][nc - 1] = Mij;
-
-    //////////////         fin transpos         ////////////////////
+    vec[(nc - 1)*n + nl - 1 ] = Mij;
 
   }
-
-  //// valeurs du tableau dans vec (compatibilite allocation memoire f90)///
-  for (i = 0; i < dimM; i++)
-  {
-    for (j = 0; j < dimM; j++)
-    {
-      vec[j * dimM + i] = M[i][j];
-
-    }
-  }
-  ////////////////////////////////////////////////////////////////////////
-
-
-
-
-  if ((f2 = fopen("DATA/q_relay.dat", "r")) == NULL)
-  {
-    perror("fopen 2");
-    exit(2);
-  }
-
-
-  if ((f5 = fopen("DATA/a_relay.dat", "r")) == NULL)
-  {
-    perror("fopen 5");
-    exit(5);
-  }
-
-
-  if ((f6 = fopen("DATA/b_relay.dat", "r")) == NULL)
-  {
-    perror("fopen 6");
-    exit(6);
-  }
-
-
-
-  q = malloc(dimM * sizeof(double));
-  z = malloc(dimM * sizeof(double));
-  zt = malloc(dimM * sizeof(double));
-  w = malloc(dimM * sizeof(double));
-  a = malloc(dimM * sizeof(double));
-  b = malloc(dimM * sizeof(double));
-
 
 
 
@@ -150,236 +141,151 @@ void test1(void)
     fscanf(f2, "%d", &nll);
     fscanf(f2, "%s", vall);
     qi = atof(vall);
-    q[nll - 1] = qi;
+    q[nll - 1] = -qi;
+
   }
 
 
-  while (!feof(f5))
+  while (!feof(f3))
   {
-    fscanf(f5, "%d", &nll);
-    fscanf(f5, "%s", vall);
+    fscanf(f3, "%d", &nll);
+    fscanf(f3, "%s", vall);
     qi = atof(vall);
-    fprintf(f5, "%d %.14e\n", nll, qi);
     a[nll - 1] = qi;
   }
 
 
-  while (!feof(f6))
+  while (!feof(f4))
   {
-    fscanf(f6, "%d", &nll);
-    fscanf(f6, "%s", vall);
+    fscanf(f4, "%d", &nll);
+    fscanf(f4, "%s", vall);
     qi = atof(vall);
-    fprintf(f6, "%d %.14e\n", nll, qi);
     b[nll - 1] = qi;
   }
 
 
-
-
-  meth_pr.pr.a = (double*)malloc(dimM * sizeof(double));
-  meth_pr.pr.b = (double*)malloc(dimM * sizeof(double));
-
-  for (i = 0; i <= n - 1; i++)
-  {
-    meth_pr.pr.a[i] = a[i];
-    meth_pr.pr.b[i] = -b[i];
-  }
-
-
-  printf("\n\n we go in the function \n\n");
-
-  info = pr_solver(vec, q, &n, &meth_pr, zt, w);
-
-
-  printf("\n\n we go out the function and info is %d\n", info);
-
   fclose(f2);
-  fclose(f6);
+  fclose(f4);
   fclose(f1);
-  fclose(f5);
-  for (i = 0; i < dimM; i++) free(M[i]);
-  free(M);
-
-
-  free(vec);
-  free(q);
-  free(z);
-  free(zt);
-  free(w);
-  free(a);
-  free(b);
-  free(meth_pr.pr.a);
-  free(meth_pr.pr.b);
-}
-
-void test2(void)
-{
-  FILE *f1, *f2, *f5, *f6;
-  int i, j, nl, nc, nll, n = 40, dimM = n;
-  double *q, *z, *w, *vec, *a, *b, *qqt, *zt;
-  int info;
-  double **M;
-  double qi, Mij;
-  char val[50], vall[50];
-  method meth_pr;
-
-  strcpy(meth_pr.pr.name, "Latin");
-  meth_pr.pr.itermax = 1000;
-  meth_pr.pr.tol = 0.0001;
-  meth_pr.pr.k_latin = 0.003; //0.00005;
+  fclose(f3);
 
 
 
-  if ((f1 = fopen("DATA/M_relay.dat", "r")) == NULL)
+  /*           End od the data loading        */
+
+
+  nonsymmetric = 0;
+
+
+  /*           Is M symmetric ?                */
+
+  for (i = 0 ; i < n ; ++i)
   {
-    perror("fopen 1");
-    exit(1);
-  }
-
-
-
-  M = (double **)malloc(dimM * sizeof(double*));
-  for (i = 0; i < n; i++)
-    M[i] = (double*)malloc(dimM * sizeof(double));
-
-
-  vec = (double*)malloc(dimM * dimM * sizeof(double));
-
-
-  while (!feof(f1))
-  {
-    fscanf(f1, "%d", &nl);
-    fscanf(f1, "%d", &nc);
-    fscanf(f1, "%s", val);
-    Mij = atof(val);
-
-    /////////////       on met la transpos       ////////////////
-
-    M[nl - 1][nc - 1] = Mij;
-
-
-    //////////////         fin transpos         ////////////////////
-
-  }
-
-  //// valeurs du tableau dans vec (compatibilite allocation memoire f90)///
-  for (i = 0; i < dimM; i++)
-  {
-    for (j = 0; j < dimM; j++)
+    for (j = 0 ; j < i ; ++j)
     {
-      vec[j * dimM + i] = M[i][j];
-
+      if (abs(vec[i * n + j] - vec[j * n + i]) > 1e-16)
+      {
+        nonsymmetric = 1;
+        break;
+      }
     }
   }
-  ////////////////////////////////////////////////////////////////////////
 
 
 
-
-  if ((f2 = fopen("DATA/q_relay.dat", "r")) == NULL)
-  {
-    perror("fopen 2");
-    exit(2);
-  }
-
-
-  if ((f5 = fopen("DATA/a_relay.dat", "r")) == NULL)
-  {
-    perror("fopen 5");
-    exit(5);
-  }
-
-
-  if ((f6 = fopen("DATA/b_relay.dat", "r")) == NULL)
-  {
-    perror("fopen 6");
-    exit(6);
-  }
-
-
-
-  q = malloc(dimM * sizeof(double));
-  z = malloc(dimM * sizeof(double));
-  zt = malloc(dimM * sizeof(double));
-  w = malloc(dimM * sizeof(double));
-  a = malloc(dimM * sizeof(double));
-  b = malloc(dimM * sizeof(double));
+#ifdef CHAT
+  if (nonsymmetric) printf("\n !! WARNING !!\n M is a non symmetric matrix \n \n");
+  else printf(" \n M is a symmetric matrix \n \n");
+#endif
 
 
 
 
 
-  while (!feof(f2))
-  {
-    fscanf(f2, "%d", &nll);
-    fscanf(f2, "%s", vall);
-    qi = atof(vall);
-    q[nll - 1] = qi;
-  }
+  /*           Initialization                 */
+
+  strcpy(meth_pr1.pr.name, "NLGS");
+  meth_pr1.pr.itermax  =  30000;
+  meth_pr1.pr.tol      =  0.0000001;
+  meth_pr1.pr.chat  =  0;
 
 
-  while (!feof(f5))
-  {
-    fscanf(f5, "%d", &nll);
-    fscanf(f5, "%s", vall);
-    qi = atof(vall);
-    fprintf(f5, "%d %.14e\n", nll, qi);
-    a[nll - 1] = qi;
-  }
-
-
-  while (!feof(f6))
-  {
-    fscanf(f6, "%d", &nll);
-    fscanf(f6, "%s", vall);
-    qi = atof(vall);
-    fprintf(f6, "%d %.14e\n", nll, qi);
-    b[nll - 1] = qi;
-  }
+  strcpy(meth_pr2.pr.name, "Latin");
+  meth_pr2.pr.itermax  =  1500;
+  meth_pr2.pr.tol      =  0.0001;
+  meth_pr2.pr.k_latin  =  0.003;
+  meth_pr2.pr.chat  =  0;
 
 
 
-  meth_pr.pr.a = (double*)malloc(dimM * sizeof(double));
-  meth_pr.pr.b = (double*)malloc(dimM * sizeof(double));
+
+  meth_pr1.pr.a = (double*)malloc(n * sizeof(double));
+  meth_pr1.pr.b = (double*)malloc(n * sizeof(double));
+  meth_pr2.pr.a = (double*)malloc(n * sizeof(double));
+  meth_pr2.pr.b = (double*)malloc(n * sizeof(double));
 
   for (i = 0; i <= n - 1; i++)
   {
-    meth_pr.pr.a[i] = a[i];
-    meth_pr.pr.b[i] = -b[i];
+
+    meth_pr1.pr.a[i] =  a[i];
+    meth_pr1.pr.b[i] = -b[i];
+    meth_pr2.pr.a[i] =  a[i];
+    meth_pr2.pr.b[i] = -b[i];
+
+
   }
 
 
-  printf("\n\n we go in the function \n\n");
-
-  info = pr_solver(vec, qqt, &n, &meth_pr, zt, w);
 
 
-  printf("\n\n we go out the function and info is %d\n", info);
+#ifdef CHAT
+  printf("**** NLGS TEST ****\n \n");
+#endif
 
-  fclose(f2);
-  fclose(f6);
-  fclose(f1);
-  fclose(f5);
-  for (i = 0; i < dimM; i++) free(M[i]);
-  free(M);
+  info1 = pr_solver(vec, q, &n, &meth_pr1, z1, w1);
+
+#ifdef CHAT
+  printf("\n**** LATIN TEST ***\n \n");
+#endif
+
+  info2 = pr_solver(vec, q, &n, &meth_pr2, z2, w2);
+
+
+#ifdef CHAT
+  printf(" *** ************************************** ***\n");
+  for (i = 0 ; i < n ; ++i)
+    printf(" NLGS RESULT : %14.7g   LATIN RESULT : % 14.7g \n" , z1[i] , z2[i]);
+
+  printf(" -- SOLVEUR ------ ITER ----- ERR\n");
+  printf(" \n NLGS RESULT : %d   % 14.7g \n" , meth_pr1.pr.iter , meth_pr1.pr.err);
+  printf(" LATIN RESULT : %d   % 14.7g \n" , meth_pr2.pr.iter , meth_pr2.pr.err);
+
+
+
+
+
+  printf(" *** ************************************** ***\n");
+#endif
 
 
   free(vec);
   free(q);
-  free(z);
-  free(zt);
-  free(w);
+  free(z1);
+  free(w1);
+  free(z2);
+  free(w2);
   free(a);
   free(b);
-  free(meth_pr.pr.a);
-  free(meth_pr.pr.b);
+  free(meth_pr1.pr.a);
+  free(meth_pr1.pr.b);
+  free(meth_pr2.pr.a);
+  free(meth_pr2.pr.b);
 }
 
 
 int main(void)
 {
-  test1();
-  test2();
+  test_mmc();
   return 1;
 }
 
