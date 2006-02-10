b119acab5991acd55e9a867f5fd5886663c1db02
Franck Perignon <franck.perignon@imag.fr>
2006-02-10 17:12:36 +0000

    Fix m4 install bug in configure.ac (494-96) and remove env.sh file
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@403 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Front-End/scilab/env.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/local_home/pissard/Workspace/siconos/distrib//lib:/local_home/pissard/Workspace/siconos/distrib//lib:/local_home/pissard/Workspace/siconos/distrib//share/SICONOS:.
-
-export SICONOSPATH=/local_home/pissard/Workspace/siconos/distrib/
-
