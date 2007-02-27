10ee13d050503481366e4e21e2df6f71bc3a2d1e
Franck Perignon <franck.perignon@imag.fr>
2007-02-27 16:25:56 +0000

    add LagrangianScleronomousR
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@672 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

---
+++ Kernel/src/modelingTools/LagrangianScleronomousR.cpp
@@ -0,0 +1,257 @@
+/* Siconos-Kernel version 2.0.1, Copyright INRIA 2005-2006.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY vincent.acary@inrialpes.fr
+ */
+
+// \todo : create a work vector for all tmp vectors used in computeG, computeH ...
+
+#include "LagrangianScleronomousR.h"
+#include "LagrangianRXML.h"
+#include "Interaction.h"
+#include "LagrangianDS.h"
+
+using namespace std;
+
+// Default constructor
+LagrangianScleronomousR::LagrangianScleronomousR():
+  LagrangianR("ScleronomousR"), hPtr(NULL), G0Ptr(NULL)
+{
+  isPlugged["G0"] = false ;
+  isAllocatedIn["G0"] = false;
+  G.resize(1, NULL);
+}
+
+// xml constructor
+LagrangianScleronomousR::LagrangianScleronomousR(RelationXML* relxml): LagrangianR(relxml, "ScleronomousR"), hPtr(NULL), G0Ptr(NULL)
+{
+  LagrangianRXML * LRxml = static_cast<LagrangianRXML *>(relationxml);
+  // h plug-in
+  if (LRxml->hasH())
+  {
+    pluginNames["h"] = LRxml->getHPlugin();
+    setComputeHFunction(cShared.getPluginName(pluginNames["h"]), cShared.getPluginFunctionName(pluginNames["h"]));
+  }
+  if (!LRxml->hasG())
+    RuntimeException::selfThrow("LagrangianScleronomousR:: xml constructor failed, can not find a definition for G0.");
+  G.resize(1, NULL);
+  // Read G matrix or plug-in names.
+  readGInXML(LRxml, 0);
+}
+
+// constructor from a set of data
+LagrangianScleronomousR::LagrangianScleronomousR(const string& computeH, const std::string& computeG):
+  LagrangianR("ScleronomousR"), hPtr(NULL), G0Ptr(NULL)
+{
+  setComputeHFunction(cShared.getPluginName(computeH), cShared.getPluginFunctionName(computeH));
+  // Note that in this case, G is not allocated since we do not have its dimensions.
+  // That will be done during initialize, with Interaction input.
+  G.resize(1, NULL);
+  string name = "G0";
+  pluginNames[name] = computeG;
+  setComputeGFunction(cShared.getPluginName(pluginNames[name]), cShared.getPluginFunctionName(pluginNames[name]), 0);
+  isAllocatedIn[name] = false;
+}
+
+LagrangianScleronomousR::~LagrangianScleronomousR()
+{
+  hPtr = NULL;
+  G0Ptr = NULL;
+}
+
+void LagrangianScleronomousR::setComputeHFunction(const string& pluginPath, const string& functionName)
+{
+  cShared.setFunction(&hPtr, pluginPath, functionName);
+  string plugin = pluginPath.substr(0, pluginPath.length() - 3);
+  pluginNames["h"] = plugin + ":" + functionName;
+  isPlugged["h"] = true;
+}
+
+void LagrangianScleronomousR::setComputeGFunction(const string& pluginPath, const string& functionName, unsigned int)
+{
+  cShared.setFunction(&G0Ptr, pluginPath, functionName);
+  string plugin = pluginPath.substr(0, pluginPath.length() - 3);
+  string name = "G0";
+  pluginNames["G0"] = plugin + ":" + functionName;
+  isPlugged["G0"] = true;
+}
+
+void LagrangianScleronomousR::computeH(double)
+{
+  // arg= time. Unused in this function but required for interface.
+  if (isPlugged["h"])
+  {
+    // get vector y of the current interaction
+    SiconosVector *y = interaction->getYPtr(0);
+
+    // Warning: temporary method to have contiguous values in memory, copy of block to simple.
+    SimpleVector * qCopy = new SimpleVector(*data["q0"]);
+    SimpleVector * zCopy = new SimpleVector(*data["z"]);
+    SimpleVector * yCopy = new SimpleVector(*y);
+
+    unsigned int sizeQ = qCopy->size();
+    unsigned int sizeY = y->size();
+    unsigned int sizeZ = zCopy->size();
+
+    if (hPtr == NULL)
+      RuntimeException::selfThrow("LagrangianScleronomousR:computeH() failed, h is not linked to a plugin function");
+    hPtr(sizeQ, &(*qCopy)(0) , sizeY, &(*yCopy)(0), sizeZ, &(*zCopy)(0));
+
+    // Copy data that might have been changed in the plug-in call.
+    *data["z"] = *zCopy;
+    *y = *yCopy;
+    delete qCopy;
+    delete yCopy;
+    delete zCopy;
+  }
+  // else nothing
+}
+
+void LagrangianScleronomousR::computeG(double, unsigned int)
+{
+  // First arg: time. Useless.
+  // Last arg: index for G - Useless, always equal to 0 for this kind of relation.
+
+  if (isPlugged["G0"])
+  {
+    // Warning: temporary method to have contiguous values in memory, copy of block to simple.
+    SimpleVector * qCopy = new SimpleVector(*data["q0"]);
+    SimpleVector * zCopy = new SimpleVector(*data["z"]);
+
+    unsigned int sizeY = G[0]->size(0);
+    unsigned int sizeQ = qCopy->size();
+    unsigned int sizeZ = zCopy->size();
+
+    if (G0Ptr == NULL)
+      RuntimeException::selfThrow("computeG() is not linked to a plugin function");
+    G0Ptr(sizeQ, &(*qCopy)(0), sizeY, &(*(G[0]))(0, 0), sizeZ, &(*zCopy)(0));
+
+    // Copy data that might have been changed in the plug-in call.
+    *data["z"] = *zCopy;
+    delete qCopy;
+    delete zCopy;
+  }
+  //  else nothing!
+}
+
+void LagrangianScleronomousR::computeHFree(double)
+{
+  if (isPlugged["h"])
+  {
+    // arg= time. Unused in this function but required for interface.
+
+    // get vector y of the current interaction
+    SiconosVector *y = interaction->getYPtr(0);
+
+    // Warning: temporary method to have contiguous values in memory, copy of block to simple.
+    SimpleVector * qCopy = new SimpleVector(*data["q0Free"]);
+    SimpleVector * zCopy = new SimpleVector(*data["z"]);
+    SimpleVector * yCopy = new SimpleVector(*y);
+
+    unsigned int sizeQ = qCopy->size();
+    unsigned int sizeY = y->size();
+    unsigned int sizeZ = zCopy->size();
+
+    if (hPtr == NULL)
+      RuntimeException::selfThrow("LagrangianScleronomousR:computeH() is not linked to a plugin function");
+    hPtr(sizeQ, &(*qCopy)(0) , sizeY, &(*yCopy)(0), sizeZ, &(*zCopy)(0));
+
+    // Copy data that might have been changed in the plug-in call.
+    *data["z"] = *zCopy;
+    *y = *yCopy;
+    delete qCopy;
+    delete yCopy;
+    delete zCopy;
+  }
+  //else nothing
+}
+
+void LagrangianScleronomousR::computeGFree(double, unsigned int)
+{
+  // First arg: time. Useless.
+  // Last arg: index for G - Useless, always equal to 0 for this kind of relation.
+
+  if (isPlugged["G0"])
+  {
+    unsigned int sizeY = interaction->getInteractionSize();
+    unsigned int sizeQ = interaction->getSizeOfDS();
+
+    // Warning: temporary method to have contiguous values in memory, copy of block to simple.
+    SimpleVector * qCopy = new SimpleVector(*data["q0Free"]);
+    SimpleVector * zCopy = new SimpleVector(*data["z"]);
+    unsigned int sizeZ = zCopy->size();
+
+    if (G0Ptr == NULL)
+      RuntimeException::selfThrow("computeG() is not linked to a plugin function");
+    G0Ptr(sizeQ, &(*qCopy)(0), sizeY, &(*(G[0]))(0, 0), sizeZ, &(*zCopy)(0));
+
+    // Copy data that might have been changed in the plug-in call.
+    *data["z"] = *zCopy;
+    delete qCopy;
+    delete zCopy;
+  }
+  //else nothing
+}
+
+void LagrangianScleronomousR::computeOutput(double time, unsigned int derivativeNumber)
+{
+  if (derivativeNumber == 0)
+    computeH(time);
+  else
+  {
+    computeG(time);
+    SiconosVector *y = interaction->getYPtr(derivativeNumber) ;
+    if (derivativeNumber == 1)
+      *y = prod(*G[0], *data["q1"]);
+    else if (derivativeNumber == 2)
+      *y = prod(*G[0], *data["q2"]);
+    else
+      RuntimeException::selfThrow("LagrangianScleronomousR::computeOutput(t,index), index out of range");
+  }
+}
+
+void LagrangianScleronomousR::computeFreeOutput(double time, unsigned int derivativeNumber)
+{
+  if (derivativeNumber == 0)
+    computeHFree(time);
+
+  else
+  {
+    computeGFree(time);
+    SiconosVector *y = interaction->getYPtr(derivativeNumber) ;
+    if (derivativeNumber == 1)
+      *y = prod(*G[0], *data["q1Free"]);
+    else if (derivativeNumber == 2)
+      *y = prod(*G[0], *data["q2"]);
+    else
+      RuntimeException::selfThrow("LagrangianScleronomousR::computeOutput(t,index), index out of range");
+  }
+}
+
+void LagrangianScleronomousR::computeInput(double time, unsigned int level)
+{
+  computeG(time, 0);
+  string name = "p" + toString<unsigned int>(level);
+  // get lambda of the concerned interaction
+  SiconosVector *lambda = interaction->getLambdaPtr(level);
+  *data[name] += prod(trans(*(G[0])), *lambda);
+}
+
+LagrangianScleronomousR* LagrangianScleronomousR::convert(Relation *r)
+{
+  LagrangianScleronomousR* lnlr = dynamic_cast<LagrangianScleronomousR*>(r);
+  return lnlr;
+}
---
+++ Kernel/src/modelingTools/LagrangianScleronomousR.h
@@ -0,0 +1,159 @@
+/* Siconos-Kernel version 2.0.1, Copyright INRIA 2005-2006.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY vincent.acary@inrialpes.fr
+ */
+/*! \file LagrangianScleronomousR.h
+
+*/
+#ifndef LagrangianScleronomousR_H
+#define LagrangianScleronomousR_H
+
+#include "LagrangianR.h"
+
+/** Lagrangian Non Linear Scleronomous and Holonomous Relation.
+ *
+ * \author SICONOS Development Team - copyright INRIA
+ *  \version 2.0.1.
+ *  \date Apr 27, 2004
+ *
+ * Relation with:
+ *
+ * \f[
+ * Y[0] = y = h(q,z)
+ *
+ * Y[1] = \dot y = G0(q,z)\dot q
+ *
+ * R = G0^t(q,z)\lambda
+ *
+ * \f]
+ *
+ * G0 and h are connected to plug-in functions.
+ *
+ */
+class LagrangianScleronomousR : public LagrangianR
+{
+
+protected:
+
+  // --- Plug-in for scleronomic-holonomic constraints case -> h(q), G(q) ---
+  /** LagrangianScleronomousR plug-in to compute h(q) (scleronomic case)
+   * @param sizeQ: size of q = sum of the sizes of all the DynamicalSystems involved in the interaction
+   * @param q : pointer to the first element of q
+   * @param sizeY : size of vector y (ie of the interaction)
+   * @param[in,out] y : pointer to the first element of y
+   * @param sizeZ : size of vector z
+   * @param[in,out] z: pointer to z vector(s) from DS.
+   */
+  FPtr3 hPtr;
+
+  /** LagrangianScleronomousR plug-in to compute G0(q), gradient of h according to q (scleronomic case)
+   * @param sizeQ: size of q = sum of the sizes of all the DynamicalSystems involved in the interaction
+   * @param q : pointer to the first element of q
+   * @param sizeY : size of vector y (ie of the intercation)
+   * @param[in,out] G0 : pointer to the first element of G0 (sizeY X sizeDS matrix)
+   * @param sizeZ : size of vector z
+   * @param[in,out] z: pointer to z vector(s) from DS.
+   */
+  FPtr3 G0Ptr;
+
+public:
+
+  /** default constructor
+   */
+  LagrangianScleronomousR();
+
+  /** constructor from xml file
+   *  \param relationXML
+   *  \exception RuntimeException
+   */
+  LagrangianScleronomousR(RelationXML*);
+
+  /** constructor from a set of data
+   *  \param string : the type of relation (scleronomic ...)
+   *  \param string : the name of the plugin for computeH
+   *  \param vector<string> : a list of names for the plugin for computeG (depends on relation type)
+   *  \exception RuntimeException
+   */
+  LagrangianScleronomousR(const std::string&, const std::string&);
+
+  /** destructor
+   */
+  ~LagrangianScleronomousR();
+
+  /** to set a specified function to compute function h(q,...)
+   *  \param string : the complete path to the plugin
+   *  \param string : the name of the function to use in this plugin
+   *  \exception SiconosSharedLibraryException
+   */
+  void setComputeHFunction(const std::string& , const std::string&);
+
+  /** to set a specified function to compute G(q, ...)
+   *  \param string : the complete path to the plugin
+   *  \param string : the name of the function to use in this plugin
+   *  \param unsigned int: the index of G that must be computed (see introduction of this class for details on indexes)
+   *  \exception SiconosSharedLibraryException
+   */
+  void setComputeGFunction(const std::string& , const std::string&  , unsigned int  = 0);
+
+  /** to compute y = h(q,v,t) using plug-in mechanism
+   * \param: double, current time
+   */
+  void computeH(double);
+
+  /** to compute G using plug-in mechanism. Index shows which G is to be computed
+   * \param: double, current time
+   * \param: unsigned int
+   */
+  void computeG(double, unsigned int = 0);
+
+  /** to compute y = h(q,v,t) using plug-in mechanism, with free DS states.
+   * \param: double, current time
+   */
+  void computeHFree(double);
+
+  /** to compute G using plug-in mechanism, with free DS states. Index shows which G is to be computed
+   * \param: double, current time
+   * \param: unsigned int
+   */
+  void computeGFree(double, unsigned int = 0);
+
+  /** to compute output
+   *  \param double : current time
+   *  \param unsigned int: number of the derivative to compute, optional, default = 0.
+   */
+  void computeOutput(double, unsigned int = 0);
+
+  /** to compute y for the free state
+   *  \param double : current time
+   *  \param unsigned int: number of the derivative to compute, optional, default = 0.
+   */
+  void computeFreeOutput(double, unsigned int = 0);
+
+  /** to compute p
+   *  \param double : current time
+   *  \param unsigned int: "derivative" order of lambda used to compute input
+   */
+  void computeInput(double, unsigned int);
+
+  /** encapsulates an operation of dynamic casting. Needed by Python interface.
+   *  \param Relation* : the relation which must be converted
+   * \return a pointer on the relation if it is of the right type, NULL otherwise
+   */
+  static LagrangianScleronomousR* convert(Relation *r);
+};
+
+#endif // LAGRANGIANRELATION_H
