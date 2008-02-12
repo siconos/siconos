88abccd8f0df38b303f7ae06ff1c35ad603354b8
Maurice Bremond <maurice.bremond@inria.fr>
2008-02-12 16:36:26 +0000

    Kernel cmake config (many compilation failures...)
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@993 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

---
+++ Kernel/config.h.cmake
@@ -0,0 +1,11 @@
+
+#define WITH_CMAKE
+
+#cmakedefine SVN_REVISION ${SVN_REVISION}
+
+#cmakedefine HAVE_BLAS
+#cmakedefine HAVE_LAPACK
+#cmakedefine HAVE_ATLAS
+#cmakedefine HAVE_CLAPACK_H
+#cmakedefine HAVE_CBLAS_H
+#cmakedefine HAVE_XERBLA
