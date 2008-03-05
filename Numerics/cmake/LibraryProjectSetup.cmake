80f975f59bc4331dd2c3d9ec22508d5043d115c1
Maurice Bremond <maurice.bremond@inria.fr>
2008-03-05 14:35:56 +0000

    run the tests with a chdir inside the test dir
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@1026 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Numerics/cmake/LibraryProjectSetup.cmake
+++ Numerics/cmake/LibraryProjectSetup.cmake
@@ -8,155 +8,152 @@
 # <PROJECT_NAME>_VERSION
 # <PROJECT_NAME>_SOURCE_FILE_EXTENSIONS may contains the wanted sources extensions (default: all extensions)
 
-macro(LIBRARY_PROJECT_SETUP)
+MACRO(LIBRARY_PROJECT_SETUP)
 
-message(STATUS "")
-message(STATUS "Setting up ${PROJECT_NAME} library build")
+  MESSAGE(STATUS "")
+  MESSAGE(STATUS "Setting up ${PROJECT_NAME} library build")
 
-set(_ALL_DIRS ${${PROJECT_NAME}_DIRS})
+  SET(_ALL_DIRS ${${PROJECT_NAME}_DIRS})
+  IF(NOT ${PROJECT_NAME}_SOURCE_FILE_EXTENSIONS)
+    # all extensions we know (should be done only with the project languages)
+    SET(_ALL_EXTS)
+    FOREACH(_EXT ${CMAKE_CXX_SOURCE_FILE_EXTENSIONS} ${CMAKE_C_SOURCE_FILE_EXTENSIONS} ${CMAKE_Fortran_SOURCE_FILE_EXTENSIONS} ${CMAKE_Java_SOURCE_FILE_EXTENSIONS} ${CMAKE_RC_SOURCE_FILE_EXTENSIONS})
+      LIST(APPEND _ALL_EXTS *.${_EXT})
+    ENDFOREACH(_EXT ${CMAKE_CXX_SOURCE_FILE_EXTENSIONS} ${CMAKE_C_SOURCE_FILE_EXTENSIONS} ${CMAKE_Fortran_SOURCE_FILE_EXTENSIONS} ${CMAKE_Java_SOURCE_FILE_EXTENSIONS} ${CMAKE_RC_SOURCE_FILE_EXTENSIONS})
+  ELSE(NOT ${PROJECT_NAME}_SOURCE_FILE_EXTENSIONS)
+    # specified extensions
+    FOREACH(_EXT ${${PROJECT_NAME}_SOURCE_FILE_EXTENSIONS})
+      LIST(APPEND _ALL_EXTS *.${_EXT})
+    ENDFOREACH(_EXT ${${PROJECT_NAME}_SOURCE_FILE_EXTENSIONS})
+  ENDIF(NOT ${PROJECT_NAME}_SOURCE_FILE_EXTENSIONS)
+  
+  SET(_ALL_FILES)
+  FOREACH(_DIR ${_ALL_DIRS})
+    SET(_DIR_FILES)
+    FOREACH(_EXT ${_ALL_EXTS})
+      FILE(GLOB _DIR_FILES_EXT ${_DIR}/${_EXT})
+      IF(_DIR_FILES_EXT)
+        LIST(APPEND _DIR_FILES ${_DIR_FILES_EXT})
+      ENDIF(_DIR_FILES_EXT)
+    ENDFOREACH(_EXT ${_ALL_EXTS})
+    IF(_DIR_FILES)
+      FOREACH(_F ${_DIR_FILES})
+        IF(NOT ${_F}_T)
+          SET(${_F}_T T)
+          LIST(APPEND _ALL_FILES ${_F})
+        ENDIF(NOT ${_F}_T)
+      ENDFOREACH(_F ${_DIR_FILES})
+    ENDIF(_DIR_FILES)
+  ENDFOREACH(_DIR ${_ALL_DIRS})
+  
+  #
+  # headers
+  #
+  IF(NOT ${PROJECT_NAME}_HDRS)
+    FOREACH(_DIR ${_ALL_DIRS})
+      FILE(GLOB _HDRS  ${_DIR}/*.h)
+      IF(_HDRS)
+        LIST(APPEND ${PROJECT_NAME}_HDRS ${_HDRS})
+      ENDIF(_HDRS)
+    ENDFOREACH(_DIR ${_ALL_DIRS})
+  ENDIF(NOT ${PROJECT_NAME}_HDRS)
+  
+  #
+  # Unstable sources
+  #
+  IF(NOT UNSTABLE)
+    IF(${PROJECT_NAME}_Unstable_SRCS)
+      MESSAGE(STATUS "Some unstables sources files are going to be excluded")
+      MESSAGE(STATUS "To configure an unstable build, run : `cmake -DUNSTABLE=ON .'")
+      FOREACH(_FILE ${${PROJECT_NAME}_Unstable_SRCS})
+        FILE(GLOB _GFILE ${_FILE})
+        IF(_GFILE)
+          MESSAGE(STATUS "excluded : ${_GFILE}")
+          LIST(REMOVE_ITEM _ALL_FILES ${_GFILE})
+        ELSE(_GFILE)
+          MESSAGE(STATUS "WARNING : Unstable file NOT FOUND : ${_FILE}")
+        ENDIF(_GFILE)
+      ENDFOREACH(_FILE ${${PROJECT_NAME}_Unstable_SRCS})
+    ENDIF(${PROJECT_NAME}_Unstable_SRCS)
+  ENDIF(NOT UNSTABLE)
+  
+  #
+  # Attempt to specify file properties
+  #
+  
+  #if(${PROJECT_NAME}_SOURCES_PROPERTIES)
+  #  foreach(_PROPERTY ${${PROJECT_NAME}_SOURCES_PROPERTIES})
+  #    foreach(_FILE ${_ALL_FILES})
+  #      if(${${PROJECT_NAME}_${_PROPERTY}_REGEX})
+  #        if(${_FILE} MATCHES ${${PROJECT_NAME}_${_PROPERTY}}_REGEX})
+  #          if(${${PROJECT_NAME}_${_PROPERTY})
+  #            set_source_files_properties(${_FILE} ${${PROJECT_NAME}_${_PROPERTY}})
+  #          endif(${${PROJECT_NAME}_${_PROPERTY})
+  #        endif(${_FILE} MATCHES ${${PROJECT_NAME}_${_PROPERTY}}_REGEX})
+  #      endif(${${PROJECT_NAME}_${_PROPERTY}_REGEX})
+  #    endforeach(_FILE ${_ALL_FILES})
+  #  endforeach(_PROPERTY ${${PROJECT_NAME}_SOURCES_PROPERTIES})
+  #endif(${PROJECT_NAME}_SOURCES_PROPERTIES)
+  
+  SET(${PROJECT_NAME}_SRCS ${_ALL_FILES})
+  
+  INCLUDE_DIRECTORIES(${_ALL_DIRS})
 
-#
+  ADD_LIBRARY(${PROJECT_NAME}_static  STATIC ${_ALL_FILES})
+  ADD_LIBRARY(${PROJECT_NAME}_shared  SHARED ${_ALL_FILES})
 
-#
+  SET_TARGET_PROPERTIES(${PROJECT_NAME}_static PROPERTIES 
+    OUTPUT_NAME "${PROJECT_NAME}" 
+    VERSION "${${PROJECT_NAME}_VERSION}" 
+    CLEAN_DIRECT_OUTPUT 1 # no clobbering
+    LINKER_LANGUAGE C)
+  
+  SET_TARGET_PROPERTIES(${PROJECT_NAME}_shared PROPERTIES
+    OUTPUT_NAME "${PROJECT_NAME}" 
+    VERSION "${${PROJECT_NAME}_VERSION}" 
+    CLEAN_DIRECT_OUTPUT 1
+    LINKER_LANGUAGE C)
 
-if(NOT ${PROJECT_NAME}_SOURCE_FILE_EXTENSIONS)
-  # all extensions we know (should be done only with the project languages)
-  set(_ALL_EXTS)
-  foreach(_EXT ${CMAKE_CXX_SOURCE_FILE_EXTENSIONS} ${CMAKE_C_SOURCE_FILE_EXTENSIONS} ${CMAKE_Fortran_SOURCE_FILE_EXTENSIONS} ${CMAKE_Java_SOURCE_FILE_EXTENSIONS} ${CMAKE_RC_SOURCE_FILE_EXTENSIONS})
-    list(APPEND _ALL_EXTS *.${_EXT})
-  endforeach(_EXT ${CMAKE_CXX_SOURCE_FILE_EXTENSIONS} ${CMAKE_C_SOURCE_FILE_EXTENSIONS} ${CMAKE_Fortran_SOURCE_FILE_EXTENSIONS} ${CMAKE_Java_SOURCE_FILE_EXTENSIONS} ${CMAKE_RC_SOURCE_FILE_EXTENSIONS})
-else(NOT ${PROJECT_NAME}_SOURCE_FILE_EXTENSIONS)
-  # specified extensions
-  foreach(_EXT ${${PROJECT_NAME}_SOURCE_FILE_EXTENSIONS})
-    list(APPEND _ALL_EXTS *.${_EXT})
-  endforeach(_EXT ${${PROJECT_NAME}_SOURCE_FILE_EXTENSIONS})
-endif(NOT ${PROJECT_NAME}_SOURCE_FILE_EXTENSIONS)
+  TARGET_LINK_LIBRARIES(${PROJECT_NAME}_static ${${PROJECT_NAME}_LINK_LIBRARIES})
+  TARGET_LINK_LIBRARIES(${PROJECT_NAME}_shared ${${PROJECT_NAME}_LINK_LIBRARIES})
+  
+  IF(APPLE)
+    SET(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "${CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS} -Wl,-search_paths_first")
+    IF(FORTRAN_LIBRARIES)
+      LINK_DIRECTORIES(${FORTRAN_COMPILER_LIB_DIRECTORY})
+      TARGET_LINK_LIBRARIES(${PROJECT_NAME}_static ${FORTRAN_LIBRARIES})
+      TARGET_LINK_LIBRARIES(${PROJECT_NAME}_shared ${FORTRAN_LIBRARIES})
+    ENDIF(FORTRAN_LIBRARIES)
+  ENDIF(APPLE)
 
-set(_ALL_FILES)
-foreach(_DIR ${_ALL_DIRS})
-  set(_DIR_FILES)
-  foreach(_EXT ${_ALL_EXTS})
-    file(GLOB _DIR_FILES_EXT ${_DIR}/${_EXT})
-    if(_DIR_FILES_EXT)
-      list(APPEND _DIR_FILES ${_DIR_FILES_EXT})
-    endif(_DIR_FILES_EXT)
-  endforeach(_EXT ${_ALL_EXTS})
-  if(_DIR_FILES)
-    foreach(_F ${_DIR_FILES})
-      if(NOT ${_F}_T)
-        set(${_F}_T T)
-        list(APPEND _ALL_FILES ${_F})
-      endif(NOT ${_F}_T)
-    endforeach(_F ${_DIR_FILES})
-  endif(_DIR_FILES)
-endforeach(_DIR ${_ALL_DIRS})
-
-#
-# headers
-#
-if(NOT ${PROJECT_NAME}_HDRS)
-  foreach(_DIR ${_ALL_DIRS})
-    file(GLOB _HDRS  ${_DIR}/*.h)
-    if(_HDRS)
-      list(APPEND ${PROJECT_NAME}_HDRS ${_HDRS})
-    endif(_HDRS)
-  endforeach(_DIR ${_ALL_DIRS})
-endif(NOT ${PROJECT_NAME}_HDRS)
-
-#
-# Unstable sources
-#
-if(NOT UNSTABLE)
-  if(${PROJECT_NAME}_Unstable_SRCS)
-    message(STATUS "Some unstables sources files are going to be excluded")
-    message(STATUS "To configure an unstable build, run : `cmake -DUNSTABLE=ON .'")
-    foreach(_FILE ${${PROJECT_NAME}_Unstable_SRCS})
-      file(GLOB _GFILE ${_FILE})
-      if(_GFILE)
-        message(STATUS "excluded : ${_GFILE}")
-        list(REMOVE_ITEM _ALL_FILES ${_GFILE})
-      else(_GFILE)
-        message(STATUS "WARNING : Unstable file NOT FOUND : ${_FILE}")
-      endif(_GFILE)
-    endforeach(_FILE ${${PROJECT_NAME}_Unstable_SRCS})
-  endif(${PROJECT_NAME}_Unstable_SRCS)
-endif(NOT UNSTABLE)
-
-#
-# Attempt to specify file properties
-#
-
-#if(${PROJECT_NAME}_SOURCES_PROPERTIES)
-#  foreach(_PROPERTY ${${PROJECT_NAME}_SOURCES_PROPERTIES})
-#    foreach(_FILE ${_ALL_FILES})
-#      if(${${PROJECT_NAME}_${_PROPERTY}_REGEX})
-#        if(${_FILE} MATCHES ${${PROJECT_NAME}_${_PROPERTY}}_REGEX})
-#          if(${${PROJECT_NAME}_${_PROPERTY})
-#            set_source_files_properties(${_FILE} ${${PROJECT_NAME}_${_PROPERTY}})
-#          endif(${${PROJECT_NAME}_${_PROPERTY})
-#        endif(${_FILE} MATCHES ${${PROJECT_NAME}_${_PROPERTY}}_REGEX})
-#      endif(${${PROJECT_NAME}_${_PROPERTY}_REGEX})
-#    endforeach(_FILE ${_ALL_FILES})
-#  endforeach(_PROPERTY ${${PROJECT_NAME}_SOURCES_PROPERTIES})
-#endif(${PROJECT_NAME}_SOURCES_PROPERTIES)
-
-set(${PROJECT_NAME}_SRCS ${_ALL_FILES})
-
-include_directories(${_ALL_DIRS})
-
-add_library(${PROJECT_NAME}_static  STATIC ${_ALL_FILES})
-add_library(${PROJECT_NAME}_shared  SHARED ${_ALL_FILES})
-
-set_target_properties(${PROJECT_NAME}_static PROPERTIES 
-  OUTPUT_NAME "${PROJECT_NAME}" 
-  VERSION "${${PROJECT_NAME}_VERSION}" 
-  CLEAN_DIRECT_OUTPUT 1 # no clobbering
-  LINKER_LANGUAGE C)
-
-set_target_properties(${PROJECT_NAME}_shared PROPERTIES
-  OUTPUT_NAME "${PROJECT_NAME}" 
-  VERSION "${${PROJECT_NAME}_VERSION}" 
-  CLEAN_DIRECT_OUTPUT 1
-  LINKER_LANGUAGE C)
-
-target_link_libraries(${PROJECT_NAME}_static ${${PROJECT_NAME}_LINK_LIBRARIES})
-target_link_libraries(${PROJECT_NAME}_shared ${${PROJECT_NAME}_LINK_LIBRARIES})
-
-if(APPLE)
-  set(CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS "${CMAKE_SHARED_LIBRARY_CREATE_C_FLAGS} -Wl,-search_paths_first")
-  if(FORTRAN_LIBRARIES)
-    link_directories(${FORTRAN_COMPILER_LIB_DIRECTORY})
-    target_link_libraries(${PROJECT_NAME}_static ${FORTRAN_LIBRARIES})
-    target_link_libraries(${PROJECT_NAME}_shared ${FORTRAN_LIBRARIES})
-  endif(FORTRAN_LIBRARIES)
-endif(APPLE)
-
-# output in ${PROJECT_NAME}_STATIC|SHARED_LIB the path of the libraries
-get_target_property(${PROJECT_NAME}_STATIC_LIB ${PROJECT_NAME}_static LOCATION)
-get_target_property(${PROJECT_NAME}_SHARED_LIB ${PROJECT_NAME}_shared LOCATION)
+  # output in ${PROJECT_NAME}_STATIC|SHARED_LIB the path of the libraries
+  GET_TARGET_PROPERTY(${PROJECT_NAME}_STATIC_LIB ${PROJECT_NAME}_static LOCATION)
+  GET_TARGET_PROPERTY(${PROJECT_NAME}_SHARED_LIB ${PROJECT_NAME}_shared LOCATION)
 
 
-# Installation
-if(${PROJECT_NAME}_LIB_DIR)
-  set(_install_lib ${${PROJECT_NAME}_LIB_DIR})
-else(${PROJECT_NAME}_LIB_DIR)
-  set(_install_lib lib)
-endif(${PROJECT_NAME}_LIB_DIR)
+  # Installation
+  IF(${PROJECT_NAME}_LIB_DIR)
+    SET(_install_lib ${${PROJECT_NAME}_LIB_DIR})
+  ELSE(${PROJECT_NAME}_LIB_DIR)
+    SET(_install_lib lib)
+  ENDIF(${PROJECT_NAME}_LIB_DIR)
 
-if(${PROJECT_NAME}_INSTALL_INCLUDE_DIR)
-  set(_install_include ${${PROJECT_NAME}_INSTALL_INCLUDE_DIR})
-else(${PROJECT_NAME}_INSTALL_INCLUDE_DIR)
-  set(_install_include include)
-endif(${PROJECT_NAME}_INSTALL_INCLUDE_DIR)
+  IF(${PROJECT_NAME}_INSTALL_INCLUDE_DIR)
+    SET(_install_include ${${PROJECT_NAME}_INSTALL_INCLUDE_DIR})
+  ELSE(${PROJECT_NAME}_INSTALL_INCLUDE_DIR)
+    SET(_install_include include)
+  ENDIF(${PROJECT_NAME}_INSTALL_INCLUDE_DIR)
 
-install(TARGETS 
-  ${PROJECT_NAME}_static ${PROJECT_NAME}_shared 
-  ARCHIVE DESTINATION ${_install_lib}
-  LIBRARY DESTINATION ${_install_lib})
-install(FILES ${${PROJECT_NAME}_HDRS} DESTINATION ${_install_include})
+  INSTALL(TARGETS 
+    ${PROJECT_NAME}_static ${PROJECT_NAME}_shared 
+    ARCHIVE DESTINATION ${_install_lib}
+    LIBRARY DESTINATION ${_install_lib})
+  INSTALL(FILES ${${PROJECT_NAME}_HDRS} DESTINATION ${_install_include})
 
-message(STATUS "${PROJECT_NAME} library setup done")
-message(STATUS "")
+  EXPORT_LIBRARY_DEPENDENCIES(DEPEND.cmake)
 
-endmacro(LIBRARY_PROJECT_SETUP)
+  MESSAGE(STATUS "${PROJECT_NAME} library setup done")
+  MESSAGE(STATUS "")
+
+ENDMACRO(LIBRARY_PROJECT_SETUP)
 
