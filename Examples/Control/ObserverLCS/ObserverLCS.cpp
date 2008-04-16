946ddb578335327ebe42bda87e99bec5e03e4206
Franck Perignon <franck.perignon@imag.fr>
2008-04-16 09:09:32 +0000

    LCSObserver example
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@1109 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Examples/Control/ObserverLCS/ObserverLCS.cpp
+++ Examples/Control/ObserverLCS/ObserverLCS.cpp
@@ -12,16 +12,31 @@ int main(int argc, char* argv[])
     // == User-defined parameters ==
     unsigned int ndof = 2;  // number of degrees of freedom of your system
     double t0 = 0.0;
-    double T = 10;        // Total simulation time
+    double T = 3;        // Total simulation time
     double h = 1.0e-3;      // Time step
     double Vinit = 1.0;
 
 
     // ================= Creation of the model =======================
+    // Steps:
+    // - create some Dynamical Systems
+    // - create some Interactions between those Dynamical Systems
+    //   Interaction = some relations (constraints) and a NonSmoothLaw
+    // - create a NonSmoothDynamicalSystem with the DynamicalSystems and the Interactions
+    // - add this NonSmoothDynamicalSystem into a Model
+    // - add a Simulation to the model
+    //  Simulation = TimeDiscretisation + OneStepIntegrator and OneStepNSProblem
 
-    // == Creation of the NonSmoothDynamicalSystem ==
-    // DynamicalSystem(s)
-    SiconosMatrix * A = new SimpleMatrix(ndof, ndof); // All components of A are automatically set to 0.
+    // -------------------------
+    // --- Dynamical systems ---
+    // -------------------------
+
+    // First System:
+    // dx/dt = Ax + u(t) + r
+    // x(0) = x0
+    // Note: r = Blambda, B defines in relation below.
+
+    SiconosMatrix * A = new SimpleMatrix(ndof, ndof);
     (*A)(0, 0) = 1.0;
     (*A)(0, 1) = 1.0;
     (*A)(1, 0) = 3.0;
@@ -29,20 +44,47 @@ int main(int argc, char* argv[])
     SiconosVector * x0 = new SimpleVector(ndof);
     (*x0)(0) = Vinit;
     FirstOrderLinearDS * process  = new FirstOrderLinearDS(0, *x0, *A);
+    process->setComputeBFunction("ObserverLCSPlugin.so", "uProcess");
 
-    process->setComputeBFunction("ObserverLCSPlugin.so", "computeU");
+    // Second System, the observer:
+    // dx/dt = A hatx + u(t) + L(y-haty)
+    // hatx(0) = x0
+    // y = Gx
+    // u(t) + Ly  is computed with uObserver function
 
+    unsigned int noutput = 1;
+    SiconosMatrix * L = new SimpleMatrix(ndof, noutput);
+    (*L)(0, 0) = 1.0;
+    (*L)(1, 0) = 1.0;
+    SiconosMatrix * G = new SimpleMatrix(noutput, ndof);
+    (*G)(0, 0) = 2.0;
+    (*G)(0, 1) = 2.0;
 
+    // hatA is initialized with A
+    SimpleMatrix* hatA = new SimpleMatrix(ndof, ndof);
+    (*hatA)(0, 0) = -1.0;
+    (*hatA)(0, 1) = -1.0;
+    (*hatA)(1, 0) = 1.0;
+    (*hatA)(1, 1) = 1.0;
+
+    FirstOrderLinearDS* observer = new FirstOrderLinearDS(1, *x0, *hatA);
+    observer->setComputeBFunction("ObserverLCSPlugin.so", "uObserver");
+    SimpleVector * z = new SimpleVector(1);
+    observer->setZPtr(z);
+    // The set of all DynamicalSystems
     DynamicalSystemsSet allDS;
     allDS.insert(process);
+    allDS.insert(observer);
 
-
-
-    // Relations
+    // --------------------
+    // --- Interactions ---
+    // --------------------
     unsigned int ninter = 1; // dimension of your Interaction = size of y and lambda vectors
-    unsigned int noutput = 1;
     unsigned int ncontrol = 1;
 
+    // First relation, related to the process
+    // y = Cx + Dlambda
+    // r = Blambda
     SiconosMatrix * B = new SimpleMatrix(ndof, ninter);
     (*B)(0, 0) = -1.0;
     (*B)(1, 0) = 1.0;
@@ -50,110 +92,63 @@ int main(int argc, char* argv[])
     (*C)(0, 0) = -1.0;
     (*C)(0, 1) = 1.0;
     FirstOrderLinearR * myProcessRelation = new FirstOrderLinearR(C, B);
-
-    myProcessRelation->setComputeEFunction("ObserverLCSPlugin.so", "computeE");
-
-
     SiconosMatrix* D = new SimpleMatrix(ninter, ninter);
     (*D)(0, 0) = 1.0;
+
     myProcessRelation->setDPtr(D);
-    SiconosMatrix * F = new SimpleMatrix(ninter, ncontrol);
-    (*F)(0, 0) = 1.0;
-    SiconosMatrix * E = new SimpleMatrix(ndof, ncontrol);
-    (*E)(0, 0) = 1.0;
-    (*E)(1, 0) = 2.0;
-    SiconosMatrix * L = new SimpleMatrix(ndof, noutput);
-    (*L)(0, 0) = 1.0;
-    (*L)(1, 0) = 1.0;
-    SiconosMatrix * G = new SimpleMatrix(noutput, ndof);
-    (*G)(0, 0) = 2.0;
-    (*G)(0, 1) = 2.0;
-    SiconosMatrix * hatA = new SimpleMatrix(ndof, ndof);
+    myProcessRelation->setComputeEFunction("ObserverLCSPlugin.so", "computeE");
 
-
-
-
-    (*hatA) = (*A)     -   prod(*L, *G);
-
-    FirstOrderLinearDS* observer = new FirstOrderLinearDS(1, *x0, *hatA);
-    //observer->setComputeBFunction("ObserverLCSPlugin.so","computebobserver");
-    SimpleVector * z = new SimpleVector(1);
-    observer->setZPtr(z);
-
-
-    allDS.insert(observer);
-
-
+    // Second relation, related to the observer
+    // haty = C hatX + D hatLambda + E
+    // hatR = B hatLambda
     FirstOrderLinearR * myObserverRelation = new FirstOrderLinearR(C, B);
-
-    myObserverRelation->setComputeEFunction("ObserverLCSPlugin.so", "computeE");
-
     myObserverRelation->setDPtr(D);
-
-
+    myObserverRelation->setComputeEFunction("ObserverLCSPlugin.so", "computeE");
 
     // NonSmoothLaw
     unsigned int nslawSize = 1;
     NonSmoothLaw * myNslaw = new ComplementarityConditionNSL(nslawSize);
 
+    // The Interaction which involves the first DS (the process)
+    string nameInter = "processInteraction"; // Name
+    unsigned int numInter = 1; // Dim of the interaction = dim of y and lambda vectors
 
+    Interaction* myProcessInteraction = new Interaction(nameInter, process, numInter, ninter, myNslaw, myProcessRelation);
 
-    // Choose a name and a number for your Interaction
-    string nameInter = "processInteraction";
-    unsigned int numInter = 1;
-    cout << "tutu " << endl;
-
-    allDS.display();
-
-
-    DynamicalSystemsSet dsProcessConcerned;
-    dsProcessConcerned.insert(allDS.getPtr(0));
-    cout << "tutu " << endl;
-
-    Interaction* myProcessInteraction = new Interaction(nameInter, dsProcessConcerned, numInter, ninter, myNslaw, myProcessRelation);
-
-    InteractionsSet allInteractions;
-    allInteractions.insert(myProcessInteraction);
-    cout << "tutu " << endl;
-
-
-    string nameInter2 = "observerInteraction";
+    // The Interaction which involves the second DS (the observer)
+    string nameInter2 = "observerInteraction"; // Name
     unsigned int numInter2 = 2;
 
-    DynamicalSystemsSet dsObserverConcerned;
-    dsObserverConcerned.insert(allDS.getPtr(1));
+    Interaction* myObserverInteraction = new Interaction(nameInter2, observer, numInter2, ninter, myNslaw, myObserverRelation);
 
-    Interaction* myObserverInteraction = new Interaction(nameInter2, dsObserverConcerned, numInter2, ninter, myNslaw, myObserverRelation);
-
+    // The set of all Interactions
+    InteractionsSet allInteractions;
+    allInteractions.insert(myProcessInteraction);
     allInteractions.insert(myObserverInteraction);
 
-
-
-
-    // NonSmoothDynamicalSystem
+    // --------------------------------
+    // --- NonSmoothDynamicalSystem ---
+    // --------------------------------
     NonSmoothDynamicalSystem* myNSDS = new NonSmoothDynamicalSystem(allDS, allInteractions);
 
-    cout << "toto " << endl;
-
-
-    // Model
+    // -------------
+    // --- Model ---
+    // -------------
     Model * ObserverLCS = new Model(t0, T);
-    ObserverLCS->setNonSmoothDynamicalSystemPtr(myNSDS);
+    ObserverLCS->setNonSmoothDynamicalSystemPtr(myNSDS); // set NonSmoothDynamicalSystem of this model
+
+    // ------------------
+    // --- Simulation ---
+    // ------------------
     // TimeDiscretisation
     TimeDiscretisation * td = new TimeDiscretisation(h, ObserverLCS);
     // == Creation of the Simulation ==
     TimeStepping * s = new TimeStepping(td);
-
-
-    // OneStepIntegrator
+    // -- OneStepIntegrators --
     double theta = 0.5;
-    // One Step Integrator
     Moreau* myIntegrator = new Moreau(allDS, theta, s);
-    // OneStepNSProblem
 
-
-
-    // One Step non smooth problem
+    // -- OneStepNsProblem --
     IntParameters iparam(5);
     iparam[0] = 1000; // Max number of iteration
     DoubleParameters dparam(5);
@@ -162,60 +157,68 @@ int main(int argc, char* argv[])
     NonSmoothSolver * mySolver = new NonSmoothSolver(solverName, iparam, dparam);
     LCP * osnspb = new LCP(s, mySolver);
 
+    // =========================== End of model definition ===========================
+
     // ================================= Computation =================================
 
-    // --- Initialisation of the simulation ---
+    // --- Simulation initialization ---
+
+    cout << "====> Simulation initialisation ..." << endl << endl;
+
     s->initialize();
+
+    // --- Get the values to be plotted ---
+    unsigned int outputSize = 7; // number of required data
+    int N = td->getNSteps(); // Number of time steps
+    SimpleMatrix dataPlot(N, outputSize);
+
+    SiconosVector * xProc = process->getXPtr();
+    SiconosVector * xObs = observer->getXPtr();
+    SiconosVector * lambdaProc = myProcessInteraction->getLambdaPtr(0);
+    SiconosVector * lambdaObs = myObserverInteraction->getLambdaPtr(0);
+
+    // -> saved in a matrix dataPlot
+    dataPlot(0, 0) = ObserverLCS->getT0(); // Initial time of the model
+    dataPlot(0, 1) = (*xProc)(0);
+    dataPlot(0, 2) = (*xProc)(1);
+    dataPlot(0, 3) = (*xObs)(0);
+    dataPlot(0, 4) = (*xObs)(1);
+    dataPlot(0, 5) = (*lambdaProc)(0);
+    dataPlot(0, 6) = (*lambdaObs)(0);
+
+
+    // ==== Simulation loop =====
+    cout << "====> Start computation ... " << endl << endl;
 
     *z = *(myProcessInteraction->getYPtr(0)->getVectorPtr(0));
     int k = 0; // Current step
-    int N = td->getNSteps(); // Number of time steps
-    unsigned int outputSize = 7; // number of required data
-    SimpleMatrix dataPlot(N, outputSize);
-    SiconosVector * processLambda = myProcessInteraction->getLambdaPtr(0);
-    SiconosVector * observerLambda = myObserverInteraction->getLambdaPtr(0);
-    // We get values for the initial time step:
-    // time
-    dataPlot(k, 0) = s->getNextTime();
-    dataPlot(k, 1) = (observer->getX())(0);
-    dataPlot(k, 2) = (observer->getX())(1);
-    dataPlot(k, 3) = (process->getX())(0);
-    dataPlot(k, 4) = (process->getX())(1);
-    dataPlot(k, 5) = (*processLambda)(0);
-    dataPlot(k, 6) = (*observerLambda)(0);
-
 
     // Simulation loop
+    boost::timer time;
+    time.restart();
     while (k < N - 1)
     {
       k++;
-
-      s->nextStep();
-      // get current time step
-
       s->computeOneStep();
       *z = *(myProcessInteraction->getYPtr(0)->getVectorPtr(0));
-      dataPlot(k, 0) = s->getNextTime();;
-      dataPlot(k, 1) = (observer->getX())(0);
-      dataPlot(k, 2) = (observer->getX())(1);
-      dataPlot(k, 3) = (process->getX())(0);
-      dataPlot(k, 4) = (process->getX())(1);
-      dataPlot(k, 5) = (*processLambda)(0);
-      dataPlot(k, 6) = (*observerLambda)(0);
+      dataPlot(k, 0) = s->getNextTime();
+      dataPlot(k, 1) = (*xProc)(0);
+      dataPlot(k, 2) = (*xProc)(1);
+      dataPlot(k, 3) = (*xObs)(0);
+      dataPlot(k, 4) = (*xObs)(1);
+      dataPlot(k, 5) = (*lambdaProc)(0);
+      dataPlot(k, 6) = (*lambdaObs)(0);
+      s->nextStep();
     }
+    cout << endl << "End of computation - Number of iterations done: " << k - 1 << endl;
+    cout << "Computation Time " << time.elapsed()  << endl;
 
-    // Write the results into the file "ObserverLCS.dat"
+    // --- Output files ---
+    cout << "====> Output file writing ..." << endl;
     ioMatrix io("ObserverLCS.dat", "ascii");
     io.write(dataPlot, "noDim");
 
-    // --- Time loop ---
-
   }
-
-
-
-
-
 
   catch (SiconosException e)
   {
--- Examples/Control/ObserverLCS/ObserverLCSPlugin.cpp
+++ Examples/Control/ObserverLCS/ObserverLCSPlugin.cpp
@@ -2,20 +2,14 @@
 #include <math.h>
 
 
-extern "C"   double computeControl(double time)
+extern "C" double computeControl(double time)
 {
-
-
   double u;
   double alpha = 1.0;
   double T = 0.1;
-  int oddoreven = 1 ;
-  printf("oodoreven = %i \n ", oddoreven);
+  int oddoreven = int(time / T);
   printf("oodoreven = %i \n ", int(time));
   printf("oodoreven = %i \n ", int(T));
-
-  oddoreven = int(time / T);
-
   printf("oodoreven = %i \n ", oddoreven);
 
   if ((oddoreven / 2) == 0) u = alpha;
@@ -24,41 +18,30 @@ extern "C"   double computeControl(double time)
   return u;
 }
 
-
-
-
-extern "C"   void computeU(double time, unsigned int sizeOfB, double* b, unsigned int sizeOfZ, double* z)
+extern "C" void uProcess(double time, unsigned int sizeOfB, double* b, unsigned int sizeOfZ, double* z)
 {
-
-  double u;
-  u = computeControl(time);
+  double u = computeControl(time);
   b[0] = u ;
   b[1] = 2.0 * u;
-  printf("End ComputeU\n ");
+  printf("End u(t) \n ");
 }
 
-
-extern "C"   void computebobserver(double time, unsigned int sizeOfB, double* b, unsigned int sizeOfZ, double* z)
+extern "C"   void uObserver(double time, unsigned int sizeOfB, double* b, unsigned int sizeOfZ, double* z)
 {
-
-  // Si b est de taille 2
-
-  printf("start ComputeU\n ");
+  printf("Compute u for observer \n ");
+  double u = computeControl(time);
   double L[2];
-  double u ;
-  u = computeControl(time);
   L[0] = 1.0;
   L[1] = 1.0;
 
   b[0] = u + L[0] * z[0];
   b[1] = 2.0 * u + L[1] * z[0];
-  printf("End ComputeU\n ");
-
+  printf("End u observer \n ");
 }
 
 extern "C"   void computeE(double time, unsigned int sizeOfB, double* e, unsigned int sizeOfZ, double* z)
 {
   printf("ComputeE\n ");
-  // Si b est de taille 2
-  e[0] = computeControl(time) ;
+  e[0] = computeControl(time);
+  e[1] = 0.0;
 }
