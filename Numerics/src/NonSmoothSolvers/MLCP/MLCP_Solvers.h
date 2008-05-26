0749d8b0be5b9b125580dd9c4db9f81719d95e40
Franck Perignon <franck.perignon@imag.fr>
2008-05-26 09:41:00 +0000

    fix bug in doxygen
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@1182 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Numerics/src/NonSmoothSolvers/MLCP/MLCP_Solvers.h
+++ Numerics/src/NonSmoothSolvers/MLCP/MLCP_Solvers.h
@@ -210,14 +210,14 @@ extern "C" {
   void mlcp_direct_enum(MixedLinearComplementarity_Problem* problem, double *z, double *w, int *info, Solver_Options* options);
 
   /** direct-simplex solver
-  * \param[in] problem structure that represents the MLCP (n,mM, q...)
-  * \param[out] z a m+n-vector of doubles which contains the initial solution and returns the solution of the problem.
-  * \param[out] w a m+n-vector of doubles which returns the solution of the problem.
-  * \param[out] info an integer which returns the termination value:\n
-  0 : success,it found a solution\n
-  1 : echec,it did not find any solution\n
-  \param[in-out] options structure used to define the solver and its parameters.
-  \author Olivier Bonnefon
+   * \param[in] problem structure that represents the MLCP (n,mM, q...)
+   * \param[out] z a m+n-vector of doubles which contains the initial solution and returns the solution of the problem.
+   * \param[out] w a m+n-vector of doubles which returns the solution of the problem.
+   * \param[out] info an integer which returns the termination value:\n
+   0 : success,it found a solution\n
+   1 : echec,it did not find any solution\n
+   \param[in-out] options structure used to define the solver and its parameters.
+   \author Olivier Bonnefon
   */
   void mlcp_direct_simplex(MixedLinearComplementarity_Problem* problem, double *z, double *w, int *info, Solver_Options* options);
 
@@ -247,37 +247,37 @@ extern "C" {
   void mlcp_simplex(MixedLinearComplementarity_Problem* problem, double *z, double *w, int *info, Solver_Options* options);
 
   /**
-   * This function checks the validity of the vector z as a solution \n
-   * of the MLCP : \n
-   * \f$
-   *  \left\lbrace
-   *   \begin{array}{l}
-   *   A u + Cv +a =w1\\
-   *   D u + Bv +b = w2\\
-   *   0 \le v \perp  w2 \ge 0\\
-   *   w1=0\\
-   *   \end{array}
-   *  \right.
-   * w =
-   * \left\lbrace
-   *   \begin{array}{c}
-   *   w1\\
-   *   w2\\
-   *   \end{array}
-   *  \right
-   * \f$
-   * The criterion is based on \f$ \sum [ (z[i]*(Mz+q)[i])_{pos} + (z[i])_{neg} + (Mz+q)[i])_{neg} ] \f$ \n
-   * with \f$ x_{pos} = max(0,x) \f$ and \f$ xneg = max(0,-x)\f$. \n
-   * This sum is divided by \f$ \|q\| \f$ and then compared to tol.\n
-   * It changes the input vector w by storing \f$ Mz + q \f$ in it.\n
-   * \param[in] problem structure that represents the MLCP (n,m,M, q... or (A,B,C...))
-   * \param[in,out] z a m+n-vector of doubles which contains the initial solution and returns the solution of the problem.
-   * \param[in,out] w a m+n-vector of doubles which returns the solution of the problem.
-   * \param[in] tolerance
-   * \param[in,out] error
-   * \return status: 0 : convergence, 1: error > tolerance
-   * \author Vincent Acary form the routine  filter_result_LCP.c of Pascal Denoyelle
-   */
+    This function checks the validity of the vector z as a solution \n
+    of the MLCP : \n
+    \f{eqnarray*}
+     \left\lbrace
+      \begin{array}{l}
+      A u + Cv +a =w1\\
+      D u + Bv +b = w2\\
+      0 \le v \perp  w2 \ge 0\\
+      w1=0\\
+      \end{array}
+     \right.
+     w =
+     \left\lbrace
+     \begin{array}{c}
+     w1\\
+     w2\\
+     \end{array}
+     \right\rbrace
+     \f}
+     The criterion is based on \f$ \sum [ (z[i]*(Mz+q)[i])_{pos} + (z[i])_{neg} + (Mz+q)[i])_{neg} ] \f$ \n
+     with \f$ x_{pos} = max(0,x) \f$ and \f$ xneg = max(0,-x)\f$. \n
+     This sum is divided by \f$ \|q\| \f$ and then compared to tol.\n
+     It changes the input vector w by storing \f$ Mz + q \f$ in it.\n
+     \param[in] problem structure that represents the MLCP (n,m,M, q... or (A,B,C...))
+     \param[in,out] z a m+n-vector of doubles which contains the initial solution and returns the solution of the problem.
+     \param[in,out] w a m+n-vector of doubles which returns the solution of the problem.
+     \param[in] tolerance
+     \param[in,out] error
+     \return status: 0 : convergence, 1: error > tolerance
+    \author Vincent Acary form the routine  filter_result_LCP.c of Pascal Denoyelle
+  */
   int mlcp_compute_error(MixedLinearComplementarity_Problem* problem, double *z, double *w, double tolerance, double * error);
 
 #ifdef __cplusplus
