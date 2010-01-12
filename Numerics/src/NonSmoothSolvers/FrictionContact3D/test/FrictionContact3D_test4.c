c4052ddb69e5064824c949603528afe9cb26f9d6
Vincent Acary <vincent.acary@inria.fr>
2010-01-12 14:50:37 +0000

    split FC3D test and remove old test
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@1732 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

---
+++ Numerics/src/NonSmoothSolvers/FrictionContact3D/test/FrictionContact3D_test4.c
@@ -0,0 +1,42 @@
+/* Siconos-Numerics version 3.0.0, Copyright INRIA 2005-2008.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY vincent.acary@inrialpes.fr
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "NonSmoothDrivers.h"
+#include "frictionContact3D_test_function.h"
+
+int main(void)
+{
+  int info = 0 ;
+  printf("Test on ./data/Example1_Fc3D_SBM.dat\n");
+
+  FILE * finput  =  fopen("./data/Example1_Fc3D_SBM.dat", "r");
+
+  char solvername[10] = "NSGS";
+
+  int iparam[5] = {1001, 0, 0, 0, 3} ;
+  double dparam[5] = {1e-16, 0, 1e-16, 0, 0};
+  info = frictionContact3D_test_function(finput, solvername, iparam, dparam);
+
+  fclose(finput);
+  printf("End of test on ./data/Example1_Fc3D_SBM.dat\n");
+
+
+  return info;
+}
