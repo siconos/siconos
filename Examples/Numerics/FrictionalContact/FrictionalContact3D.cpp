a616dff28166446bdc6884398d82adf0b6f4de27
Vincent Acary <vincent.acary@inria.fr>
2010-01-15 15:59:36 +0000

    add cpp extension
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@1763 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Examples/Numerics/FrictionalContact/FrictionalContact3D.cpp
+++ Examples/Numerics/FrictionalContact/FrictionalContact3D.cpp
@@ -1,1 +1,150 @@
-FrictionalContact3D.c
+/* Siconos-sample version 3.0.0, Copyright INRIA 2005-2008.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY vincent.acary@inrialpes.fr
+ */
+
+/*!\file FrictionalContact3D.cpp
+  A very simple example to chow how to use SiconosNumerics to solve the time--discretized FrictionalContact3D problem with a dense storage.
+  The problem: Find \f$(reaction,velocity)\f$ such that:\n
+
+  \f$
+  \left\lbrace
+  \begin{array}{l}
+  M \ reaction + q = velocity \\
+  0 \le reaction_n \perp velocity_n \ge 0 \\
+  -velocity_t \in \partial\psi_{D_(\mu reaction_n)}(reaction_t)\\
+  D_(\mu reaction_n) = \{ reaction_t \mid  \|reaction_t\| \leq \mu reaction_n  \}
+  \end{array}
+  \right.
+  \f$
+
+  \f$ reaction, velocity, q\f$ are vectors of size n and \f$ M \f$ is a nXn matrix, with \f$ n = 2*nc or 3*nc \f$, nc being the number of contacts. \n
+  \f$ reaction_n\f$ represents the normal part of the reaction while \f$ reaction_t\f$ is its tangential part.
+
+  \f$ \mu \f$ is the friction coefficient (it may be different for each contact).
+
+  Use the generic function frictionContact3D_driver() to call one the the specific solvers listed below:
+
+  - frictionContact3D_nsgs() : non-smooth Gauss-Seidel solver
+
+  (see the functions/solvers list in FrictionContact3D_Solvers.h)
+
+  FrictionContact3D problems needs some specific parameters, given to the FrictionContact3D_driver() function thanks to a Solver_Options structure. \n
+  They are:\n
+     - the name of the solver (ex: NSGS), used to switch to the right solver function
+
+     - isStorageSparse: 1 if a SparseBlockStructuredMatrix is used for M, else 0 (double* storage)
+
+
+  \brief
+*/
+
+#include "SiconosNumerics.h"
+
+int main(int argc, char* argv[])
+{
+
+
+  // Problem Definition
+  int NC = 3;//Number of contacts
+  double M[81] = {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+  double q[9] = { -1, 1, 3, -1, 1, 3, -1, 1, 3};
+  double mu[3] = {0.1, 0.1, 0.1};
+
+  int i, j, k;
+  /*  for (j =0 ; j<9; j++){ */
+  /*  for (k =0 ; k<9; k++) */
+  /*      M[j][k]=0.0; */
+  /*     } */
+  /*     for (i =0 ; i<NC; i++) */
+  /*  { */
+  /*      mu[i]=0.1; */
+  /*      for (j =0 ; j<3; j++){ */
+  /*      q[3*i+j] =-1.0+j*2.0;  */
+  /*    M[3*i+j][3*i+j]=1.0;    */
+  /*      } */
+  /*  } */
+
+
+  FrictionContact_Problem NumericsProblem;
+  NumericsProblem.numberOfContacts = NC;
+  NumericsProblem.dimension = 3;
+  NumericsProblem.isComplete = 0;
+  NumericsProblem.mu = mu;
+  NumericsProblem.q = q;
+
+  NumericsMatrix *MM = (NumericsMatrix*)malloc(sizeof(*MM));
+  MM->storageType = 0;
+  MM->matrix0 = M;
+  MM->size0 = 3 * NC;
+  MM->size1 = 3 * NC;
+
+  NumericsProblem.M = MM;
+
+  // Unknown Declaration
+
+  double *reaction = (double*)malloc(3 * NC * sizeof(double));
+  double *velocity = (double*)malloc(3 * NC * sizeof(double));
+
+  // Numerics and Solver Options
+
+  Numerics_Options numerics_options;
+  numerics_options.verboseMode = 1; // turn verbose mode to off by default
+
+
+  Solver_Options numerics_solver_options;
+  numerics_solver_options.filterOn = 0;
+  numerics_solver_options.isSet = 1;
+
+  strcpy(numerics_solver_options.solverName, "NSGS");
+
+  numerics_solver_options.iSize = 5;
+  numerics_solver_options.iparam = (int*)malloc(numerics_solver_options.iSize * sizeof(int));
+  numerics_solver_options.dSize = 5;
+  numerics_solver_options.dparam = (double*)malloc(numerics_solver_options.dSize * sizeof(double));
+
+  int nmax = 10000; // Max number of iteration
+  int localsolver = 0; // 0: projection on Cone, 1: Newton/AlartCurnier,  2: projection on Cone with local iteration, 3: projection on Disk  with diagonalization,
+  double tolerance = 1e-10;
+  double localtolerance = 1e-12;
+
+
+  numerics_solver_options.iparam[0] = nmax ;
+  numerics_solver_options.iparam[1] = 2 ;
+  numerics_solver_options.iparam[4] = localsolver ;
+  numerics_solver_options.dparam[0] = tolerance ;
+  numerics_solver_options.dparam[2] = localtolerance ;
+
+  //Driver call
+  frictionContact3D_driver(&NumericsProblem,
+                           reaction , velocity,
+                           &numerics_solver_options, &numerics_options);
+
+
+
+  // Solver output
+  printf("\n");
+  for (k = 0 ; k < 3 * NC; k++) printf("Velocity[%i] = %12.8e \t \t Reaction[%i] = %12.8e \n ", k, velocity[k], k , reaction[k]);
+  printf("\n");
+
+
+  free(reaction);
+  free(velocity);
+  free(MM);
+
+
+}
--- Examples/Numerics/FrictionalContact/PrimalFrictionalContact3D.cpp
+++ Examples/Numerics/FrictionalContact/PrimalFrictionalContact3D.cpp
@@ -1,1 +1,214 @@
-PrimalFrictionalContact3D.c
+/* Siconos-sample version 3.0.0, Copyright INRIA 2005-2008.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY vincent.acary@inrialpes.fr
+ */
+
+/*!\file FrictionalContact3D.cpp
+  A very simple example to chow how to use SiconosNumerics to solve the time--discretized FrictionalContact3D problem with a dense storage.
+  The problem: Find \f$(reaction,velocity)\f$ such that:\n
+
+  \f$
+  \left\lbrace
+  \begin{array}{l}
+  M \ reaction + q = velocity \\
+  0 \le reaction_n \perp velocity_n \ge 0 \\
+  -velocity_t \in \partial\psi_{D_(\mu reaction_n)}(reaction_t)\\
+  D_(\mu reaction_n) = \{ reaction_t \mid  \|reaction_t\| \leq \mu reaction_n  \}
+  \end{array}
+  \right.
+  \f$
+
+  \f$ reaction, velocity, q\f$ are vectors of size n and \f$ M \f$ is a nXn matrix, with \f$ n = 2*nc or 3*nc \f$, nc being the number of contacts. \n
+  \f$ reaction_n\f$ represents the normal part of the reaction while \f$ reaction_t\f$ is its tangential part.
+
+  \f$ \mu \f$ is the friction coefficient (it may be different for each contact).
+
+  Use the generic function frictionContact3D_driver() to call one the the specific solvers listed below:
+
+  - frictionContact3D_nsgs() : non-smooth Gauss-Seidel solver
+
+  (see the functions/solvers list in FrictionContact3D_Solvers.h)
+
+  FrictionContact3D problems needs some specific parameters, given to the FrictionContact3D_driver() function thanks to a Solver_Options structure. \n
+  They are:\n
+     - the name of the solver (ex: NSGS), used to switch to the right solver function
+     - iparam[0]: max. number of iterations allowed
+     - iparam[1]:
+     - dparam[0]: tolerance
+     - isStorageSparse: 1 if a SparseBlockStructuredMatrix is used for M, else 0 (double* storage)
+
+
+  \brief
+*/
+
+#include "SiconosNumerics.h"
+
+int main(int argc, char* argv[])
+{
+
+
+  // Problem Definition
+  int info = -1;
+
+
+
+  int NC = 1;//Number of contacts
+  int Ndof = 9;//Number of DOF
+  int m = 3;
+  int n = 9;
+  double M[81] = {1, 0, 0, 0, 0, 0, 0, 0, 0,
+                  0, 1, 0, 0, 0, 0, 0, 0, 0,
+                  0, 0, 1, 0, 0, 0, 0, 0, 0,
+                  0, 0, 0, 1, 0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 1, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0, 1, 0, 0, 0,
+                  0, 0, 0, 0, 0, 0, 1, 0, 0,
+                  0, 0, 0, 0, 0, 0, 0, 1, 0,
+                  0, 0, 0, 0, 0, 0, 0, 0, 1
+                 };
+
+  double H[27] = {1, 0, 0, 0, 0, 0, -1, 0, 0,
+                  0, 1, 0, 0, 0, 0, 0, -1, 0,
+                  0, 0, 1, 0, 0, 0, 0, 0, -1
+                 };
+
+  double q[9] = { -3, -3, -3, -1, 1, 3, -1, 1, 3};
+  double b[3] = {0, 0, 0};
+  double mu[1] = {0.1};
+
+  /*     int NC = 3;//Number of contacts  */
+  /*     int Ndof = 9;//Number of DOF  */
+  /*     double M[81] = {1, 0, 0, 0, 0, 0, 0, 0, 0,  */
+  /*        0, 1, 0, 0, 0, 0, 0, 0, 0,  */
+  /*        0, 0, 1, 0, 0, 0, 0, 0, 0,  */
+  /*        0, 0, 0, 1, 0, 0, 0, 0, 0,  */
+  /*        0, 0, 0, 0, 1, 0, 0, 0, 0,  */
+  /*        0, 0, 0, 0, 0, 1, 0, 0, 0,  */
+  /*        0, 0, 0, 0, 0, 0, 1, 0, 0,  */
+  /*        0, 0, 0, 0, 0, 0, 0, 1, 0,  */
+  /*        0, 0, 0, 0, 0, 0, 0, 0, 1}; */
+  /*     double H[81] = {1, 0, 0, 0, 0, 0, 0, 0, 0,  */
+  /*        0, 1, 0, 0, 0, 0, 0, 0, 0,  */
+  /*        0, 0, 1, 0, 0, 0, 0, 0, 0,  */
+  /*        0, 0, 0, 1, 0, 0, 0, 0, 0,  */
+  /*        0, 0, 0, 0, 1, 0, 0, 0, 0,  */
+  /*        0, 0, 0, 0, 0, 1, 0, 0, 0,  */
+  /*        0, 0, 0, 0, 0, 0, 1, 0, 0,  */
+  /*        0, 0, 0, 0, 0, 0, 0, 1, 0,  */
+  /*        0, 0, 0, 0, 0, 0, 0, 0, 1}; */
+
+
+  /*     double q[9] = {-1, 1, 3, -1, 1, 3, -1, 1, 3}; */
+  /*     double b[9] = {0, 0, 0,0, 0, 0,0, 0, 0 }; */
+  /*     double mu[3] = {0.1,0.1,0.1};    */
+
+
+  int i = 0, j = 0, k = 0;
+
+  PrimalFrictionContact_Problem numericsProblem;
+  numericsProblem.numberOfContacts = NC;
+  numericsProblem.dimension = 3;
+  numericsProblem.isComplete = 0;
+  numericsProblem.mu = mu;
+  numericsProblem.q = q;
+  numericsProblem.b = b;
+
+
+  numericsProblem.M = (NumericsMatrix*)malloc(sizeof(NumericsMatrix));
+  NumericsMatrix *MM = numericsProblem.M ;
+  MM->storageType = 0;
+  MM->matrix0 = M;
+  MM->size0 = n;
+  MM->size1 = n;
+
+
+  numericsProblem.H  = (NumericsMatrix*)malloc(sizeof(NumericsMatrix));
+  NumericsMatrix *HH = numericsProblem.H;
+  HH->storageType = 0;
+  HH->matrix0 = H;
+  HH->size0 = n;
+  HH->size1 = m;
+
+
+  /*     FILE * foutput = fopen("Example_PrimalFrictionContact.dat", "w"); */
+  /*     primalFrictionContact_printInFile(&numericsProblem,  foutput ); */
+  /*     fclose(foutput); */
+
+
+
+  // Unknown Declaration
+
+  double *reaction = (double*)malloc(m * sizeof(double));
+  double *velocity = (double*)malloc(m * sizeof(double));
+  double *globalVelocity = (double*)malloc(n * sizeof(double));
+
+  // Numerics and Solver Options
+
+  Numerics_Options numerics_options;
+  numerics_options.verboseMode = 1; // turn verbose mode to off by default
+
+
+  Solver_Options numerics_solver_options;
+  numerics_solver_options.filterOn = 0;
+  numerics_solver_options.isSet = 1;
+
+  strcpy(numerics_solver_options.solverName, "PROX_WR");
+  strcpy(numerics_solver_options.solverName, "NSGS");
+
+  numerics_solver_options.iSize = 5;
+  numerics_solver_options.iparam = (int*)malloc(numerics_solver_options.iSize * sizeof(int));
+  numerics_solver_options.dSize = 5;
+  numerics_solver_options.dparam = (double*)malloc(numerics_solver_options.dSize * sizeof(double));
+
+  int nmax = 10000; // Max number of iteration
+  int localsolver = 0; // 0: projection on Cone, 1: Newton/AlartCurnier,  2: projection on Cone with local iteration, 2: projection on Disk  with diagonalization,
+  double tolerance = 1e-10;
+  double localtolerance = 1e-12;
+
+  for (i = 0; i < numerics_solver_options.iSize ; i++) numerics_solver_options.iparam[i] = 0;
+  for (i = 0; i < numerics_solver_options.dSize ; i++) numerics_solver_options.dparam[i] = 0.0;
+
+
+  numerics_solver_options.iparam[0] = nmax ;
+  numerics_solver_options.iparam[4] = localsolver ;
+  numerics_solver_options.dparam[0] = tolerance ;
+  numerics_solver_options.dparam[2] = localtolerance ;
+
+  //Driver call
+  i = 0;
+  info = primalFrictionContact3D_driver(&numericsProblem,
+                                        reaction , velocity, globalVelocity,
+                                        &numerics_solver_options, &numerics_options);
+
+
+  // Solver output
+  printf("\n");
+  for (k = 0 ; k < m; k++) printf("velocity[%i] = %12.8e \t \t reaction[%i] = %12.8e \n ", k, velocity[k], k , reaction[k]);
+  for (k = 0 ; k < n; k++) printf("globalVelocity[%i] = %12.8e \t \n ", k, globalVelocity[k]);
+  printf("\n");
+
+  free(reaction);
+  free(velocity);
+  free(globalVelocity);
+  free(numericsProblem.M);
+  free(numericsProblem.H);
+  free(numerics_solver_options.iparam);
+  free(numerics_solver_options.dparam);
+  return info;
+
+
+}
