6006f22546a0e8de6bdd9ad6dd6fd19db4a0829e
Maurice Bremond <maurice.bremond@inria.fr>
2010-05-12 17:58:30 +0000

    package target and Pipol var (escape $ madness with CMake / Make / Shell / backquotes ...)
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@1986 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Numerics/cmake/Pipol.cmake
+++ Numerics/cmake/Pipol.cmake
@@ -17,20 +17,15 @@ ENDIF(_STMP STREQUAL "[]")
 #
 
 SET(PIPOL_USER $ENV{PIPOL_USER})
-IF(NOT PIPOL_USER)
-  SET(PIPOL_USER $ENV{USER})
-  IF(NOT PIPOL_USER)
-    MESSAGE(STATUS "neither $PIPOL_USER nor $USER exists")
-  ENDIF(NOT PIPOL_USER)
-ENDIF(NOT PIPOL_USER)
 
 
-# scp is needed to get pipol-sub
-FIND_PROGRAM(HAVE_SCP scp)
+
+# ssh/rsync mandatory 
 FIND_PROGRAM(HAVE_SSH ssh)
-
+FIND_PROGRAM(HAVE_RSYNC rsync)
 
 IF(PIPOL_USER)
+  MESSAGE(STATUS "Pipol user is ${PIPOL_USER}")
   IF(HAVE_SSH)
     # get pipol systems
     EXECUTE_PROCESS(COMMAND 
@@ -38,13 +33,13 @@ IF(PIPOL_USER)
       OUTPUT_VARIABLE PIPOL_SYSTEMS OUTPUT_STRIP_TRAILING_WHITESPACE)
   ENDIF(HAVE_SSH)
 
-  IF(HAVE_SCP)
+  IF(HAVE_RSYNC)
     MACRO(PIPOL_TARGET
         SYSTEM_PATTERN)
 
       # defaults
       IF(NOT PIPOL_CONFIGURE_COMMAND)
-        SET(PIPOL_CONFIGURE_COMMAND "cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} ${CMAKE_SOURCE_DIR}")
+        SET(PIPOL_CONFIGURE_COMMAND "cmake -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} ${CMAKE_SOURCE_DIR}")
       ENDIF(NOT PIPOL_CONFIGURE_COMMAND)
 
       IF(NOT PIPOL_MAKE_COMMAND)
@@ -59,35 +54,48 @@ IF(PIPOL_USER)
         SET(PIPOL_MAKE_INSTALL_COMMAND sudo make -$(MAKEFLAGS) install)
       ENDIF(NOT PIPOL_MAKE_INSTALL_COMMAND)
 
+      IF(NOT PIPOL_POST_INSTALL_COMMAND)
+        SET(PIPOL_POST_INSTALL_COMMAND sudo chown ${PIPOL_USER} install_manifest.txt) 
+      ENDIF(NOT PIPOL_POST_INSTALL_COMMAND)
+
+      IF(NOT PIPOL_PACKAGE_COMMAND)
+        SET(PIPOL_PACKAGE_COMMAND cpack -G \\\$$PIPOL_CPACK_G .)
+      ENDIF(NOT PIPOL_PACKAGE_COMMAND)
+
+      IF(NOT PIPOL_SUB_RSYNC_OPTIONS)
+        SET(PIPOL_SUB_RSYNC_OPTIONS "-aC")
+      ENDIF(NOT PIPOL_SUB_RSYNC_OPTIONS)
+
       ADD_CUSTOM_TARGET(
         ${SYSTEM_PATTERN}
         COMMENT "PIPOL Build : ${SYSTEM_PATTERN}"
-        COMMAND scp -q pipol.inria.fr:/usr/local/bin/pipol-sub . \;\\
-        COMMAND ./pipol-sub --pipol-user=${PIPOL_USER} ${SYSTEM_PATTERN} 02:00 --reconnect --group --keep --verbose=1 --export=${CMAKE_SOURCE_DIR} ${PIPOL_RC_DIR_OPTION}  
-        \"sudo mkdir -p /pipol/\${USER}/${CMAKE_BUILD_TYPE}/${PROJECT_SHORT_NAME} \;
-          sudo chown \${USER} /pipol/\${USER}/${CMAKE_BUILD_TYPE}/${PROJECT_SHORT_NAME} \;
-          cd /pipol/\${USER}/${CMAKE_BUILD_TYPE}/${PROJECT_SHORT_NAME} \;
+        COMMAND rsync ${PIPOL_USER}@pipol.inria.fr:/usr/local/bin/pipol-sub . 
+        COMMAND ./pipol-sub --pipol-user=${PIPOL_USER} ${SYSTEM_PATTERN} 02:00 --reconnect --group --keep --verbose=1 --export=${CMAKE_SOURCE_DIR} ${PIPOL_RC_DIR_OPTION} --rsynco=${PIPOL_SUB_RSYNC_OPTIONS}
+        \"sudo mkdir -p \\\$$PIPOL_WDIR/${PIPOL_USER}/${CMAKE_BUILD_TYPE}/${PROJECT_NAME} \;
+          sudo chown ${PIPOL_USER} \\\$$PIPOL_WDIR/${PIPOL_USER}/${CMAKE_BUILD_TYPE}/${PROJECT_NAME} \;
+          cd \\\$$PIPOL_WDIR/${PIPOL_USER}/${CMAKE_BUILD_TYPE}/${PROJECT_NAME} \;
           ${PIPOL_CONFIGURE_COMMAND} \;
           ${PIPOL_MAKE_COMMAND} \;
           ${PIPOL_MAKE_TEST_COMMAND} \; 
-          ${PIPOL_MAKE_INSTALL_COMMAND} \"
+          ${PIPOL_MAKE_INSTALL_COMMAND} \;
+          ${PIPOL_POST_INSTALL_COMMAND} \"
         )
 
       ADD_CUSTOM_TARGET(
         package-${SYSTEM_PATTERN}
         COMMENT "PIPOL Build : ${SYSTEM_PATTERN}"
-        COMMAND scp -q pipol.inria.fr:/usr/local/bin/pipol-sub . \;\\
+        COMMAND rsync ${PIPOL_USER}@pipol.inria.fr:/usr/local/bin/pipol-sub . \;\\
         COMMAND ./pipol-sub --pipol-user=${PIPOL_USER} ${SYSTEM_PATTERN} 02:00 --reconnect --group --keep --verbose=1 --export=${CMAKE_SOURCE_DIR} ${PIPOL_RC_DIR_OPTION}  
-        \"sudo mkdir -p /pipol/\${USER}/${CMAKE_BUILD_TYPE}/${PROJECT_SHORT_NAME} \;
-          sudo chown \${USER} /pipol/\${USER}/${CMAKE_BUILD_TYPE}/${PROJECT_SHORT_NAME} \;
-          cd /pipol/\${USER}/${CMAKE_BUILD_TYPE}/${PROJECT_SHORT_NAME} \;
+        \"sudo mkdir -p \\\$$PIPOL_WDIR/${PIPOL_USER}/${CMAKE_BUILD_TYPE}/${PROJECT_NAME} \;
+        sudo chown ${PIPOL_USER} \\\$$PIPOL_WDIR/${PIPOL_USER}/${CMAKE_BUILD_TYPE}/${PROJECT_NAME} \;
+        cd \\\$$PIPOL_WDIR/${PIPOL_USER}/${CMAKE_BUILD_TYPE}/${PROJECT_NAME} \;
           ${PIPOL_CONFIGURE_COMMAND} \;
           ${PIPOL_MAKE_COMMAND} \;
-          ${PIPOL_MAKE_TEST_COMMAND} \; 
-          ${PIPOL_MAKE_INSTALL_COMMAND} \"
-        )
+          ${PIPOL_PACKAGE_COMMAND} \" \;\\
+       COMMAND rsync -av ${PIPOL_USER}@`./pipol-sub --pipol-user=${PIPOL_USER} ${SYSTEM_PATTERN} 02:00 --reconnect --group --keep --verbose=0 echo \\\\$$PIPOL_HOST`.inrialpes.fr:`./pipol-sub --pipol-user=${PIPOL_USER} ${SYSTEM_PATTERN} 02:00 --reconnect --group --keep --verbose=0 ls \\\\$$PIPOL_WDIR/${PIPOL_USER}/${CMAKE_BUILD_TYPE}/${PROJECT_NAME}/*.\\\\$$PIPOL_PACK_EXT` .
+       )
     ENDMACRO(PIPOL_TARGET)
-  ENDIF(HAVE_SCP)
+  ENDIF(HAVE_RSYNC)
   
 ENDIF(PIPOL_USER)
 
@@ -97,6 +105,7 @@ ENDIF(PIPOL_RC_DIR)
 
 # add a target for each pipol system
 IF(PIPOL_SYSTEMS)
+  MESSAGE(STATUS "Adding Pipol targets")
   FOREACH(SYSTEM ${PIPOL_SYSTEMS})
     # target with rc-dir
     PIPOL_TARGET(${SYSTEM})
