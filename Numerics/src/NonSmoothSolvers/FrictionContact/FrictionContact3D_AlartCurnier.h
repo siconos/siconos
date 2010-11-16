02b7f0da3fd6fa5312358c4af01420c2b06ed4b8
Maurice Bremond <maurice.bremond@inria.fr>
2010-11-16 14:07:23 +0000

    missing declarations
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@2327 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Numerics/src/NonSmoothSolvers/FrictionContact/FrictionContact3D_AlartCurnier.h
+++ Numerics/src/NonSmoothSolvers/FrictionContact/FrictionContact3D_AlartCurnier.h
@@ -20,9 +20,12 @@
 #define FRICTIONCONTACT3D_AlartCurnier_H
 
 /*!\file FrictionContact3D_AlartCurnier.h
-  \brief Typedef and functions declarations related to Alart-Curnier formulation for 3 dimension frictional contact problems.
 
-  Subroutines used when the friction-contact 3D problem is written using Alart-Curnier formulation:
+  \brief Typedef and functions declarations related to Alart-Curnier
+  formulation for 3 dimension frictional contact problems.
+
+  Subroutines used when the friction-contact 3D problem is written
+  using Alart-Curnier formulation:
 
   \f{eqnarray*}
   F(reaction)=\left[\begin{array}{c}
@@ -32,10 +35,13 @@
   \end{array}\right]
   \f}
 
-  where M is an n by n  matrix, q an n-dimensional vector, reaction an n-dimensional  vector and velocity an n-dimensional vector.\n
+  where M is an n by n matrix, q an n-dimensional vector, reaction an
+  n-dimensional vector and velocity an n-dimensional vector.
 
-  We consider a "global" (ie for several contacts) problem, used to initialize the static global variables.
-  Then a "local" (ie for one contact => size = 3) problem is built (update function) and solved (solve function).
+  We consider a "global" (ie for several contacts) problem, used to
+  initialize the static global variables.  Then a "local" (ie for one
+  contact => size = 3) problem is built (update function) and solved
+  (solve function).
 
   Two different storages are available for M: dense and sparse block.
 
@@ -54,8 +60,11 @@ extern "C"
   void frictionContact3D_AC_initialize(FrictionContactProblem*, FrictionContactProblem*);
 
   /** Update friction-contact 3D problem: formalize local problem for one contact
-      \param number (position in global matrix) of the considered contact
-      \param global reaction (only the block corresponding to the current contact will be modified,
+      \param number (position in global matrix) of the considered
+      contact
+      \param global reaction (only the block corresponding to the
+      current contact will be modified,
+
       the rest is used to formalize the local problem)
   */
   void frictionContact3D_AC_update(int, FrictionContactProblem* , FrictionContactProblem* , double *, SolverOptions* options);
@@ -70,22 +79,30 @@ extern "C"
       \param size of the local problem
       \param local reaction
       \param in-out F vector
-      \param bool = 1 (true) if the problem is uptodate (ie if F or its jacobian have already been computed for the current local problem)
+      \param bool = 1 (true) if the problem is uptodate (ie if F or
+      its jacobian have already been computed for the current local
+      problem)
    */
   void F_AC(int, double *, double *, int);
 
-  /** Computes the jacobian of F function used in Newton process for Alart-Curnier formulation
+  /** Computes the jacobian of F function used in Newton process for
+   * Alart-Curnier formulation
       \param size of the local problem
       \param local reaction
       \param in-out jacobianF matrix
-      \param bool = 1 (true) if the problem is uptodate (ie if F or its jacobian have already been computed for the current local problem)
+      \param bool = 1 (true) if the problem is uptodate (ie if F or
+      its jacobian have already been computed for the current local
+      problem)
   */
   void jacobianF_AC(int, double *, double *, int);
 
-  /** Computes FGlobal function with Alart-Curnier formulation, but for all contacts (ie FGlobal = [F(contact)])
+  /** Computes FGlobal function with Alart-Curnier formulation, but
+   * for all contacts (ie FGlobal = [F(contact)])
       \param global reaction
       \param in-out FGlobal vector
-      \param bool = 1 (true) if the problem is uptodate (ie if F or its jacobian have already been computed for the current local problem)
+      \param bool = 1 (true) if the problem is uptodate (ie if F or
+      its jacobian have already been computed for the current local
+      problem)
   */
   void computeFGlobal_AC(double*, double*);
 
@@ -100,6 +117,12 @@ extern "C"
   void computeAlartCurnierCKPS(double reaction[3], double velocity[3],
                                double mu, double rho[3],
                                double result[3], double A[9], double B[9]);
+
+  int LocalNonsmoothNewtonSolver(FrictionContactProblem* localproblem,
+                                 double * R, int *iparam, double *dparam);
+
+  int DampedLocalNonsmoothNewtonSolver(FrictionContactProblem* localproblem,
+                                       double * R, int *iparam, double *dparam);
 
 #ifdef __cplusplus
 }
