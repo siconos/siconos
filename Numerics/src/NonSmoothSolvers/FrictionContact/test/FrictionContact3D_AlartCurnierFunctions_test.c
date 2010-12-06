4d7aaa9829bda81280159e9d949c6e5422b03aab
Maurice Bremond <maurice.bremond@inria.fr>
2010-12-06 12:24:16 +0000

    AlartCurnierFunctions random tests
     computeAlartCurnierSTD:
    
    #4  0x080493a4 in main () at /home/sed/bremond/src/siconos/trunk/Numerics/src/NonSmoothSolvers/FrictionContact/test/FrictionContact3D_AlartCurnierFunctions_test.c:91
    (gdb) p print3x3(A1)
             1        nan        nan
        0.1957    0.01189   -0.03922
       0.05932   -0.03922     0.1294
    $41 = void
    (gdb) 
    
    SandBox/Sympy/tests/test_friction_contact.py ok ? => numpy sets array to 0!
    
    
    
    
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@2354 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

---
+++ Numerics/src/NonSmoothSolvers/FrictionContact/test/FrictionContact3D_AlartCurnierFunctions_test.c
@@ -0,0 +1,138 @@
+#undef NDEBUG
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "op3x3.h"
+#include "NonSmoothDrivers.h"
+#include "FrictionContact3D_AlartCurnier.h"
+#include "FrictionContact3D_globalAlartCurnier.h"
+
+
+int main()
+{
+  int info = 0;
+  int r = -1;
+
+  FILE* file = fopen("./data/ACinputs.dat", "r");
+  int dim = 0;
+  double* reactions;
+  double* velocities;
+  double *mus;
+  double *rhos;
+
+  r = fscanf(file, "%d\n", &dim);
+  assert(r > 0);
+  if (r <= 0) return(r);
+
+  reactions = (double *) malloc(3 * dim * sizeof(double));
+  velocities = (double *) malloc(3 * dim * sizeof(double));
+  mus = (double *) malloc(dim * sizeof(double));
+  rhos = (double *) malloc(3 * dim * sizeof(double));
+
+  for (unsigned int i = 0; i < dim * 3 ; ++i)
+  {
+    r = fscanf(file, "%lf\n", &reactions[i]);
+    assert(r > 0);
+  };
+
+  for (unsigned int i = 0; i < dim * 3 ; ++i)
+  {
+    r = fscanf(file, "%lf\n", &velocities[i]);
+    assert(r > 0);
+  };
+
+  for (unsigned int k = 0; k < dim ; ++k)
+  {
+    r = fscanf(file, "%lf\n", &mus[k]);
+    assert(r > 0);
+  };
+
+  for (unsigned int i = 0; i < dim * 3 ; ++i)
+  {
+    r = fscanf(file, "%lf\n", &rhos[i]);
+    assert(r > 0);
+  };
+
+  double F1[3], A1[9], B1[9],
+         F2[3], A2[9], B2[9];
+  for (unsigned int k = 0; k < dim; ++k)
+  {
+
+    double* p;
+
+    p = F1;
+    OP3(*p++ = NAN);
+
+    p = F2;
+    OP3(*p++ = NAN);
+
+    p = A1;
+    OP3X3(*p++ = NAN);
+
+    p = B1;
+    OP3X3(*p++ = NAN);
+
+    p = B2;
+    OP3X3(*p++ = NAN);
+
+    p = A2;
+    OP3X3(*p++ = NAN);
+
+    computeAlartCurnierSTD(&reactions[k * 3], &velocities[k * 3], mus[k], &rhos[k * 3], F1, A1, B1);
+
+    p = F1;
+    OP3(info |= isnan(*p++));
+    assert(!info);
+
+    p = A1;
+    OP3X3(info |= isnan(*p++));
+    assert(!info);
+
+    p = B1;
+    OP3X3(info |= isnan(*p++));
+    assert(!info);
+
+
+    frictionContact3D_localAlartCurnierFunctionGenerated(&reactions[k * 3], &velocities[k * 3], mus[k], &rhos[k * 3], F2, A2, B2);
+
+    p = F1;
+    OP3(info |= isnan(*p++));
+    assert(!info);
+
+    p = A1;
+    OP3X3(info |= isnan(*p++));
+    assert(!info);
+
+    p = B1;
+    OP3X3(info |= isnan(*p++));
+    assert(!info);
+
+    sub3(F1, F2);
+    sub3x3(A1, A2);
+    sub3x3(B1, B2);
+
+#define EPS 1e-6
+    p = F2;
+    OP3(info |= !(*p++ < EPS));
+    assert(!info);
+
+    p = A2;
+    OP3X3(info |= !(*p++ < EPS));
+    assert(!info);
+
+    p = B2;
+    OP3X3(info |= !(*p++ < EPS));
+    assert(!info);
+
+  }
+
+  free(reactions);
+  free(velocities);
+  free(mus);
+  free(rhos);
+
+  return (info);
+}
+
