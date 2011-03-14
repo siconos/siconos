76613d2e896979924fac9323d2f63774da703b05
bonnefon <bonnefon@bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5>
2011-03-14 08:20:14 +0000

    A mistake: I wanted a logical or.
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@2555 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Numerics/src/NonSmoothSolvers/GenericMechanical/GMPReduced.c
+++ Numerics/src/NonSmoothSolvers/GenericMechanical/GMPReduced.c
@@ -429,7 +429,7 @@ void GMPReducedSolve(GenericMechanicalProblem* pInProblem, double *reaction , do
   int Me_size;
   int Mi_size;
   buildReducedGMP(pInProblem, Me, Mi, Qe, Qi, &Me_size, &Mi_size);
-  if ((Me_size == 0 | Mi_size == 0))
+  if ((Me_size == 0 || Mi_size == 0))
   {
     genericMechanicalProblem_GS(pInProblem, reaction, velocity, info, options);
     free(Me);
