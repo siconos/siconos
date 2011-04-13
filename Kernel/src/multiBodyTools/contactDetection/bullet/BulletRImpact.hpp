26320dd66c395f789628be2b5bb68ea05adbda41
bonnefon <bonnefon@bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5>
2011-04-13 07:53:27 +0000

    add contact without friction.
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@2606 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

---
+++ Kernel/src/multiBodyTools/contactDetection/bullet/BulletRImpact.hpp
@@ -0,0 +1,52 @@
+/* Siconos-Kernel, Copyright INRIA 2005-2010.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY, siconos-team@lists.gforge.inria.fr
+*/
+
+#ifndef BulletRImpact_hpp
+#define BulletRImpact_hpp
+
+#include "BulletSiconos.hpp"
+
+#include "NewtonEulerR.hpp"
+
+class BulletRImpact : public NewtonEulerRImpact
+{
+private:
+  SP::btManifoldPoint _contactPoints;
+
+public:
+  BulletR(SP::btManifoldPoint);
+
+  SP::btManifoldPoint contactPoints() const
+  {
+    return _contactPoints;
+  };
+
+  void computeh(double);
+
+  ACCEPT_STD_VISITORS();
+};
+
+struct ForContactPoints : public Question<SP::btManifoldPoint>
+{
+  ANSWER(BulletRImpact, contactPoints());
+};
+
+TYPEDEF_SPTR(BulletRImpact);
+
+#endif
