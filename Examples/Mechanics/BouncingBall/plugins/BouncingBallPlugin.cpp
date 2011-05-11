a665f066f78ec1693396630cca5adec6d0a7b5f6
Vincent Acary <vincent.acary@inria.fr>
2011-05-11 13:11:39 +0000

    add Bouncing BallTS with plugin and LagrangianR
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@2642 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

---
+++ Examples/Mechanics/BouncingBall/plugins/BouncingBallPlugin.cpp
@@ -0,0 +1,75 @@
+/* Siconos-sample version 3.0.0, Copyright INRIA 2005-2008.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY vincent.acary@inrialpes.fr
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+// extern double gravity;
+// extern double m1;
+// extern double l1;
+
+double gravity = 10.0;
+double m1 = 1.0;
+double m2 = 1.0 ;
+double l1 = 1.0 ;
+double l2 = 1.0 ;
+
+
+double alpha = 0.1;
+
+
+//extern "C" void mass(unsigned int sizeOfq, const double *q, double *mass, unsigned int sizeZ, double* z)
+//{
+//  mass[0]= (m1*l1);
+//}
+
+extern "C" void FInt(double time, unsigned int sizeOfq, const double *q, const double *velocity, double *fInt, unsigned int sizeZ, double* z)
+{
+  fInt[0] =  m1 * sin(q[0]) * gravity;
+}
+extern "C" void jacobianFIntq(double time, unsigned int sizeOfq, const double *q, const double *velocity, double *jacob, unsigned int sizeZ, double* z)
+{
+  jacob[0] =  cos(q[0]) * gravity * (m1);
+}
+
+extern "C" void jacobianVFInt(double time, unsigned int sizeOfq, const double *q, const double *velocity, double *jacob, unsigned int sizeZ, double* z)
+{
+  jacob[0] = 0.0;
+}
+
+// extern "C" void FExt(unsigned int  sizeOfq, double time, double *fExt, unsigned int sizeZ, double* z)
+// {
+//   unsigned int i;
+//   unsigned int n = sizeOfq;
+//   for(i = 0; i<n ; i++)
+//     fExt[i] = 0.0;
+
+// }
+
+
+extern "C" void h0(unsigned int sizeOfq, const double* q, unsigned int sizeOfY, double* y, unsigned int sizeZ, double* z)
+{
+  y[0] = q[0] + alpha * q[1];
+}
+
+extern "C" void G0(unsigned int sizeOfq, const double* q, unsigned int sizeOfY, double* G, unsigned int sizeZ, double* z)
+{
+  G[0] = 1.0;
+  G[1] = alpha;
+}
