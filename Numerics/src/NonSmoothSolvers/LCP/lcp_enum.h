ff033ba6331dd03acf8caac4e78a276ec1a769e5
Franck Perignon <franck.perignon@imag.fr>
2011-06-03 08:11:53 +0000

    Add missing header
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@2668 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

---
+++ Numerics/src/NonSmoothSolvers/LCP/lcp_enum.h
@@ -0,0 +1,25 @@
+/* Siconos-Numerics, Copyright INRIA 2005-2010.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY, siconos-team@lists.gforge.inria.fr
+ */
+#ifndef LCP_ENUM_H
+#define LCP_ENUM_H
+
+int lcp_enum_getNbIWork(LinearComplementarityProblem* problem, SolverOptions* options);
+int lcp_enum_getNbDWork(LinearComplementarityProblem* problem, SolverOptions* options);
+
+#endif //LCP_ENUM_H
