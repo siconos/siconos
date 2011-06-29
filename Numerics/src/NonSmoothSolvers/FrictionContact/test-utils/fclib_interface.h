8ac62fe1ccf2c2d59b7e6457b6377877816cf1ec
Vincent Acary <vincent.acary@inria.fr>
2011-06-29 13:57:37 +0000

    add a test which write a siconos FCD3D example into the fclib format
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@2723 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

---
+++ Numerics/src/NonSmoothSolvers/FrictionContact/test-utils/fclib_interface.h
@@ -0,0 +1,38 @@
+/* Siconos-Numerics, Copyright INRIA 2005-2010.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY, siconos-team@lists.gforge.inria.fr
+ */
+#ifndef FCLIB_INTERFACE_H
+#define FCLIB_INTERFACE_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+  int frictionContact_fclib_write(FrictionContactProblem* problem, char * title, char * description,
+                                  char * math_info,
+                                  const char *path);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
---
+++ Numerics/src/NonSmoothSolvers/FrictionContact/test/FrictionContact3D_writefclib_local_test.c
@@ -0,0 +1,49 @@
+/* Siconos-Numerics, Copyright INRIA 2005-2010.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY, siconos-team@lists.gforge.inria.fr
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include "NonSmoothDrivers.h"
+#include "fclib_interface.h"
+
+
+int main(void)
+{
+  printf("\n Start of test \n");
+  int info = 0 ;
+
+  char filename[50] = "./data/Confeti-ex03-Fc3D-SBM.dat";
+  printf("Test on %s\n", filename);
+
+  FILE * f  =  fopen(filename, "r");
+
+  FrictionContactProblem* problem = (FrictionContactProblem *)malloc(sizeof(FrictionContactProblem));
+
+  info = frictionContact_newFromFile(problem, f);
+
+  char title[50] = "Confeti-ex03-Fc3D-SBM";
+  char description[100] = " rewriting siconos test ./data/Confeti-ex03-Fc3D-SBM.dat";
+  char math_info[50] = "unknown";
+
+  frictionContact_fclib_write(problem, title, description, math_info,
+                              "./data/Confeti-ex03-Fc3D-SBM.hdf5");
+
+
+  printf("\n End of test \n");
+  return info;
+}
