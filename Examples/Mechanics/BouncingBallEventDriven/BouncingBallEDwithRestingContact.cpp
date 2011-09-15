56eb5b52bc8597fd0a1cb08c394d510d1344c5c0
Vincent Acary <vincent.acary@inria.fr>
2011-09-15 14:52:25 +0000

    Start to clean up the use of _p
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@2805 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Examples/Mechanics/BouncingBallEventDriven/BouncingBallEDwithRestingContact.cpp
+++ Examples/Mechanics/BouncingBallEventDriven/BouncingBallEDwithRestingContact.cpp
@@ -205,6 +205,7 @@ int main(int argc, char* argv[])
     if ((dataPlot - dataPlotRef).normInf() > 1e-12)
     {
       std::cout << "Warning. The results is rather different from the reference file." << std::endl;
+      std::cout << "error = " << (dataPlot - dataPlotRef).normInf() << std::endl;
       return 1;
     }
 
