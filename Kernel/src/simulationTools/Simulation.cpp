siconos/siconos

commit 0467bd21da007277dc88834b77b0194f01ba05f1

Resize the IndexSet when there is no interaction By default, the size of IndexSet is LEVELMAX (999). This was not updated when there was no interaction. Thus a lot a time was spend going through IndexSet. Now the size of IndexSet is set to 0 if there is no interaction. A simulation with no relation is now 10 times faster. There is no visible regression.
git-svn-id: svn+ssh://gforge/svn/siconos/trunk@3010 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

Kernel/src/simulationTools/Simulation.cpp
@@ -202,7 +202,6 @@ void Simulation::updateIndexSets()
   // Warning, I0 is not updated and must remain unchanged !
   unsigned int nindexsets = model()->nonSmoothDynamicalSystem()
                             ->topology()->indexSetsSize();
-
   if (nindexsets > 1)
   {
     for (unsigned int i = 1; i < nindexsets ; ++i)
@@ -228,15 +227,17 @@ void Simulation::updateInteractions()
 
   SP::InteractionsSet allInteractions =
     model()->nonSmoothDynamicalSystem()->interactions();
+  if (!allInteractions->isEmpty())
+  {
+    double time = model()->currentTime(); // init with current model time
 
-  double time = model()->currentTime(); // init with current model time
+    ComputeLevelsForInputAndOutput();
 
-  ComputeLevelsForInputAndOutput();
+    std::for_each(allInteractions->begin(), allInteractions->end(),
+                  boost::bind(&Interaction::initialize, _1, time));
 
-  std::for_each(allInteractions->begin(), allInteractions->end(),
-                boost::bind(&Interaction::initialize, _1, time));
-
-  initOSNS();
+    initOSNS();
+  }
 
 }
 
@@ -284,24 +285,30 @@ void Simulation::initialize(SP::Model m, bool withOSI)
   //  have been declared
   {
     ComputeLevelsForInputAndOutput();
-
-    if (_levelsAreComputed)
+    if (allInteractions->isEmpty())
     {
-      topo->indexSetsResize(_levelMaxForOutput + 1);
+      topo->indexSetsResize(0);
     }
     else
     {
-      topo->indexSetsResize(LEVELMAX);
-      // ComputeLevelsForInputAndOutput will resize the indexSets when some interactions appear
+      if (_levelsAreComputed)
+      {
+        topo->indexSetsResize(_levelMaxForOutput + 1);
+      }
+      else
+      {
+        topo->indexSetsResize(LEVELMAX);
+        // ComputeLevelsForInputAndOutput will resize the indexSets when some interactions appear
+      }
+
+      std::for_each(allInteractions->begin(), allInteractions->end(),
+                    boost::bind(&Interaction::initialize, _1, _tinit));
+
+      for (unsigned int i = 1; i < topo->indexSetsSize(); ++i)
+        topo->resetIndexSetPtr(i);
+      // Initialize OneStepNSProblem: in derived classes specific functions.
+      initOSNS();
     }
-
-    std::for_each(allInteractions->begin(), allInteractions->end(),
-                  boost::bind(&Interaction::initialize, _1, _tinit));
-
-    for (unsigned int i = 1; i < topo->indexSetsSize(); ++i)
-      topo->resetIndexSetPtr(i);
-    // Initialize OneStepNSProblem: in derived classes specific functions.
-    initOSNS();
   }
 
 
@@ -710,8 +717,17 @@ void Simulation::ComputeLevelsForInputAndOutput()
 {
   SP::InteractionsSet allInteractions =
     model()->nonSmoothDynamicalSystem()->interactions();
+  if (allInteractions->isEmpty())
+  {
+    _levelMinForInput = 0;
+    _levelMaxForInput = 0;
+    _levelMinForOutput = 0;
+    _levelMaxForOutput = 0;
 
-  if (not _staticLevels or not _levelsAreComputed)
+    _levelsAreComputed = true;
+    model()->nonSmoothDynamicalSystem()->topology()->indexSetsResize(_levelMaxForOutput);
+  }
+  else if (not _staticLevels or not _levelsAreComputed)
   {
     _levelMinForInput = LEVELMAX;
     _levelMaxForInput = 0;

commit 3ff02977a536599cc03fe70eb2a303b2177babda

Fix regression introduced in r3010 Now MultiBodyTest runs smoothly
git-svn-id: svn+ssh://gforge/svn/siconos/trunk@3021 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

Kernel/src/simulationTools/Simulation.cpp
@@ -227,7 +227,7 @@ void Simulation::updateInteractions()
 
   SP::InteractionsSet allInteractions =
     model()->nonSmoothDynamicalSystem()->interactions();
-  if (!allInteractions->isEmpty())
+  if (!_allNSProblems->empty())
   {
     double time = model()->currentTime(); // init with current model time
 
@@ -285,7 +285,7 @@ void Simulation::initialize(SP::Model m, bool withOSI)
   //  have been declared
   {
     ComputeLevelsForInputAndOutput();
-    if (allInteractions->isEmpty())
+    if (_allNSProblems->empty())
     {
       topo->indexSetsResize(0);
     }
@@ -717,13 +717,8 @@ void Simulation::ComputeLevelsForInputAndOutput()
 {
   SP::InteractionsSet allInteractions =
     model()->nonSmoothDynamicalSystem()->interactions();
-  if (allInteractions->isEmpty())
+  if (_allNSProblems->empty())
   {
-    _levelMinForInput = 0;
-    _levelMaxForInput = 0;
-    _levelMinForOutput = 0;
-    _levelMaxForOutput = 0;
-
     _levelsAreComputed = true;
     model()->nonSmoothDynamicalSystem()->topology()->indexSetsResize(_levelMaxForOutput);
   }