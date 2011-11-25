50e12b07eecec6378e9291018e46420a651b5550
Olivier Huber <olivier.huber@inria.fr>
2011-11-25 10:49:14 +0000

    Fix test on i386 Casting a float into an int does not yield the same result on 32bits or 64bits
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@3031 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Examples/Control/Relay/Filippov.cpp
+++ Examples/Control/Relay/Filippov.cpp
@@ -150,7 +150,7 @@ int main(int argc, char* argv[])
 
     // --- Get the values to be plotted ---
     unsigned int outputSize = 7; // number of required data
-    int N = (int)((T - t0) / h); // Number of time steps
+    int N = ceil((T - t0) / h); // Number of time steps
 
     SimpleMatrix dataPlot(N, outputSize);
 
