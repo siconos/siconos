3324c37f1e9d65bac5b734f36a05615f069c676a
Maurice Bremond <maurice.bremond@inria.fr>
2012-11-09 17:00:53 +0000

    undef debug
    git-svn-id: svn+ssh://gforge/svn/siconos/trunk@3557 bf01a9d7-0df3-0310-8ad4-b1c94d9ae2d5

--- Kernel/src/simulationTools/LinearOSNS.cpp
+++ Kernel/src/simulationTools/LinearOSNS.cpp
@@ -40,7 +40,10 @@
 
 using namespace std;
 using namespace RELATION;
-#define LINEAROSNS_DEBUG
+
+//#define LINEAROSNS_DEBUG
+
+
 LinearOSNS::LinearOSNS(): _MStorageType(0), _keepLambdaAndYState(false)
 {
 }
