0ac9d9d812549d7f3f8f47c2d7af222cee96a473
Olivier Huber <olivier.huber@inria.fr>
2013-01-25 16:39:37 +0100

    add a new python submodule: Siconos.functions
    This submodule is in pure python, no C++ bindings. It is meant to
    provide some functions that could be useful in general.

--- Front-End/src/swig/Siconos/tests/test_matrix_exp.py
+++ Front-End/src/swig/Siconos/tests/test_matrix_exp.py
@@ -21,51 +21,7 @@
 from Siconos.Kernel import *
 import numpy as np
 from scipy.linalg import expm
-
-
-def compute_phi_psi(A, B, h, TV=False):
-    # variable declaration
-    t0 = 0.0  # start time
-    T = 1  # end time
-    n, m = B.shape
-
-     # Matrix declaration
-    x0 = np.random.random(n)
-    Csurface = np.random.random((m, n))
-
-    # Declaration of the Dynamical System
-    if TV:
-        processDS = FirstOrderLinearDS(x0, A)
-    else:
-        processDS = FirstOrderLinearTIDS(x0, A)
-    # Model
-    process = Model(t0, T)
-    process.nonSmoothDynamicalSystem().insertDynamicalSystem(processDS)
-    # time discretisation
-    processTD = TimeDiscretisation(t0, h)
-    # Creation of the Simulation
-    processSimulation = TimeStepping(processTD, 0)
-    processSimulation.setName("plant simulation")
-    # Declaration of the integrator
-    processIntegrator = ZeroOrderHold(processDS)
-    processSimulation.insertIntegrator(processIntegrator)
-
-    rel = FirstOrderLinearTIR(Csurface, B)
-    idi = "interaction for control"
-    nslaw = RelayNSL(m)
-    inter = Interaction(idi, processDS, m, m, nslaw, rel)
-
-    process.nonSmoothDynamicalSystem().insertInteraction(inter, True)
-    #process.nonSmoothDynamicalSystem().link(inter, processDS)
-    # Initialization
-    process.initialize(processSimulation)
-
-    # Main loop
-    processSimulation.computeOneStep()
-    phi = processIntegrator.Phi(processDS)
-    psi = processIntegrator.Psi(processDS)
-
-    return (phi, psi)
+from functions import compute_phi_psi
 
 
 def test_TI():
