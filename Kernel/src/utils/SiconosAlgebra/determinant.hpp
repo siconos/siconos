ebac96a846386d92d13315e0ad885a7ba12ed303
Vincent Acary <vincent.acary@inria.fr>
2013-02-06 14:13:50 +0100

    Add determinant computation in SimpleMatrix

---
+++ Kernel/src/utils/SiconosAlgebra/determinant.hpp
@@ -0,0 +1,62 @@
+/* Siconos-Kernel, Copyright INRIA 2005-2012.
+ * Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ * Siconos is a free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Siconos is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Siconos; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Contact: Vincent ACARY, siconos-team@lists.gforge.inria.fr
+ */
+
+#ifndef __determinant__
+#define __determinant__
+
+
+#include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <boost/numeric/ublas/lu.hpp>
+
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <boost/numeric/ublas/lu.hpp>
+
+
+namespace ublas = boost::numeric::ublas;
+
+
+template<class matrix_T>
+double determinant(ublas::matrix_expression<matrix_T> const& mat_r)
+{
+  double det = 1.0;
+
+  matrix_T mLu(mat_r() );
+  ublas::permutation_matrix<std::size_t> pivots(mat_r().size1() );
+
+  int is_singular = lu_factorize(mLu, pivots);
+
+  if (!is_singular)
+  {
+    for (std::size_t i=0; i < pivots.size(); ++i)
+    {
+      if (pivots(i) != i)
+        det *= -1.0;
+
+      det *= mLu(i,i);
+    }
+  }
+  else
+    det = 0.0;
+
+  return det;
+}
+
+
+#endif
