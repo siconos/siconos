1edb975561a6723809f6de0737390cffead991bd
Maurice Bremond <Maurice.Bremond@inria.fr>
2013-08-28 10:45:00 +0200

    compilation with bullet

--- IO/src/MechanicsIO.cpp
+++ IO/src/MechanicsIO.cpp
@@ -56,27 +56,22 @@ struct ContactPointVisitor : public SiconosVisitor
   void visit(const BulletR& rel)
   {
     answer.resize(9);
-    btPersistentManifold& cm = *rel.contactManifold();
-    if (cm.getNumContacts() > rel.contactNum())
-    {
-      btManifoldPoint& cp = cm.getContactPoint(rel.contactNum());
-      const btVector3& posa = cp.getPositionWorldOnA();
-      const SiconosVector& nc = *rel.nc();
-      const SimpleMatrix& jachqT = *rel.jachqT();
-      SiconosVector cf(7);
-      prod(*inter.lambda(1), jachqT, cf, true);
-      const btPersistentManifold& cm = *rel.contactManifold();
-      answer.setValue(0, posa[0]);
-      answer.setValue(1, posa[1]);
-      answer.setValue(2, posa[2]);
-      answer.setValue(3, nc(0));
-      answer.setValue(4, nc(1));
-      answer.setValue(5, nc(2));
-      answer.setValue(6, cf(0));
-      answer.setValue(7, cf(1));
-      answer.setValue(8, cf(2));
-    }
-  };
+    btManifoldPoint& cp = *rel.contactPoint();
+    const btVector3& posa = cp.getPositionWorldOnA();
+    const SiconosVector& nc = *rel.nc();
+    const SimpleMatrix& jachqT = *rel.jachqT();
+    SiconosVector cf(7);
+    prod(*inter.lambda(1), jachqT, cf, true);
+    answer.setValue(0, posa[0]);
+    answer.setValue(1, posa[1]);
+    answer.setValue(2, posa[2]);
+    answer.setValue(3, nc(0));
+    answer.setValue(4, nc(1));
+    answer.setValue(5, nc(2));
+    answer.setValue(6, cf(0));
+    answer.setValue(7, cf(1));
+    answer.setValue(8, cf(2));
+  }
 #endif
 };
 
