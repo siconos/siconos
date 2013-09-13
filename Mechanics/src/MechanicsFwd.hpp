18ba4b3a151c0272f4000ef16273151839f09241
Maurice Bremond <Maurice.Bremond@inria.fr>
2013-09-13 16:03:54 +0200

    missing file

---
+++ Mechanics/src/MechanicsFwd.hpp
@@ -0,0 +1,23 @@
+
+#ifndef MechanicsFwd_hpp
+#define MechanicsFwd_hpp
+#include <SiconosPointers.hpp>
+DEFINE_SPTR(SpaceFilter);
+DEFINE_SPTR(SiconosBodies);
+DEFINE_SPTR(ExternalBody);
+DEFINE_SPTR(Disk);
+DEFINE_SPTR(Circle);
+DEFINE_SPTR(SphereNEDS);
+DEFINE_SPTR(SphereLDS);
+DEFINE_SPTR(CircleCircleR);
+DEFINE_SPTR(DiskDiskR);
+DEFINE_SPTR(DiskPlanR);
+DEFINE_SPTR(DiskMovingPlanR);
+DEFINE_SPTR(CircularDS);
+DEFINE_SPTR(CircularR);
+DEFINE_SPTR(SphereNEDSPlanR);
+DEFINE_SPTR(SphereNEDSSphereNEDSR);
+DEFINE_SPTR(SphereLDSPlanR);
+DEFINE_SPTR(SphereLDSSphereLDSR);
+DEFINE_SPTR(FMatrix);
+#endif
