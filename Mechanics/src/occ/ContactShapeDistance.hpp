9118c48d437c59250ac89be0df700330961aa5ba
Maurice Bremond <Maurice.Bremond@inria.fr>
2014-09-04 09:49:20 +0200

    [Mechanics] ContactShapeDistance

---
+++ Mechanics/src/occ/ContactShapeDistance.hpp
@@ -0,0 +1,21 @@
+#ifndef ContactShapeDistance_hpp
+#define ContactShapeDistance_hpp
+
+struct ContactShapeDistance
+{
+  double value;
+
+  double x1;
+  double y1;
+  double z1;
+
+  double x2;
+  double y2;
+  double z2;
+
+  double nx;
+  double ny;
+  double nz;
+};
+
+#endif
