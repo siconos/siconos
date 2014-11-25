7923cb35c32f2ff55b0c60660a78e59a2e421061
Maurice Bremond <Maurice.Bremond@inria.fr>
2014-11-25 13:07:59 +0100

    [Examples] BulletIO : insert -> add

--- Examples/Mechanics/ContactDetection/BulletIO/misc.py
+++ Examples/Mechanics/ContactDetection/BulletIO/misc.py
@@ -12,29 +12,29 @@ from Siconos.Mechanics import IO
 with IO.Hdf5(mode='w') as io:
 
     # Definition of two boxes shape
-    io.insertPrimitiveShape('BigBox', 'Box', (3, 5, 2))
+    io.addPrimitiveShape('BigBox', 'Box', (3, 5, 2))
 
-    io.insertPrimitiveShape('LongBox', 'Box', (1, 1, 5))
+    io.addPrimitiveShape('LongBox', 'Box', (1, 1, 5))
 
     # Definition of the ground shape
-    io.insertPrimitiveShape('Ground', 'Box', (100, 100, .5))
+    io.addPrimitiveShape('Ground', 'Box', (100, 100, .5))
 
     # Definition of a non smooth law between groups 0 and 1
-    io.insertNewtonImpactFrictionNSL('contact1', mu=0.1,
+    io.addNewtonImpactFrictionNSL('contact1', mu=0.1,
                                      collision_group1=0, collision_group2=1)
 
     # Definition of a non smooth law between groups 1 and 1
-    io.insertNewtonImpactFrictionNSL('contact2', mu=0.7,
+    io.addNewtonImpactFrictionNSL('contact2', mu=0.7,
                                      collision_group1=1, collision_group2=1)
 
     # Definition of a non smooth law between groups 0 and 0
-    io.insertNewtonImpactFrictionNSL('contact3', mu=0.1,
+    io.addNewtonImpactFrictionNSL('contact3', mu=0.1,
                                      collision_group1=0, collision_group2=0)
 
     # A 'two boxes object made with two Contactors.
     # As a mass is given, it is a dynamic system involved in contact
     # detection and in the simulation.
-    io.insertObject('twoboxes', [Contactor('BigBox',
+    io.addObject('twoboxes', [Contactor('BigBox',
                                            collision_group=0,
                                            relative_position=[0, 0, 0]),
                                  Contactor('LongBox', collision_group=1,
@@ -46,7 +46,7 @@ with IO.Hdf5(mode='w') as io:
     # the ground object made with the ground shape. As the mass is
     # not given, it is a static object only involved for contact
     # detection.
-    io.insertObject('ground', [Contactor('Ground', collision_group=1)],
+    io.addObject('ground', [Contactor('Ground', collision_group=1)],
                     position=[0, 0, 0])
 
 
