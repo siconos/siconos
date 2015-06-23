639f6bd03e005ffed5d464bff2577634c98b7be1
Maurice Bremond <Maurice.Bremond@inria.fr>
2015-06-23 15:29:45 +0200

    [Kernel] supression of compiler warnings in TestPlugin

--- Kernel/src/plugin/test/TestPlugin.cpp
+++ Kernel/src/plugin/test/TestPlugin.cpp
@@ -26,12 +26,14 @@
 
 // ==== Dynamical System ====
 
+extern "C" DLLEXPORT void computef(double time, unsigned int sizeOfX, double* x, double* f, unsigned int sizeZ, double* z);
 extern "C" DLLEXPORT void computef(double time, unsigned int sizeOfX, double* x, double* f, unsigned int sizeZ, double* z)
 {
   for (unsigned int i = 0; i < sizeOfX; i++)
     f[i] = time * x[i];
 }
 
+extern "C" DLLEXPORT void computeJacobianfx(double time, unsigned int sizeOfX, double* x, double* jacob, unsigned int sizeZ, double* z);
 extern "C" DLLEXPORT void computeJacobianfx(double time, unsigned int sizeOfX, double* x, double* jacob, unsigned int sizeZ, double* z)
 {
   for (unsigned int i = 0; i < sizeOfX * sizeOfX; i++)
@@ -40,6 +42,7 @@ extern "C" DLLEXPORT void computeJacobianfx(double time, unsigned int sizeOfX, d
 
 // ===== Lagrangian DS  =====
 
+extern "C" DLLEXPORT void computeMass(unsigned int sizeOfq, double *q, double *mass, unsigned int sizeZ, double* z);
 extern "C" DLLEXPORT void computeMass(unsigned int sizeOfq, double *q, double *mass, unsigned int sizeZ, double* z)
 {
   for (unsigned int i = 0; i < (sizeOfq * sizeOfq); ++i)
@@ -49,42 +52,49 @@ extern "C" DLLEXPORT void computeMass(unsigned int sizeOfq, double *q, double *m
   mass[8] = 3;
 }
 
+extern "C" DLLEXPORT void computeFInt(double time, unsigned int sizeOfq, double *q, double *velocity, double *fInt, unsigned int sizeZ, double * z);
 extern "C" DLLEXPORT void computeFInt(double time, unsigned int sizeOfq, double *q, double *velocity, double *fInt, unsigned int sizeZ, double * z)
 {
   for (unsigned int i = 0; i < sizeOfq; ++i)
     fInt[i] = i * q[i];
 }
 
+extern "C" DLLEXPORT void computeFExt(double time, unsigned int sizeOfq, double *fExt, unsigned int sizeOfZ, double *z);
 extern "C" DLLEXPORT void computeFExt(double time, unsigned int sizeOfq, double *fExt, unsigned int sizeOfZ, double *z)
 {
   for (unsigned int i = 0; i < sizeOfq; ++i)
     fExt[i] = i * time;
 }
 
+extern "C" DLLEXPORT void computeFGyr(unsigned int sizeOfq, double *q, double *velocity, double *FGyr, unsigned int sizeOfZ, double *z);
 extern "C" DLLEXPORT void computeFGyr(unsigned int sizeOfq, double *q, double *velocity, double *FGyr, unsigned int sizeOfZ, double *z)
 {
   for (unsigned int i = 0; i < sizeOfq; ++i)
     FGyr[i] = i * q[i];
 }
 
+extern "C" DLLEXPORT void computeJacobianFIntq(double time, unsigned int sizeOfq, double *q, double *velocity, double *jacob, unsigned int sizeOfZ, double* z);
 extern "C" DLLEXPORT void computeJacobianFIntq(double time, unsigned int sizeOfq, double *q, double *velocity, double *jacob, unsigned int sizeOfZ, double* z)
 {
   for (unsigned int i = 0; i < (sizeOfq * sizeOfq); ++i)
     jacob[i] = i * q[0];
 }
 
+extern "C" DLLEXPORT void computeJacobianFintVelocity(double time, unsigned int sizeOfq, double *q, double *velocity, double *jacob, unsigned int sizeOfZ, double* z);
 extern "C" DLLEXPORT void computeJacobianFintVelocity(double time, unsigned int sizeOfq, double *q, double *velocity, double *jacob, unsigned int sizeOfZ, double* z)
 {
   for (unsigned int i = 0; i < (sizeOfq * sizeOfq); ++i)
     jacob[i] = i * q[0];
 }
 
+extern "C" DLLEXPORT void computeJacobianFGyrq(unsigned int sizeOfq, double *q, double *velocity, double *jacob, unsigned int sizeOfZ, double* z);
 extern "C" DLLEXPORT void computeJacobianFGyrq(unsigned int sizeOfq, double *q, double *velocity, double *jacob, unsigned int sizeOfZ, double* z)
 {
   for (unsigned int i = 0; i < (sizeOfq * sizeOfq); ++i)
     jacob[i] = i * q[0];
 }
 
+extern "C" DLLEXPORT void computeJacobianFGyrVelocity(unsigned int sizeOfq, double *q, double *velocity, double *jacob, unsigned int sizeOfZ, double* z);
 extern "C" DLLEXPORT void computeJacobianFGyrVelocity(unsigned int sizeOfq, double *q, double *velocity, double *jacob, unsigned int sizeOfZ, double* z)
 {
   for (unsigned int i = 0; i < (sizeOfq * sizeOfq); ++i)
@@ -94,12 +104,14 @@ extern "C" DLLEXPORT void computeJacobianFGyrVelocity(unsigned int sizeOfq, doub
 
 //==================  FirstOrderLinearDS ==================
 
+extern "C" DLLEXPORT void computeb(double time, unsigned int sizeOfB, double* b, unsigned int sizeOfZ, double *z);
 extern "C" DLLEXPORT void computeb(double time, unsigned int sizeOfB, double* b, unsigned int sizeOfZ, double *z)
 {
   for (unsigned int i = 0; i < sizeOfB; i++)
     b[i] = time * i ;
 
 }
+extern "C" DLLEXPORT void computeA(double time, unsigned int rowA, unsigned int colA, double* A, unsigned int sizeOfZ, double *z);
 extern "C" DLLEXPORT void computeA(double time, unsigned int rowA, unsigned int colA, double* A, unsigned int sizeOfZ, double *z)
 {
   for (unsigned int j = 0; j < rowA; j++)
@@ -110,11 +122,13 @@ extern "C" DLLEXPORT void computeA(double time, unsigned int rowA, unsigned int 
 }
 
 //==================  LagrangianScleronomousR ==================
+extern "C" DLLEXPORT void hSclero(unsigned int sizeDS, double* q, unsigned int sizeY, double* y, unsigned int sizeZ, double* z);
 extern "C" DLLEXPORT void hSclero(unsigned int sizeDS, double* q, unsigned int sizeY, double* y, unsigned int sizeZ, double* z)
 {
   printf("Call of the function 'hSclero' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void G0Sclero(unsigned int sizeDS, double* q, unsigned int sizeY, double* G0, unsigned int sizeZ, double* z);
 extern "C" DLLEXPORT void G0Sclero(unsigned int sizeDS, double* q, unsigned int sizeY, double* G0, unsigned int sizeZ, double* z)
 {
   printf("Call of the function 'G0' of the test plugin.\n");
@@ -122,16 +136,19 @@ extern "C" DLLEXPORT void G0Sclero(unsigned int sizeDS, double* q, unsigned int 
 
 //==================  LagrangianRheonomousR ==================
 
+extern "C" DLLEXPORT void hRheo(unsigned int, double*, double, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void hRheo(unsigned int, double*, double, unsigned int, double*, unsigned int, double*)
 {
   printf("Call of the function 'hRheo' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void G0Rheo(unsigned int, double*, double, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void G0Rheo(unsigned int, double*, double, unsigned int, double*, unsigned int, double*)
 {
   printf("Call of the function 'G0Rheo' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void hDot(unsigned int, double*, double, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void hDot(unsigned int, double*, double, unsigned int, double*, unsigned int, double*)
 {
   printf("Call of the function 'hDot' of the test plugin.\n");
@@ -139,16 +156,19 @@ extern "C" DLLEXPORT void hDot(unsigned int, double*, double, unsigned int, doub
 
 //==================  LagrangianCompliantR ==================
 
+extern "C" DLLEXPORT void hCompl(unsigned int, double*, unsigned int, double*, double*, unsigned int, double*);
 extern "C" DLLEXPORT void hCompl(unsigned int, double*, unsigned int, double*, double*, unsigned int, double*)
 {
   printf("Call of the function 'hCompl' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void G0Compl(unsigned int, double*, unsigned int, double*, double*, unsigned int, double*);
 extern "C" DLLEXPORT void G0Compl(unsigned int, double*, unsigned int, double*, double*, unsigned int, double*)
 {
   printf("Call of the function 'G0Compl' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void G1Compl(unsigned int, double*, unsigned int, double*, double*, unsigned int, double*);
 extern "C" DLLEXPORT void G1Compl(unsigned int, double*, unsigned int, double*, double*, unsigned int, double*)
 {
   printf("Call of the function 'G1Compl' of the test plugin.\n");
@@ -156,21 +176,25 @@ extern "C" DLLEXPORT void G1Compl(unsigned int, double*, unsigned int, double*, 
 
 // ========== FirstOrderType1R ==========
 
+extern "C" DLLEXPORT void hT1(unsigned int, double*, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void hT1(unsigned int, double*, unsigned int, double*, unsigned int, double*)
 {
   printf("Warning: call of the function 'hT1' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void gT1(unsigned int, double*, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void gT1(unsigned int, double*, unsigned int, double*, unsigned int, double*)
 {
   printf("Warning: call of the function 'gT1' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void Jh0T1(unsigned int, double*, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void Jh0T1(unsigned int, double*, unsigned int, double*, unsigned int, double*)
 {
   printf("Warning: call of the function 'Jh0T1' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void Jg0T1(unsigned int, double*, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void Jg0T1(unsigned int, double*, unsigned int, double*, unsigned int, double*)
 {
   printf("Warning: call of the function 'Jg0T1' of the test plugin.\n");
@@ -178,26 +202,31 @@ extern "C" DLLEXPORT void Jg0T1(unsigned int, double*, unsigned int, double*, un
 
 //==================  FirstOrderLinearR ==================
 
+extern "C" DLLEXPORT void C(double, unsigned int, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void C(double, unsigned int, unsigned int, double*, unsigned int, double*)
 {
   printf("Warning: call of the function 'C' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void D(double, unsigned int, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void D(double, unsigned int, unsigned int, double*, unsigned int, double*)
 {
   printf("Warning: call of the function 'D' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void F(double, unsigned int, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void F(double, unsigned int, unsigned int, double*, unsigned int, double*)
 {
   printf("Warning: call of the function 'F' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void e(double, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void e(double, unsigned int, double*, unsigned int, double*)
 {
   printf("Warning: call of the function 'e' of the test plugin.\n");
 }
 
+extern "C" DLLEXPORT void B(double, unsigned int, unsigned int, double*, unsigned int, double*);
 extern "C" DLLEXPORT void B(double, unsigned int, unsigned int, double*, unsigned int, double*)
 {
   printf("Warning: call of the function 'B' of the test plugin.\n");
