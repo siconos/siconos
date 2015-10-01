d6f134a84e37111ff91f93aab7dd76aa1ab04680
Franck Pérignon <franck.perignon@imag.fr>
2015-10-01 23:35:51 +0200

    cmake review - Step 2:Kernel

---
+++ Kernel/kernel_tests.cmake
@@ -0,0 +1,85 @@
+include(tools4tests)
+
+set(TEST_WRAP FALSE)
+
+if(WITH_${COMPONENT}_TESTING)
+  
+  # We don't use COMPILE_WITH since we don't want to link cppunit with the
+  # kernel library
+  find_package(CppUnit REQUIRED)
+  set(TEST_LIBS ${TEST_LIBS} ${CPPUNIT_LIBRARIES})
+  set(TEST_INCLUDE_DIR ${TEST_INCLUDE_DIR} ${CPPUNIT_INCLUDE_DIR})
+
+  # Plugin library for tests
+  file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/src/plugin/test)
+  # MODULE rather than SHARED for Macosx compatibility. 
+  add_library(TestPlugin MODULE src/plugin/test/TestPlugin.cpp)
+  set_target_properties(TestPlugin 
+    PROPERTIES PREFIX ""
+    OUTPUT_NAME TestPlugin)
+
+  # the main test driver
+  SET(TEST_MAIN src/model/test/TestMain.cpp)
+
+  # For Windows
+  SET(PATH_FOR_PLUGIN
+   ".\;${CMAKE_CURRENT_BINARY_DIR}/src/plugin\;${CMAKE_CURRENT_BINARY_DIR}/src/plugin/test")
+
+ # Siconos Algebra
+  BEGIN_TEST(src/utils/SiconosAlgebra/test)
+
+  NEW_TEST(testSiconosAlgebra
+    BlockMatrixTest.cpp  SimpleMatrixTest.cpp BlockVectorTest.cpp  SiconosVectorTest.cpp EigenProblemsTest.cpp AlgebraToolsTest.cpp)
+  END_TEST()
+  
+  # Siconos Memory
+  BEGIN_TEST(src/utils/SiconosMemory/test)
+  
+  NEW_TEST(testSiconosMemory SiconosMemoryTest.cpp)
+  END_TEST()
+  
+  # model
+  BEGIN_TEST(src/model/test)
+  
+  NEW_TEST(testModel ModelTest.cpp)
+  
+  END_TEST()
+  # modeling tools 
+  BEGIN_TEST(src/modelingTools/test)
+  
+  NEW_TEST(testModelingTools FirstOrderLinearDSTest.cpp FirstOrderLinearTIRTest.cpp FirstOrderLinearRTest.cpp 
+    FirstOrderType1RTest.cpp 
+    LagrangianLinearTIRTest.cpp LagrangianScleronomousRTest.cpp LagrangianRheonomousRTest.cpp 
+    LagrangianCompliantRTest.cpp)
+  END_TEST()
+  #FirstOrderNonLinearDSTest.cpp FirstOrderLinearDSTest.cpp 
+  #LagrangianDSTest.cpp LagrangianLinearTIDSTest.cpp TestMain.cpp)
+
+  # Global tests
+  BEGIN_TEST(src/global_tests)
+
+  ADD_LIBRARY(gTestPlugin MODULE src/global_tests/gTestPlugin.cpp)
+  SET_TARGET_PROPERTIES(gTestPlugin 
+    PROPERTIES PREFIX "" 
+    OUTPUT_NAME src/global_tests/gTestPlugin)
+  
+  NEW_TEST(testGlobal
+    DiodeBridge.cpp FunctionTest.cpp  GlobalTests.cpp)
+  
+  END_TEST()
+  
+  BEGIN_TEST(src/utils/SiconosTools/test)
+
+  NEW_TEST(testSiconosTools SiconosGraphTest.cpp SiconosVisitorTest.cpp SiconosPropertiesTest.cpp)
+
+  END_TEST()
+
+  # Simulation tests
+  BEGIN_TEST(src/simulationTools/test)
+
+  NEW_TEST(testSimulationTools ZOHTest.cpp OSNSPTest.cpp)
+
+
+  END_TEST()
+
+endif()
