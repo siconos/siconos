54a909335c09c19a3386d50be01b9e25243b9f92
Maurice Bremond <Maurice.Bremond@inria.fr>
2015-10-13 21:39:12 +0200

    [CI] no python => no xml

--- CI/no_python.cmake
+++ CI/no_python.cmake
@@ -1,5 +1,6 @@
 include(CI/default.cmake)
 set(WITH_PYTHON_WRAPPER OFF CACHE BOOL "Python" FORCE)
+set(WITH_XML OFF CACHE BOOL "Xml" FORCE)
 foreach(component "numerics;kernel;control;mechanics;io")
   set(WITH_${component}_PYTHON_WRAPPER OFF CACHE BOOL "Python" FORCE)
 endforeach()
