5a216f052f71a3ddb443011d85e6a81bd190c40f
Maurice Bremond <Maurice.Bremond@inria.fr>
2015-10-16 13:09:26 +0200

    [CI] add openblas_lapacke & clang tasks + fix issue in Docker.cmake with hostname length

--- CI/tasks.py
+++ CI/tasks.py
@@ -2,6 +2,12 @@ from machinery.ci_task import CiTask
 
 siconos_default = CiTask()
 
+siconos_openblas_lapacke = CiTask(pkgs=['build-base','gcc', 'gfortran', 'g++',
+                                        'openblas-lapacke'])
+
+siconos_clang = CiTask(pkgs=['build-base','clang', 'gfortran',
+                             'atlas-lapack'])
+
 siconos_serialization = CiTask(ci_config='with_serialization',
                                pkgs = siconos_default._pkgs + ['serialization'])
 siconos_debian_latest = CiTask(distrib='debian:latest')
@@ -10,7 +16,7 @@ siconos_with_mumps = CiTask(pkgs = siconos_default._pkgs + ['mumps'],
                             ci_config='with_mumps')
 
 # dispatch based on hostname
-known_tasks = {'fedora18-x86-64': [siconos_default,
-                                   siconos_with_mumps,
-                                   siconos_serialization],
-               'coqhardi': [siconos_serialization]}
+known_tasks = {'fedora18-x86-64': [siconos_with_mumps,
+                                   siconos_openblas_lapacke,
+                                   siconos_clang,
+                                   siconos_serialization]}
