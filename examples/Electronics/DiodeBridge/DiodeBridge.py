bd268c1d71502577eabfd09b644a9abfdc9f996c
Vincent Acary <vincent.acary@inria.fr>
2015-11-27 10:34:21 -0300

    [examples] correct Typos

--- examples/Electronics/DiodeBridge/DiodeBridge.py
+++ examples/Electronics/DiodeBridge/DiodeBridge.py
@@ -119,7 +119,7 @@ DiodeBridge.nonSmoothDynamicalSystem().link(InterDiodeBridge, LSDiodeBridge)
 # (1) OneStepIntegrators
 theta = 0.5
 aOSI = EulerMoreauOSI(theta)
- aOSI.insertDynamicalSystem(LSDiodeBridge)
+aOSI.insertDynamicalSystem(LSDiodeBridge)
 # (2) Time discretisation
 aTiDisc = TimeDiscretisation(t0, h_step)
 
