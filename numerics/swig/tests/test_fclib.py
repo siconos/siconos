84c17a6cfe2774aa42e3e1f84105d406aa7d6e0b
Olivier Huber <ohuber2@wisc.edu>
2016-03-05 19:27:25 -0600

    [numerics/py] add test using fclib files

---
+++ numerics/swig/tests/test_fclib.py
@@ -0,0 +1,111 @@
+import siconos.numerics as sn
+import os.path
+import scipy.sparse
+import scipy.sparse.linalg
+import numpy as np
+from siconos.tests_setup import working_dir
+
+
+# this could be changed at install time
+data_dir = working_dir + '/data/'
+# solvers = [sn.SICONOS_GLOBAL_FRICTION_3D_NSGS, sn.SICONOS_GLOBAL_FRICTION_3D_NSN_AC]
+solvers = (sn.SICONOS_GLOBAL_FRICTION_3D_NSGS,)
+solvers_reduced1 = (sn.SICONOS_FRICTION_3D_NSGS, sn.SICONOS_FRICTION_3D_NSN_AC)
+solvers_reduced2 = (sn.SICONOS_FRICTION_3D_NSN_AC,)  # sn.SICONOS_FRICTION_3D_NSN_FB)
+
+no = sn.NumericsOptions()
+no.verboseMode = 0
+
+
+def condensed_from_global(fcp):
+    # spsolve expect the indices to be cint aka 32 bits int
+    # Hence, we do some magic to cirumvent those issues.
+    Mcoo = scipy.sparse.coo_matrix(fcp.M)
+    Mcsc = scipy.sparse.csc_matrix(Mcoo)
+    Hcoo = scipy.sparse.coo_matrix(fcp.H)
+    Hcsc = scipy.sparse.csc_matrix(Hcoo)
+    WW = Hcsc.T.dot(scipy.sparse.linalg.spsolve(Mcsc, Hcsc))
+    qprime = fcp.H.T.dot(scipy.sparse.linalg.spsolve(Mcsc, fcp.q))
+
+    fcp_reduced = sn.FrictionContactProblem()
+    fcp_reduced.dimension = fcp.dimension
+    fcp_reduced.numberOfContacts = fcp.numberOfContacts
+
+    # this is a hack to deal with the inability of fc3d solvers to work with
+    # sparse matrices
+    _, Wsbm = sn.sparseToSBM(3, WW)
+    fcp_reduced.M = Wsbm
+    fcp_reduced.mu = fcp.mu
+    fcp_reduced.q = fcp.b + qprime
+    return fcp_reduced
+
+
+def solve_reduced(fcp, solver_reduced):
+    SO_reduced = sn.SolverOptions(solver_reduced)
+    SO_reduced.dparam[0] = np.sqrt(fcp.numberOfContacts)*1e-9
+    size_reaction = fcp.numberOfContacts * 3
+    reaction_reduced = np.zeros((size_reaction,))
+    velocities_reduced = np.zeros((size_reaction,))
+
+    return sn.fc3d_driver(fcp, reaction_reduced, velocities_reduced, SO_reduced, no)
+
+
+def solve_global(fcp, solver):
+    SO = sn.SolverOptions(solver)
+    SO.dparam[0] = np.sqrt(fcp.numberOfContacts)*1e-9
+
+    n, m = fcp.H.shape
+    size_reaction = m
+    reaction = np.zeros((size_reaction,))
+    velocities = np.zeros((size_reaction,))
+    global_velocities = np.zeros((n,))
+    return sn.gfc3d_driver(fcp, reaction, velocities, global_velocities, SO, no)
+
+
+def test_gfc3d():
+    data_files = ('CubeH8.hdf5', 'LMGC_GlobalFrictionContactProblem00046.hdf5')
+    mark_as_failed = False
+
+    for d in data_files:
+        full_path = data_dir + d
+        if os.path.isfile(full_path):
+
+            fcp = sn.globalFrictionContact_fclib_read(full_path)
+            for s in solvers:
+                res = solve_global(fcp, s)
+                if res:
+                    print('Solver {:} on problem {:} failed with info = {:}'.format(sn.idToName(s), d, res))
+                    mark_as_failed = True
+                else:
+                    print('Solver {:} on problem {:} is ok'.format(sn.idToName(s), d))
+
+            fcp_reduced = condensed_from_global(fcp)
+            for s in solvers_reduced1:
+                res = solve_reduced(fcp_reduced, s)
+                if res:
+                    print('Solver {:} on problem {:} in reduced form failed with info = {:}'.format(sn.idToName(s), d, res))
+                    mark_as_failed = True
+                else:
+                    print('Solver {:} on problem {:} is ok'.format(sn.idToName(s), d))
+
+    assert mark_as_failed is False
+
+
+def test_fc3d():
+    data_files = ['Capsules-i125-1213.hdf5']
+    mark_as_failed = False
+
+    for d in data_files:
+        full_path = data_dir + d
+        if os.path.isfile(full_path):
+
+            fcp = sn.frictionContact_fclib_read(full_path)
+            for s in solvers_reduced2:
+                res = solve_reduced(fcp, s)
+                if res:
+                    print('Solver {:} on problem {:} failed with info = {:}'.format(sn.idToName(s), d, res))
+                    mark_as_failed = True
+                else:
+                    print('Solver {:} on problem {:} is ok'.format(sn.idToName(s), d))
+
+    assert mark_as_failed is False
