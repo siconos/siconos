4fe5bf94194297e64522e7214a081497d756d421
Vincent Acary <vincent.acary@inria.fr>
2016-03-14 14:48:36 -0300

    [io] add a simple simulation from a hdf5 file that contains a scene

---
+++ examples/Mechanics/ContactDetection/BulletIO/cube_from_HDFfile.py
@@ -0,0 +1,12 @@
+
+from siconos.io.mechanics_io import Hdf5
+# Run the simulation from the inputs previously defined and add
+# results to the hdf5 file. The visualisation of the output may be done
+# with the vview command.
+with Hdf5(mode='r+',io_filename='cube_scene.hdf5') as io:
+
+    # By default earth gravity is applied and the units are those
+    # of the International System of Units.
+    # Because of fixed collision margins used in the collision detection,
+    # sizes of small objects may need to be expressed in cm or mm.
+    io.run(output_frequency=100)
