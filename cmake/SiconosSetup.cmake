91989a8cd026d9b26e3fc532c5568cf6f3ff95ff
Olivier Huber <ohuber2@wisc.edu>
2016-03-16 20:08:56 -0500

    [cmake] bring back code for non-APPLE systems
    The previous code was working fine for all non-apple platform. Without
    this, the lib are missing many symbols on linux

--- cmake/SiconosSetup.cmake
+++ cmake/SiconosSetup.cmake
@@ -181,7 +181,26 @@ if(APPLE)
   if("${isSystemDir}" STREQUAL "-1")
      set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib")
   endif("${isSystemDir}" STREQUAL "-1")
-endif()
+else(APPLE)
+  # =========== RPATH stuff ===========
+  # do not skip the full RPATH for the build tree
+  if(FORCE_SKIP_RPATH)
+    set(CMAKE_SKIP_BUILD_RPATH TRUE)
+  else(FORCE_SKIP_RPATH)
+    set(CMAKE_SKIP_BUILD_RPATH FALSE)
+  endif(FORCE_SKIP_RPATH)
+
+  # when building, don't use the install RPATH already
+  # (but later on when installing)
+  set(CMAKE_BUILD_WITH_INSTALL_RPATH FALSE) 
+
+  # the RPATH to be used when installing
+  set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
+
+  # don't add the automatically determined parts of the RPATH
+  # which point to directories outside the build tree to the install RPATH
+  set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+endif(APPLE)
 
 # init all common options for enabled components
 set(common_options DOCUMENTATION TESTING UNSTABLE PYTHON_WRAPPER
