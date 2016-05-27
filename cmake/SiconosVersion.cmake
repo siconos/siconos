9d6ea4f119a15f7b47e89f46ec567ef5c5c70d94
Maurice Bremond <Maurice.Bremond@inria.fr>
2016-05-27 16:48:01 +0200

    [siconos] 4.0.0

--- cmake/SiconosVersion.cmake
+++ cmake/SiconosVersion.cmake
@@ -1,6 +1,6 @@
 # --- set siconos current version ---
 # This file is also required for examples.
-set(MAJOR_VERSION 3)
-set(MINOR_VERSION 9)
+set(MAJOR_VERSION 4)
+set(MINOR_VERSION 0)
 set(PATCH_VERSION 0)
 set(SICONOS_VERSION "${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}")
