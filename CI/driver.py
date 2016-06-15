3fa2c4d5d4982c84822abc511f92894b5217826a
Maurice Bremond <Maurice.Bremond@inria.fr>
2016-06-15 18:25:09 +0200

    [CI] add --run and --print={script,docker,vagrant} options for driver.py + rename --show-tasks in --list-tasks
         --run mandatory for action
         examples for --print:
         ./driver.py --task=siconos_ubuntu_16_04 --print=docker

--- CI/driver.py
+++ CI/driver.py
@@ -3,7 +3,7 @@
 import sys
 from socket import gethostname
 
-from tasks import siconos_default, known_tasks
+from tasks import default, known_tasks, database
 from subprocess import check_output, check_call
 
 from getopt import gnu_getopt, GetoptError
@@ -11,15 +11,15 @@ from getopt import gnu_getopt, GetoptError
 
 def usage():
     print("""
-{0} [-v] [--tasks=<task1>,<task2>,...] [--show-tasks] [--targets] \
+{0} [-v] [--tasks=<task1>,<task2>,...] [--list-tasks] [--targets] [--run]\
     [--brutal-docker-clean]
     """.format(sys.argv[0]))
 
 
 try:
     opts, args = gnu_getopt(sys.argv[1:], 'v',
-                            ['tasks=', 'show-tasks', 'targets=',
-                             'brutal-docker-clean'])
+                            ['run', 'tasks=', 'list-tasks', 'targets=',
+                             'print=', 'brutal-docker-clean'])
 
 except GetoptError as err:
     sys.stderr.write(str(err))
@@ -29,17 +29,24 @@ except GetoptError as err:
 
 tasks = None
 brutal_clean = False
-targets_override=None
-verbose=False
+targets_override = None
+verbose = False
+run = False
+return_code = 0
+print_mode = False
 
 for o, a in opts:
+    if o in ('--run',):
+        run = True
+
     if o in ('-v',):
-        verbose=True
+        verbose = True
+
     if o in ('--tasks',):
         import tasks
         tasks = [getattr(tasks, s) for s in a.split(',')]
 
-    if o in ('--show-tasks',):
+    if o in ('--list-tasks',):
         import tasks
         from machinery.ci_task import CiTask
         for s in dir(tasks):
@@ -48,7 +55,10 @@ for o, a in opts:
                 if verbose:
                     t = getattr(tasks, s)
                     t.display()
-        exit(0)
+
+    if o in ('--print',):
+        print_mode = True
+        output_mode_str = a
 
     if o in ('--targets',):
         targets_override = a.split(',')
@@ -63,34 +73,48 @@ if tasks is None:
     if hostname in known_tasks:
         tasks = known_tasks[hostname]
     else:
-        tasks = [siconos_default]
+        tasks = [default]
 
-return_code = 0
-for task in tasks:
+if print_mode:
 
-    return_code += task.run(targets_override=targets_override)
+    from machinery.mksenv import print_commands, OutputMode, output_mode_spec
 
-for task in tasks:
+    for task in tasks:
+        distrib, distrib_version = task._distrib.split(':')
+        print_commands(specfilename=database,
+                       distrib=distrib,
+                       distrib_version=distrib_version,
+                       pkgs=task._pkgs,
+                       output_mode=output_mode_spec[output_mode_str],
+                       split=False)
+if run:
 
-    task.clean()
+    for task in tasks:
 
+        return_code += task.run(targets_override=targets_override)
+
+    for task in tasks:
+
+        task.clean()
+
+# docker specific, to be moved elsewhere
 if brutal_clean:
 
     # clean everything (-> maybe once a week?)
     def mklist(sstr):
-        return filter(lambda s: s!='', sstr.strip().split('\n'))
+        return filter(lambda s: s != '', sstr.strip().split('\n'))
 
     running_containers = mklist(check_output(['docker', 'ps', '-q']))
 
-    if len(running_containers)>0:
+    if len(running_containers) > 0:
         check_call(['docker', 'kill'] + running_containers)
 
     containers = mklist(check_output(['docker', 'ps', '-a', '-q']))
-    if len(containers)>0:
+    if len(containers) > 0:
         check_call(['docker', 'rm'] + containers)
 
     images = mklist(check_output(['docker', 'images', '-q']))[1:]
-    if len(images)>0:
+    if len(images) > 0:
         check_call(['docker', 'rmi'] + images)
 
     exit(return_code)
