89ff81790dbdd817e48417300240f06f2c9aa785
Stephen Sinclair <stephen.sinclair@inria.cl>
2016-06-23 11:52:08 -0400

    [cmake] Use ccache if available.

--- cmake/SiconosSetup.cmake
+++ cmake/SiconosSetup.cmake
@@ -204,3 +204,9 @@ if(WITH_DOCUMENTATION)
   set(USE_SPHINX TRUE)
 endif()
 
+# =========== use ccache if available ===========
+find_program(CCACHE_FOUND ccache)
+if(CCACHE_FOUND)
+  set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+  set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+endif(CCACHE_FOUND)
