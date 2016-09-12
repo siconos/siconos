2357b585d602c34de1e1fb908f4e85ac6f8ab2d3
Maurice Bremond <Maurice.Bremond@inria.fr>
2016-09-12 16:07:34 +0200

    [IO] mechanics_io.py : mv import vtk where needed

--- io/swig/io/mechanics_io.py
+++ io/swig/io/mechanics_io.py
@@ -16,11 +16,6 @@ import tempfile
 from contextlib import contextmanager
 from operator import itemgetter
 
-try:
-    import vtk
-except:
-    pass
-
 from siconos.mechanics.contact_detection.tools import Contactor
 
 from siconos.mechanics import joints
@@ -199,6 +194,8 @@ def loadMesh(shape_filename, collision_margin, scale=None):
     WARNING triangles cells assumed!
     """
 
+    import vtk
+    
     reader = vtk.vtkXMLPolyDataReader()
     reader.SetFileName(shape_filename)
     reader.Update()
@@ -1141,6 +1138,9 @@ class Hdf5():
         Add a mesh shape from a string.
         Accepted format : mesh encoded in VTK .vtp format
         """
+
+        import vtk
+        
         if name not in self._ref:
 
             shape = self._ref.create_dataset(name, (1,),
@@ -1158,6 +1158,9 @@ class Hdf5():
         Add a mesh shape from a file.
         Accepted format : .stl or mesh encoded in VTK .vtp format
         """
+
+        import vtk
+        
         if filename[0] != os.path.sep:
             filename = os.path.join(os.path.split(os.path.abspath(sys.argv[0]))[0],
                                     filename)
