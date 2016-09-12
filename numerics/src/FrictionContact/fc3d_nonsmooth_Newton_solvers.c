siconos/siconos

commit 38a8c3f56d4e10c24b677c243fbc929b8511cde5

[numerics] fc3d_compute_error externalize computation of the norm of the reference vector normq

numerics/src/FrictionContact/fc3d_nonsmooth_Newton_solvers.c
@@ -514,11 +514,13 @@ void fc3d_nonsmooth_Newton_solvers_solve(fc3d_nonsmooth_Newton_solvers* equation
 
   double tolerance = options->dparam[0];
   assert(tolerance > 0);
+  
   if (verbose > 0)
     printf("------------------------ FC3D - _nonsmooth_Newton_solversSolve - Start with tolerance = %g\n", tolerance);
 
   unsigned int _3problemSize = 3 * problemSize;
-
+  double normq = cblas_dnrm2(_3problemSize , problem->q , 1);
+ 
   void *buffer;
 
   if (!options->dWork)
@@ -695,7 +697,7 @@ void fc3d_nonsmooth_Newton_solvers_solve(fc3d_nonsmooth_Newton_solvers* equation
 
       fc3d_compute_error(problem, reaction, velocity,
 //      fc3d_FischerBurmeister_compute_error(problem, reaction, velocity,
-                                      tolerance, options, &(options->dparam[1]));
+                         tolerance, options, normq, &(options->dparam[1]));
 
       DEBUG_EXPR_WE(equation->function(equation->data, problemSize,
                                        reaction, velocity, equation->problem->mu, rho,

numerics/src/FrictionContact/fc3d_ACLMFixedPoint.c
@@ -52,6 +52,7 @@ void fc3d_ACLMFixedPoint(FrictionContactProblem* problem, double *reaction, doub
   int itermax = iparam[0];
   /* Tolerance */
   double tolerance = dparam[0];
+  double normq = cblas_dnrm2(nc*3 , problem->q , 1);
 
 
 
@@ -72,7 +73,6 @@ void fc3d_ACLMFixedPoint(FrictionContactProblem* problem, double *reaction, doub
   int iter = 0; /* Current iteration number */
   double error = 1.; /* Current error */
   int hasNotConverged = 1;
-
   soclcp_InternalSolverPtr internalsolver;
 
   SecondOrderConeLinearComplementarityProblem* soclcp = (SecondOrderConeLinearComplementarityProblem *)malloc(sizeof(SecondOrderConeLinearComplementarityProblem));
@@ -139,7 +139,7 @@ void fc3d_ACLMFixedPoint(FrictionContactProblem* problem, double *reaction, doub
     cumul_iter +=  internalsolver_options->iparam[7];
     /* **** Criterium convergence **** */
 
-    fc3d_compute_error(problem, reaction , velocity, tolerance, options, &error);
+    fc3d_compute_error(problem, reaction , velocity, tolerance, options, normq, &error);
 
     if (options->callback)
     {

numerics/src/FrictionContact/fc3d_DeSaxceFixedpoint.c
@@ -44,6 +44,7 @@ void fc3d_DeSaxceFixedPoint(FrictionContactProblem* problem, double *reaction, d
   int itermax = iparam[0];
   /* Tolerance */
   double tolerance = dparam[0];
+  double normq = cblas_dnrm2(nc*3 , problem->q , 1);
 
   /*****  Fixed point iterations *****/
   int iter = 0; /* Current iteration number */
@@ -55,7 +56,6 @@ void fc3d_DeSaxceFixedPoint(FrictionContactProblem* problem, double *reaction, d
   double * velocitytmp = (double *)malloc(n * sizeof(double));
 
   double rho = 0.0;
-
 
   if (dparam[3] > 0.0)
   {
@@ -96,7 +96,7 @@ void fc3d_DeSaxceFixedPoint(FrictionContactProblem* problem, double *reaction, d
       }
 
       /* **** Criterium convergence **** */
-      fc3d_compute_error(problem, reaction , velocity, tolerance, options, &error);
+      fc3d_compute_error(problem, reaction , velocity, tolerance, options, normq, &error);
 
       if (options->callback)
       {

numerics/src/FrictionContact/fc3d_ExtraGradient.c
@@ -44,7 +44,7 @@ void fc3d_ExtraGradient(FrictionContactProblem* problem, double *reaction, doubl
   int itermax = iparam[0];
   /* Tolerance */
   double tolerance = dparam[0];
-
+  double normq = cblas_dnrm2(nc*3 , problem->q , 1);
 
 
 
@@ -57,7 +57,7 @@ void fc3d_ExtraGradient(FrictionContactProblem* problem, double *reaction, doubl
   int nLocal = 3;
   dparam[0] = dparam[2]; // set the tolerance for the local solver
 
-
+  
   double * velocitytmp = (double *)malloc(n * sizeof(double));
   double * reactiontmp = (double *)malloc(n * sizeof(double));
 
@@ -142,7 +142,7 @@ void fc3d_ExtraGradient(FrictionContactProblem* problem, double *reaction, doubl
       }
 
       /* **** Criterium convergence **** */
-      fc3d_compute_error(problem, reaction , velocity, tolerance, options, &error);
+      fc3d_compute_error(problem, reaction , velocity, tolerance, options, normq, &error);
 
       if (options->callback)
       {
@@ -283,7 +283,7 @@ void fc3d_ExtraGradient(FrictionContactProblem* problem, double *reaction, doubl
         );
 
       /* **** Criterium convergence **** */
-      fc3d_compute_error(problem, reaction , velocity, tolerance, options, &error);
+      fc3d_compute_error(problem, reaction , velocity, tolerance, options, normq, &error);
       DEBUG_PRINTF("error = %12.8e\t error_k = %12.8e\n",error,error_k);
       /*Update rho*/
       if ((rho_k*a1 < Lmin * a2) && (error < error_k))

numerics/src/FrictionContact/fc3d_FixedPointProjection.c
@@ -44,6 +44,7 @@ void fc3d_fixedPointProjection(FrictionContactProblem* problem, double *reaction
   int itermax = iparam[0];
   /* Tolerance */
   double tolerance = dparam[0];
+  double normq = cblas_dnrm2(nc*3 , problem->q , 1);
 
   /*****  Fixed point iterations *****/
   int iter = 0; /* Current iteration number */
@@ -202,7 +203,7 @@ void fc3d_fixedPointProjection(FrictionContactProblem* problem, double *reaction
 
 
       /* **** Criterium convergence **** */
-      fc3d_compute_error(problem, reaction , velocity, tolerance, options, &error);
+      fc3d_compute_error(problem, reaction , velocity, tolerance, options, normq, &error);
       DEBUG_EXPR_WE(
         if ((error < error_k))
         {

numerics/src/FrictionContact/fc3d_HyperplaneProjection.c
@@ -44,7 +44,8 @@ void fc3d_HyperplaneProjection(FrictionContactProblem* problem, double *reaction
   int lsitermax = iparam[1];
   /* Tolerance */
   double tolerance = dparam[0];
-
+  double normq = cblas_dnrm2(nc*3 , problem->q , 1);
+ 
 
 
 
@@ -177,7 +178,7 @@ void fc3d_HyperplaneProjection(FrictionContactProblem* problem, double *reaction
     }
 
     /* **** Criterium convergence **** */
-    fc3d_compute_error(problem, reaction , velocity, tolerance, options, &error);
+    fc3d_compute_error(problem, reaction , velocity, tolerance, options, normq, &error);
 
     if (options->callback)
     {

numerics/src/FrictionContact/fc3d_SOCLCP.c
@@ -27,7 +27,7 @@
 #include <math.h>
 //#define VERBOSE_DEBUG
 #include "Friction_cst.h"
-
+#include "SiconosBlas.h"
 
 #define DEBUG_MESSAGES
 #define DEBUG_STDOUT
@@ -49,7 +49,8 @@ void fc3d_SOCLCP(FrictionContactProblem* problem, double *reaction, double *velo
 
   /* Tolerance */
   double tolerance = dparam[0];
-
+  double normq = cblas_dnrm2(nc*3 , problem->q , 1);
+ 
 
 
   if (options->numberOfInternalSolvers < 1)
@@ -110,7 +111,7 @@ void fc3d_SOCLCP(FrictionContactProblem* problem, double *reaction, double *velo
 
   double real_error=0.0;
 
-  fc3d_compute_error(problem, reaction , velocity, tolerance, options, &real_error);
+  fc3d_compute_error(problem, reaction , velocity, tolerance, options, normq, &real_error);
 
   if (options->callback)
   {

numerics/src/FrictionContact/fc3d_Solvers.h
@@ -48,7 +48,7 @@ typedef void (*UpdatePtr)(int, FrictionContactProblem*, FrictionContactProblem*,
 typedef void (*PostSolverPtr)(int, double*);
 
 /** pointer to function used to update velocity and compute error */
-typedef void (*ComputeErrorPtr)(FrictionContactProblem*, double*, double*, double, SolverOptions*,  double*);
+typedef void (*ComputeErrorPtr)(FrictionContactProblem*, double*, double*, double, SolverOptions*, double,  double*);
 
 /** pointer to function used to free memory for objects used in solvers */
 typedef void (*FreeSolverPtr)();

commit 529bd2dcf5ac3be8df7d8d3bcc532be863568bb6

[numerics] correct bug in the computation introduced in 38a8c3f

numerics/src/FrictionContact/fc3d_nonsmooth_Newton_solvers.c
@@ -519,7 +519,7 @@ void fc3d_nonsmooth_Newton_solvers_solve(fc3d_nonsmooth_Newton_solvers* equation
     printf("------------------------ FC3D - _nonsmooth_Newton_solversSolve - Start with tolerance = %g\n", tolerance);
 
   unsigned int _3problemSize = 3 * problemSize;
-  double normq = cblas_dnrm2(_3problemSize , problem->q , 1);
+  double normq = cblas_dnrm2(problemSize , problem->q , 1);
  
   void *buffer;
 