61fec90808281716ee4c9ea37365fda42096e736
Vincent Acary <vincent.acary@inria.fr>
2016-11-13 20:57:38 +0100

    [numerics] fis right size for iparam

--- numerics/src/FrictionContact/gfc3d_nonsmooth_Newton_AlartCurnier.c
+++ numerics/src/FrictionContact/gfc3d_nonsmooth_Newton_AlartCurnier.c
@@ -388,8 +388,8 @@ int gfc3d_nonsmooth_Newton_AlartCurnier_setDefaultSolverOptions(
   options->numberOfInternalSolvers = 0;
   options->isSet = 1;
   options->filterOn = 1;
-  options->iSize = 14;
-  options->dSize = 11;
+  options->iSize = 20;
+  options->dSize = 20;
   options->iparam = (int *) calloc(options->iSize, sizeof(int));
   options->dparam = (double *) calloc(options->dSize,  sizeof(double));
   options->dWork = NULL;
