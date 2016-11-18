ba94f2af1e36f44ff6890e9ec9e168e17b4cefe2
Vincent Acary <vincent.acary@inria.fr>
2016-11-18 11:21:00 +0100

    [mechanics] remove warning with unused variable

--- mechanics/swig/tests/plugins/SliderCrankPlugin.cpp
+++ mechanics/swig/tests/plugins/SliderCrankPlugin.cpp
@@ -68,7 +68,7 @@ extern "C" void internalMomentsB1_Jacq(double t, double *q, double *v, double *j
   // printf("q[5] = %e\n", q[5]);
   // printf("q[6] = %e\n", q[6]);
 
-  double angle = 2*asin(q[5]);
+  // double angle = 2*asin(q[5]);
 
   // printf("angle = %e\n", angle);
   jac[1+5*3]=1e3 * 2.0 / sqrt(1 - q[5]*q[5]) ;
