4bcabce4f4e1f7b2dd8dc8108300861c6ff1b4c2
Stephen Sinclair <stephen.sinclair@inria.cl>
2016-11-28 19:00:49 -0300

    [mechanics] walker working in a funny way now with stabilising prismatic joints
    Actually lasts quite a few seconds before falling over.
    
    Eventually this should be turned into a real mechanism but it is maybe
    a funny example for now, useful for demonstration purposes.

--- examples/Mechanics/MechanicsIO/walker.py
+++ examples/Mechanics/MechanicsIO/walker.py
@@ -29,7 +29,9 @@ with Hdf5() as io:
 
     # Define shape of a bar-shaped link
     io.addPrimitiveShape('Bar1', 'Box', (10, 1, 1))
+    io.addPrimitiveShape('Bar2', 'Box', (12.67, 1, 1))
 
+    ## Core
     bar1 = \
     io.addObject('bar1', [Contactor('Bar1'),
                           Contactor('Bar1',
@@ -41,6 +43,7 @@ with Hdf5() as io:
                  translation=[0, 0, 11],
                  mass=1)
 
+    ## Legs
     io.addObject('bar2', [Contactor('Bar1')], translation=[5.5, 5.5, 6],
                  orientation=[(0,1,0), -pi/2], mass=1)
 
@@ -58,8 +61,32 @@ with Hdf5() as io:
     io.addJoint('joint3', 'bar4', 'bar1', [4.5, 0, 0], [0, 1, 0], 'PivotJointR')
     io.addJoint('joint4', 'bar5', 'bar1', [4.5, 0, 0], [0, 1, 0], 'PivotJointR')
 
+    ## Stabilizing leg links to body
+
+    # See mech.spbstu.ru/Dzenushko_Dainis:_Walking_mechanisms_survey
+    # for a survey of better walking mechanisms
+
+    io.addObject('bar6', [Contactor('Bar2')], translation=[0,  6.5, 7],
+                 orientation=[(0,1,0), -pi/8], mass=1)
+    io.addObject('bar7', [Contactor('Bar2')], translation=[0, -6.5, 7],
+                 orientation=[(0,1,0), -pi/8], mass=1)
+    io.addObject('bar8', [Contactor('Bar2')], translation=[0,  6.5, 7],
+                 orientation=[(0,1,0), -pi/8], mass=1)
+    io.addObject('bar9', [Contactor('Bar2')], translation=[0, -6.5, 7],
+                 orientation=[(0,1,0), -pi/8], mass=1)
+    io.addJoint('joint5', 'bar6', 'bar2', [ 4.5,  0, 0], [0, 1, 0], 'PivotJointR')
+    io.addJoint('joint6', 'bar7', 'bar3', [ 4.5,  0, 0], [0, 1, 0], 'PivotJointR')
+    io.addJoint('joint7', 'bar8', 'bar4', [-4.5,  0, 0], [0, 1, 0], 'PivotJointR')
+    io.addJoint('joint8', 'bar9', 'bar5', [-4.5,  0, 0], [0, 1, 0], 'PivotJointR')
+
+    io.addJoint('joint9', 'bar6', 'bar8', [1, 0, 0], [], 'PrismaticJointR')
+    io.addJoint('joint10','bar7', 'bar9', [1, 0, 0], [], 'PrismaticJointR')
+
+    io.addJoint('joint11','bar2', 'bar3', [0, 0, 0], [0, 1, 0], 'PivotJointR')
+    io.addJoint('joint12','bar4', 'bar5', [0, 0, 0], [0, 1, 0], 'PivotJointR')
+
     # Harmonic oscillator on Y-axis angular velocity = a+b*cos(omega*time+phi))
-    freq = 0.1
+    freq = 2
     amp = 0.2
     io.addBoundaryCondition('vibration', 'bar1',
                             indices=[4], # Y-angular axis is index 4 into ds->v
@@ -73,7 +100,7 @@ def my_forces(body):
     g = 9.81
     weight = numpy.array([0, 0, - body.scalarMass() * g, 0, 0, 0])
     twist = numpy.array([0,0,0,0,300,0])
-    push = numpy.array([0.3,0,0,0,0,0])
+    push = numpy.array([0,0,0,0,0,0])
     force = weight
     if body.number() == bar1:
         force = weight + push # + twist
@@ -93,7 +120,7 @@ with Hdf5(mode='r+') as io:
             edge_class=None,
             t0=0,
             T=30,
-            h=0.001,
+            h=0.005,
             multipoints_iterations=True,
             theta=0.50001,
             Newton_max_iter=1,
