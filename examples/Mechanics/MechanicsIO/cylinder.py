796f2b84b893b37cd40ad382b668a48b022fb27d
Vincent Acary <vincent.acary@inria.fr>
2016-12-11 16:16:57 +0100

    [examples] add parameters in cylinder example

--- examples/Mechanics/MechanicsIO/cylinder.py
+++ examples/Mechanics/MechanicsIO/cylinder.py
@@ -6,14 +6,17 @@
 #
 
 from siconos.mechanics.collision.tools import Contactor
-from siconos.io.mechanics_io import Hdf5
+from mechanics_io import Hdf5
 import siconos.numerics as Numerics
 
+import numpy as np
 # Creation of the hdf5 file for input/output
 with Hdf5() as io:
 
     # Definition of a sphere
-    io.addPrimitiveShape('Cyl', 'Cylinder', (0.1, 2))
+    R =0.1
+    L =2.0
+    io.addPrimitiveShape('Cyl', 'Cylinder', (R, L))
 
     # Definition of the ground shape
     io.addPrimitiveShape('Ground', 'Box', (10, 10, 1.0))
@@ -26,11 +29,21 @@ with Hdf5() as io:
     # As a mass is given, it is a dynamic system involved in contact
     # detection and in the simulation.  With no group id specified the
     # Contactor belongs to group 0
+    mass_test=1.0
+    inertia_test = np.eye(3)
+
+
+    inertia_test[0,0]=0.25*mass_test*R*R+ 1/3.0*mass_test*L*L
+    inertia_test[1,1]=0.5*mass_test*R*R
+    inertia_test[2,2]=0.25*mass_test*R*R+ 1/3.0*mass_test*L*L
+    print(inertia_test)
+    orientation_test=[0.14, 0.7, 0.7, 0]
+    #orientation_test=[1.0, 0., 0., 0]
     io.addObject('cyl', [Contactor('Cyl')],
                  translation=[0, 0, 4],
-                 orientation=[0.14, 0.7, 0.7, 0],
+                 orientation=orientation_test,
                  velocity=[0, 0, 0, 0, 0, 0],
-                 mass=1)
+                 mass=1 , inertia=inertia_test)
 
     # the ground object made with the ground shape. As the mass is
     # not given, it is a static object only involved in contact
@@ -50,13 +63,13 @@ with Hdf5(mode='r+') as io:
            edge_class=None,
            t0=0,
            T=20,
-           h=0.005,
+           h=0.0005,
            multipoints_iterations=True,
            theta=0.50001,
            Newton_max_iter=20,
            set_external_forces=None,
            solver=Numerics.SICONOS_FRICTION_3D_NSGS,
            itermax=100000,
-           tolerance=1e-8,
+           tolerance=1e-06,
            numerics_verbose=False,
            output_frequency=None)
