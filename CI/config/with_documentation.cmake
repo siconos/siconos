3b27b80f6fbfa27806956745b3de538642fa0a2f
Maurice Bremond <Maurice.Bremond@inria.fr>
2017-01-17 15:13:03 +0100

    [ci] add bullet et serialization to ci doc

--- CI/config/with_documentation.cmake
+++ CI/config/with_documentation.cmake
@@ -1,4 +1,6 @@
-# from default, test solvers with mumps
+# from default, documentation
 include(default)
 set(GFORGE_USER ci)
+set_option(WITH_SERIALIZATION ON)
+set_option(WITH_BULLET ON)
 set_option(WITH_DOCUMENTATION ON)
