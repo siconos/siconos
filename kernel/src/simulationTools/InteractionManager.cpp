616139e6b0957f96523799f5c97504ef0cf718ff
Stephen Sinclair <stephen.sinclair@inria.cl>
2017-01-27 12:09:51 -0300

    [kernel] Avoid bad_index exception in nslaw matrix for contact_groups without nslaws.
    It should be possible to disable contact in a simulation by removing
    the NSLaws, but currently this causes a ublas exception in
    symmetric_matrix.  This patch checks the indexes before looking up the
    nslaw and returns a null pointer if they exceed the matrix size.
    
    The alternative is to catch the bad_index exception, but this causes
    an error message to be printed even in release build.

--- kernel/src/simulationTools/InteractionManager.cpp
+++ kernel/src/simulationTools/InteractionManager.cpp
@@ -18,3 +18,12 @@ void InteractionManager::insertNonSmoothLaw(SP::NonSmoothLaw nslaw,
   _nslaws.resize( std::max(_nslaws.size1(), maxgroup+1) );
   _nslaws(group1, group2) = nslaw;
 }
+
+SP::NonSmoothLaw InteractionManager::nonSmoothLaw(long unsigned int group1,
+                                                  long unsigned int group2)
+{
+  if (group1 < _nslaws.size1() && group2 < _nslaws.size2())
+    return _nslaws(group1, group2);
+  else
+    return SP::NonSmoothLaw();
+}
