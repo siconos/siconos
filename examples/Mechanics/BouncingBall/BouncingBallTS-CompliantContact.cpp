8ae6d87c2f13f45c16559cfd79097f0c3f76d3a3
Vincent Acary <vincent.acary@inria.fr>
2017-02-17 16:44:56 +0100

    [kernel] add a first example of compliant contact condition at position level

---
+++ examples/Mechanics/BouncingBall/BouncingBallTS-CompliantContact.cpp
@@ -0,0 +1,234 @@
+/* Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ *
+ * Copyright 2016 INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+/*!\file BouncingBallTS.cpp
+  \brief \ref EMBouncingBall - C++ input file, Time-Stepping version -
+  V. Acary, F. Perignon.
+
+  A Ball bouncing on the ground.
+  Direct description of the model.
+  Simulation with a Time-Stepping scheme.
+*/
+
+#include "SiconosKernel.hpp"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+
+    // ================= Creation of the model =======================
+
+    // User-defined main parameters
+    unsigned int nDof = 3;           // degrees of freedom for the ball
+    double t0 = 0;                   // initial computation time
+    double T = 10;                  // final computation time
+    double h = 0.005;                // time step
+    double position_init = 1.0;      // initial position for lowest bead.
+    double velocity_init = 0.0;      // initial velocity for lowest bead.
+    double theta = 0.5;              // theta for MoreauJeanOSI integrator
+    double R = 0.1; // Ball radius
+    double height = 1.0; // height to the roof
+    double m = 1; // Ball mass
+    double g = 9.81; // Gravity
+    // -------------------------
+    // --- Dynamical systems ---
+    // -------------------------
+
+    cout << "====> Model loading ..." <<  endl;
+
+    SP::SiconosMatrix Mass(new SimpleMatrix(nDof, nDof));
+    (*Mass)(0, 0) = m;
+    (*Mass)(1, 1) = m;
+    (*Mass)(2, 2) = 2. / 5 * m * R * R;
+
+    // -- Initial positions and velocities --
+    SP::SiconosVector q0(new SiconosVector(nDof));
+    SP::SiconosVector v0(new SiconosVector(nDof));
+    (*q0)(0) = position_init;
+    (*v0)(0) = velocity_init;
+
+    // -- The dynamical system --
+    SP::LagrangianLinearTIDS ball(new LagrangianLinearTIDS(q0, v0, Mass));
+
+    // -- Set external forces (weight) --
+    SP::SiconosVector weight(new SiconosVector(nDof));
+    (*weight)(0) = -m * g;
+    ball->setFExtPtr(weight);
+
+    // --------------------
+    // --- Interactions ---
+    // --------------------
+
+    // -- nslaw --
+    double e = 0.9;
+
+    // Interaction ball-roof with impact
+    //
+    SP::SimpleMatrix H(new SimpleMatrix(1, nDof));
+    (*H)(0, 0) = -1.0;
+    SP::SiconosVector b(new SiconosVector(1));
+    (*b)(0) = - R;
+
+    SP::NonSmoothLaw nslaw(new NewtonImpactNSL(e));
+    SP::Relation relation(new LagrangianLinearTIR(H,b));
+
+    SP::Interaction inter(new Interaction(1, nslaw, relation));
+
+    // -- nslaw --
+    double stiffness = 10.0;
+
+    // Interaction ball-floor with a compliant spring
+    SP::SimpleMatrix Hfloor(new SimpleMatrix(1, nDof));
+    (*Hfloor)(0, 0) = 1.0;
+     SP::SimpleMatrix Kfloor(new SimpleMatrix(1, 1));
+    (*Kfloor)(0, 0) = stiffness;
+
+    SP::SiconosVector bfloor(new SiconosVector(1));
+    (*bfloor)(0) = height - R;
+    SP::NonSmoothLaw nslawfloor(new ComplementarityConditionNSL(1));
+
+    SP::Relation relationfloor(new LagrangianCompliantLinearTIR(Hfloor,Kfloor, bfloor));
+
+    SP::Interaction interfloor(new Interaction(1, nslawfloor, relationfloor));
+
+
+
+
+    // -------------
+    // --- Model ---
+    // -------------
+    SP::Model bouncingBall(new Model(t0, T));
+
+    // add the dynamical system in the non smooth dynamical system
+    bouncingBall->nonSmoothDynamicalSystem()->insertDynamicalSystem(ball);
+
+    // link the interaction and the dynamical system
+    bouncingBall->nonSmoothDynamicalSystem()->link(inter, ball);
+
+    // link the interaction and the dynamical system
+    bouncingBall->nonSmoothDynamicalSystem()->link(interfloor, ball);
+
+    // ------------------
+    // --- Simulation ---
+    // ------------------
+
+    // -- (1) OneStepIntegrators --
+    SP::MoreauJeanOSI OSI(new MoreauJeanOSI(theta));
+
+
+    // -- (2) Time discretisation --
+    SP::TimeDiscretisation t(new TimeDiscretisation(t0, h));
+
+    // -- (3) one step non smooth problem
+    SP::OneStepNSProblem osnspb(new LCP());
+
+    // -- (4) Simulation setup with (1) (2) (3)
+    SP::TimeStepping s(new TimeStepping(t, OSI, osnspb));
+    bouncingBall->setSimulation(s);
+
+    // =========================== End of model definition ===========================
+
+    // ================================= Computation =================================
+
+    // --- Simulation initialization ---
+
+    cout << "====> Initialisation ..." << endl;
+    //bouncingBall->nonSmoothDynamicalSystem()->topology()->setOSI(ball, OSI);
+    bouncingBall->initialize();
+
+    // -- set the integrator for the ball --
+
+
+
+
+    int N = ceil((T - t0) / h); // Number of time steps
+
+    // --- Get the values to be plotted ---
+    // -> saved in a matrix dataPlot
+    unsigned int outputSize = 5;
+    SimpleMatrix dataPlot(N + 1, outputSize);
+
+    SP::SiconosVector q = ball->q();
+    SP::SiconosVector v = ball->velocity();
+    SP::SiconosVector p = ball->p(1);
+    SP::SiconosVector lambda = inter->lambda(1);
+
+    dataPlot(0, 0) = bouncingBall->t0();
+    dataPlot(0, 1) = (*q)(0);
+    dataPlot(0, 2) = (*v)(0);
+    dataPlot(0, 3) = (*p)(0);
+    dataPlot(0, 4) = (*lambda)(0);
+    // --- Time loop ---
+    cout << "====> Start computation ... " << endl;
+    // ==== Simulation loop - Writing without explicit event handling =====
+    int k = 1;
+    boost::progress_display show_progress(N);
+
+    boost::timer time;
+    time.restart();
+
+    while (s->hasNextEvent())
+    {
+      s->computeOneStep();
+      // --- Get values to be plotted ---
+      dataPlot(k, 0) =  s->nextTime();
+      dataPlot(k, 1) = (*q)(0);
+      dataPlot(k, 2) = (*v)(0);
+      dataPlot(k, 3) = (*p)(0);
+      dataPlot(k, 4) = (*lambda)(0);
+      s->nextStep();
+      ++show_progress;
+      k++;
+    }
+    cout  << "End of computation - Number of iterations done: " << k - 1 << endl;
+    cout << "Computation Time " << time.elapsed()  << endl;
+
+  //   // --- Output files ---
+  //   cout << "====> Output file writing ..." << endl;
+  //   dataPlot.resize(k, outputSize);
+  //   ioMatrix::write("result.dat", "ascii", dataPlot, "noDim");
+  //   std::cout << "Comparison with a reference file" << std::endl;
+  //   SimpleMatrix dataPlotRef(dataPlot);
+  //   dataPlotRef.zero();
+  //   ioMatrix::read("result.ref", "ascii", dataPlotRef);
+  //   double error = (dataPlot - dataPlotRef).normInf();
+  //   std::cout << "error =" << error << std::endl;
+
+  //   if (error> 1e-12)
+  //   {
+  //     std::cout << "Warning. The result is rather different from the reference file." << std::endl;
+  //     return 1;
+  //   }
+
+  }
+
+  catch (SiconosException e)
+  {
+    cout << e.report() << endl;
+  }
+  catch (...)
+  {
+    cout << "Exception caught in BouncingBallTS.cpp" << endl;
+  }
+
+
+
+}
