e6026de74478ad389b325168530904ccfd774cab
Stephen Sinclair <stephen.sinclair@inria.cl>
2017-03-29 14:53:29 -0300

    [kernel] make make_shared available everywhere

--- kernel/src/utils/SiconosTools/SiconosPointers.hpp
+++ kernel/src/utils/SiconosTools/SiconosPointers.hpp
@@ -67,6 +67,7 @@ namespace std11 = std;
 #include <memory>
 #else
 #include <boost/shared_ptr.hpp>
+#include <boost/make_shared.hpp>
 #include <boost/enable_shared_from_this.hpp>
 namespace std11 = boost;
 #endif
