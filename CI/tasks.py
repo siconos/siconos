8611dc030bb0245bd30806572def0c65b8cad1da
Maurice Bremond <Maurice.Bremond@inria.fr>
2017-05-10 18:31:46 +0200

    [documentation] upload fix

--- CI/tasks.py
+++ CI/tasks.py
@@ -64,10 +64,15 @@ siconos_ubuntu_15_10 = siconos_default.copy()(
     add_pkgs=['umfpack'],
     distrib='ubuntu:15.10')
 
+import os
+from os.path import expanduser
+home = expanduser("~")
+
 siconos_documentation = siconos_default.copy()(
     distrib='ubuntu:16.10',
     ci_config='with_documentation',
     add_pkgs=['documentation'],
+    add_directories=[os.path.join('home', '.ssh')],
     targets={'.': ['docker-build', 'docker-cmake', 'docker-make',
                    'docker-make-install',
                    'docker-make-doc', 'docker-make-upload']})
