a696e08590671fe428fe44479bbeaf6ab3c78872
Olivier Huber <ohuber2@wisc.edu>
2017-07-19 13:08:26 -0500

    [kernel] fix build with gcc-4.9

--- kernel/src/modelingTools/NewtonEulerDS.cpp
+++ kernel/src/modelingTools/NewtonEulerDS.cpp
@@ -15,6 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
+#include "CxxStd.hpp"
+
 #include "NewtonEulerDS.hpp"
 #include "BlockVector.hpp"
 #include "BlockMatrix.hpp"
