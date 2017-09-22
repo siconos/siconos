0df291f72d6093ebe1cd5d873e791be3096716b6
Vincent Acary <vincent.acary@inria.fr>
2017-09-22 15:41:45 +0200

    [numerics] add M_PI definition if not founf in math.h

--- numerics/src/tools/op3x3.h
+++ numerics/src/tools/op3x3.h
@@ -26,6 +26,11 @@
 #include <float.h>
 #include <stdlib.h>
 
+#ifndef M_PI
+#define M_PI        3.14159265358979323846264338327950288   /* pi             */
+#define M_PI_2      1.57079632679489661923132169163975144   /* pi/2           */
+#endif
+
 #include "numerics_verbose.h"
 #include "SiconosCompat.h"
 
