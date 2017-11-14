4929ac5a7cba5d72ffbd691d6a861e3ac774f1b3
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2017-11-14 11:37:42 +0100

    [build install] nix conf (works fine on Luke)

--- default.nix
+++ default.nix
@@ -6,7 +6,27 @@ with pkgs;
    			 numerics_only=true;
 			 enable_python=false;
 			 blas_implem=pkgs.openblas.override { blas64 = false; };};
+   siconos-numerics-python3 = callPackage ./siconos.nix {
+   			 numerics_only=true;
+			 enable_python=true;
+			 blas_implem=pkgs.openblas.override { blas64 = false; };
+			 };
+   siconos-numerics-python2 = callPackage ./siconos.nix {
+   			 numerics_only=true;
+			 enable_python=true;
+			 blas_implem=pkgs.openblas.override { blas64 = false; };
+			 pythonX=python2;};
    siconos-full = callPackage ./siconos.nix {
 			 enable_python=false;
 			 blas_implem=pkgs.openblas.override { blas64 = false; };};
+   siconos-full-python = callPackage ./siconos.nix {
+			 enable_python=true;
+			 blas_implem=pkgs.openblas.override { blas64 = false; };};
+   siconos-test = callPackage ./sico_light.nix {
+			 enable_python=true;
+			 blas_implem=pkgs.openblas.override { blas64 = false; };};
+
+
+   sico-py = pkgs.python35.withPackages (ps: with ps; [ ./siconos-numerics-python3 pip numpy ipython h5py matplotlib pytest lxml scipy]);
+
 }
