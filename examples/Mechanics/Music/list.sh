7dc16139cfdaafffbb8bab10d99c846981eb1e6c
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2017-11-24 17:55:57 +0100

    [examples Music] oar things

--- examples/Mechanics/Music/list.sh
+++ examples/Mechanics/Music/list.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 oarsub -S "./run.sh 64225280 4096" -p "network_address='luke42'" -t timesharing=perignon,*
-oarsub -S "./run.sh  32112640 2048" -p "network_address='luke43'" -t timesharing=perignon,*
-oarsub -S "./run.sh 16056320 1024" -p "network_address='luke38'" -t timesharing=perignon,*
-oarsub -S "./run.sh 8028160 512" -p "network_address='luke39'" -t timesharing=perignon,*
-oarsub -S "./run.sh 4014080 256" -p "network_address='luke43'" -t timesharing=perignon,*
-oarsub -S "./run.sh 2007040 128" -p "network_address='luke43'" -t timesharing=perignon,*
-oarsub -S "./run.sh 1003520 64" -p "network_address='luke43'" -t timesharing=perignon,*
-oarsub -S "./run.sh 501760 32" -p "network_address='luke38'" -t timesharing=perignon,*
+oarsub -S "./run.sh  32112640 2048" -p "network_address='luke42'" -t timesharing=perignon,*
+oarsub -S "./run.sh 16056320 1024" -p "network_address='luke42'" -t timesharing=perignon,*
+oarsub -S "./run.sh 8028160 512" -p "network_address='luke42'" -t timesharing=perignon,*
+oarsub -S "./run.sh 4014080 256" -p "network_address='luke42'" -t timesharing=perignon,*
+oarsub -S "./run.sh 2007040 128" -p "network_address='luke42'" -t timesharing=perignon,*
+oarsub -S "./run.sh 1003520 64" -p "network_address='luke42'" -t timesharing=perignon,*
+oarsub -S "./run.sh 501760 32" -p "network_address='luke42'" -t timesharing=perignon,*
 oarsub -S "./run.sh 250880 16" -p "network_address='luke42'" -t timesharing=perignon,*
 oarsub -S "./run.sh 125440 8" -p "network_address='luke42'" -t timesharing=perignon,*
 oarsub -S "./run.sh 62720 4" -p "network_address='luke42'" -t timesharing=perignon,*
--- examples/Mechanics/Music/list_f2.sh
+++ examples/Mechanics/Music/list_f2.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-oarsub -S "./run_fretless.sh 64225280 4096" -p "network_address='luke42'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh  32112640 2048" -p "network_address='luke43'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 16056320 1024" -p "network_address='luke38'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 8028160 512" -p "network_address='luke39'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 4014080 256" -p "network_address='luke43'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 2007040 128" -p "network_address='luke43'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 1003520 64" -p "network_address='luke43'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 501760 32" -p "network_address='luke38'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 250880 16" -p "network_address='luke42'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 125440 8" -p "network_address='luke42'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 62720 4" -p "network_address='luke42'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 31360 2" -p "network_address='luke42'" -t timesharing=perignon,*
-oarsub -S "./run_fretless.sh 15680 1" -p "network_address='luke42'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 64225280 4096" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh  32112640 2048" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 16056320 1024" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 8028160 512" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 4014080 256" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 2007040 128" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 1003520 64" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 501760 32" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 250880 16" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 125440 8" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 62720 4" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 31360 2" -p "network_address='luke43'" -t timesharing=perignon,*
+oarsub -S "./run_fretless_bis.sh 15680 1" -p "network_address='luke43'" -t timesharing=perignon,*
 
 
 
