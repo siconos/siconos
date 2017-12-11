5e4eacd4ae5d5b8fb1dfd9ebf335bf31ddd830ab
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2017-12-11 11:13:07 +0100

    [examples Music] add results file (pkl)

--- examples/Mechanics/Music/simulation_campaigns.py
+++ examples/Mechanics/Music/simulation_campaigns.py
@@ -35,28 +35,28 @@ def create_jobs(outputfile, runner, restit_node, job_name, freqs=None):
 
 
     
-def create_campaign(freq2job, frequencies, filepath, hosts):
-    campaign = {}
-    nbfiles = len(freq2job)
-    filelist = [None, ] * nbfiles
-    newfilelist = [None, ] * nbfiles
-    durations = [None, ] * nbfiles
-    filepaths = [os.path.join(filepath,
-                              'F_' + str(frequencies[i]) + '_id_' + str(freq2job[frequencies[i]][0]))
-                 for i in  range(nbfiles)]
-    for i in range(nbfiles):
-        filelist[i] = 'g_862_' + str(frequencies[i]) + '.h5'
-        newfilelist[i] = 'converted_' + filelist[i]
-        filelist[i] = os.path.join(filepaths[i], filelist[i])
-        newfilelist[i] = os.path.join(filepaths[i], newfilelist[i])
-        durations[i] = freq2job[frequencies[i]][1]
+# def create_campaign(freq2job, frequencies, filepath, hosts):
+#     campaign = {}
+#     nbfiles = len(freq2job)
+#     filelist = [None, ] * nbfiles
+#     newfilelist = [None, ] * nbfiles
+#     durations = [None, ] * nbfiles
+#     filepaths = [os.path.join(filepath,
+#                               'F_' + str(frequencies[i]) + '_id_' + str(freq2job[frequencies[i]][0]))
+#                  for i in  range(nbfiles)]
+#     for i in range(nbfiles):
+#         filelist[i] = 'g_862_' + str(frequencies[i]) + '.h5'
+#         newfilelist[i] = 'converted_' + filelist[i]
+#         filelist[i] = os.path.join(filepaths[i], filelist[i])
+#         newfilelist[i] = os.path.join(filepaths[i], newfilelist[i])
+#         durations[i] = freq2job[frequencies[i]][1]
 
-    campaign['freqs'] = frequencies
-    campaign['files'] = filelist
-    campaign['files_converted'] = newfilelist
-    campaign['durations'] = durations
-    campaign['hosts'] = hosts
-    return campaign
+#     campaign['freqs'] = frequencies
+#     campaign['files'] = filelist
+#     campaign['files_converted'] = newfilelist
+#     campaign['durations'] = durations
+#     campaign['hosts'] = hosts
+#     return campaign
 
 
 
