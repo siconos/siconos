27549fcabf91b8027d6478a86f124c669d0331a6
Stephen Sinclair <stephen.sinclair@inria.cl>
2017-12-13 17:29:27 -0300

    [io] generated serialization registration categorized by component

--- io/tools/builder_common.py
+++ io/tools/builder_common.py
@@ -1,7 +1,7 @@
 
 __all__ = ['unwanted', 'get_priority', 'parse_args', 'get_headers',
            'write_header', 'write_footer', 'write_includes',
-           'write_register_with_bases', 'write_classes']
+           'write_register_with_bases', 'write_classes', 'get_target']
 
 import os
 import os.path
@@ -43,6 +43,14 @@ def unwanted(s):
     # _spo : subpluggedobject
     # _blockCSR -> double * serialization needed by hand (but uneeded anyway for a full restart)
     return m is not None
+
+def get_target(source_dir, header_path):
+    prefix = os.path.commonprefix([source_dir, header_path])
+    firstdir = header_path[len(prefix):].split(os.path.sep)[0]
+    for t in input_headers.keys():
+        if firstdir==t: return t
+    raise RuntimeError('target not found for {} (found {}?)'
+                       .format(header_path, firstdir))
 
 # try to provide an ordering for registering a class
 # The main issue is with the Model and the NonSmoothDynamicalSystem, Topology,
@@ -221,15 +229,17 @@ def write_includes(dest_file, all_headers):
 
 
 def write_register_with_bases(dest_file, with_base):
-    with_base_s = [c for c, p in sorted(with_base, key=lambda k: (k[1], k[0]))]
-    dest_file.write('\n')
-    dest_file.write('template <class Archive>\n')
-    dest_file.write('void siconos_io_register_generated(Archive& ar)\n')
-    dest_file.write('{{\n{0}\n}}\n'
-                    .format('\n'
-                            .join(
-                                '  ar.register_type(static_cast<{0}*>(NULL));'
-                                .format(x) for x in with_base_s)))
+    for target in {t: None for c,p,t in with_base}.keys():
+        with_base_s = [c for c, p, t in sorted(with_base, key=lambda k: (k[1], k[0]))
+                       if t == target]
+        dest_file.write('\n')
+        dest_file.write('template <class Archive>\n')
+        dest_file.write('void siconos_io_register_generated_{}(Archive& ar)\n'
+                        .format(target.capitalize()))
+        dest_file.write('{{\n{0}\n}}\n'
+                        .format('\n'.join(
+                            '  ar.register_type(static_cast<{0}*>(NULL));'
+                            .format(x) for x in with_base_s)))
 
 
 def write_classes(dest_file, classes):
