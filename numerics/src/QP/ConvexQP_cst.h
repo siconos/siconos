dd023b1c80557cad80ec3f0628c890f5765ad6dc
Vincent Acary <vincent.acary@inria.fr>
2018-01-18 22:16:56 +0100

    [numerics] remove warning and one unstable test

--- numerics/src/QP/ConvexQP_cst.h
+++ numerics/src/QP/ConvexQP_cst.h
@@ -19,12 +19,12 @@ extern const char* const   SICONOS_CONVEXQP_VI_FPP_STR;
 extern const char* const   SICONOS_CONVEXQP_VI_EG_STR;
 extern const char* const   SICONOS_CONVEXQP_ADMM_STR;
 
-enum SICONOS_CONVEXQP_PGOC_IPARAM
+enum SICONOS_CONVEXQP_PGOC_IPARAM_ENUM
 {
   /** index in iparam to store the maximum number of iterations */
   SICONOS_CONVEXQP_PGOC_LINESEARCH_MAXITER = 10
 };
-enum SICONOS_CONVEXQP_PGOC_DPARAM
+enum SICONOS_CONVEXQP_PGOC_DPARAM_ENUM
 {
   /** index in dparam to store the rho value for projection formulation */
   SICONOS_CONVEXQP_PGOC_RHO = 3,
@@ -36,18 +36,18 @@ enum SICONOS_CONVEXQP_PGOC_DPARAM
   SICONOS_CONVEXQP_PGOC_LINESEARCH_TAU  = 6 
 };
 
-enum SICONOS_CONVEXQP_ADMM_IPARAM
+enum SICONOS_CONVEXQP_ADMM_IPARAM_ENUM
 {
   /** index in iparam to store the acceleration paramter */
   SICONOS_CONVEXQP_ADMM_IPARAM_ACCELERATION= 10
 };
 
-enum SICONOS_CONVEXQP_ADMM_DPARAM
+enum SICONOS_CONVEXQP_ADMM_DPARAM_ENUM
 {
   /** index in dparam to store the rho value for projection formulation */
   SICONOS_CONVEXQP_ADMM_RHO = 3,
 };
-enum SICONOS_CONVEXQP_ADMM_ACCELERATION
+enum SICONOS_CONVEXQP_ADMM_ACCELERATION_ENUM
 {
   SICONOS_CONVEXQP_ADMM_NO_ACCELERATION= 0,
   SICONOS_CONVEXQP_ADMM_ACCELERATION= 1,
