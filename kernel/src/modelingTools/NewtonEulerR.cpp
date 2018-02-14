09447065b2ef99a8b0a64910067731535c52fd73
Stephen Sinclair <stephen.sinclair@inria.cl>
2018-02-14 17:07:40 -0300

    [kernel] remove debug flags

--- kernel/src/modelingTools/NewtonEulerR.cpp
+++ kernel/src/modelingTools/NewtonEulerR.cpp
@@ -28,9 +28,9 @@
 #include "SimulationGraphs.hpp"
 
 // #define DEBUG_BEGIN_END_ONLY
-#define DEBUG_NOCOLOR
-#define DEBUG_STDOUT
-#define DEBUG_MESSAGES
+// #define DEBUG_NOCOLOR
+// #define DEBUG_STDOUT
+// #define DEBUG_MESSAGES
 
 #include <debug.h>
 
