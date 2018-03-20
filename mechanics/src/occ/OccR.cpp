b9a08bf3559bb381b6f69e8568c73472013d0278
Maurice Bremond <Maurice.Bremond@inria.fr>
2018-03-20 12:05:44 +0100

    [mechanics] occ: 2 offsets, one for each contact

--- mechanics/src/occ/OccR.cpp
+++ mechanics/src/occ/OccR.cpp
@@ -18,9 +18,8 @@ OccR::OccR(const ContactPoint& contact1,
   _contact1(contact1),
   _contact2(contact2),
   _geometer(),
-  _normalFromFace1(false),
-  _offsetp1(false),
-  _offset(0.1)
+  _offset1(0.),
+  _offset2(0.)
 {
   switch (Type::value(distance_calculator))
   {
@@ -60,31 +59,19 @@ void OccR::computeh(double time, BlockVector& q0, SiconosVector& y)
   double& n1y = dist.ny;
   double& n1z = dist.nz;
 
-  if(_offsetp1)
-  {
-    _Pc1->setValue(0, X1+_offset*n1x);
-    _Pc1->setValue(1, Y1+_offset*n1y);
-    _Pc1->setValue(2, Z1+_offset*n1z);
-    _Pc2->setValue(0, X2);
-    _Pc2->setValue(1, Y2);
-    _Pc2->setValue(2, Z2);
-  }
-  else
-  {
-    _Pc1->setValue(0, X1);
-    _Pc1->setValue(1, Y1);
-    _Pc1->setValue(2, Z1);
-    _Pc2->setValue(0, X2-_offset*n1x);
-    _Pc2->setValue(1, Y2-_offset*n1y);
-    _Pc2->setValue(2, Z2-_offset*n1z);
-  }
+  _Pc1->setValue(0, X1+_offset1*n1x);
+  _Pc1->setValue(1, Y1+_offset1*n1y);
+  _Pc1->setValue(2, Z1+_offset1*n1z);
+  _Pc2->setValue(0, X2-_offset2*n1x);
+  _Pc2->setValue(1, Y2-_offset2*n1y);
+  _Pc2->setValue(2, Z2-_offset2*n1z);
 
   /* cf comments from O. Bonnefon */
   _Nc->setValue(0, n1x);
   _Nc->setValue(1, n1y);
   _Nc->setValue(2, n1z);
 
-  dist.value -= _offset;
+  dist.value -= (_offset1-_offset2);
 
   y.setValue(0, dist.value);
 
