0e3c7117f28f80ef7a371df6c6c87c5aa29f6e50
Stephen Sinclair <stephen.sinclair@inria.cl>
2018-04-06 19:12:06 -0300

    [CI] help for debugging ctest failures

--- CI/cmake/Docker.cmake
+++ CI/cmake/Docker.cmake
@@ -91,7 +91,7 @@ macro(add_docker_targets)
   string(REPLACE ":" "-" DOCKER_WORKDIR_VOLUME ${DOCKER_WORKDIR_VOLUME})
   string(REPLACE "." "-" DOCKER_WORKDIR_VOLUME ${DOCKER_WORKDIR_VOLUME})
 
-  set(CTEST_OPTIONS_WITHOUT_DOCKER)
+  set(CTEST_OPTIONS_WITHOUT_DOCKER "--output-on-failure")
   # List of options (-DOPTION=VALUE ...) to be passed to ctest
   # Warning : these are not siconos conf. options for cmake but
   # the options needed by ctest to prepare the pipeline (cmake, make ...)
