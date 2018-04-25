3abcc7da31693646dd025d90737af0090317c539
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-04-25 10:56:40 +0200

    [build] nix conf update. Works on Luke for local (from sources) install

--- siconos.nix
+++ siconos.nix
@@ -1,28 +1,79 @@
-{ pkgs ? import <nixpkgs> {},
+{ stdenv, fetchurl, cmake, pkgconfig,
+  pkgs ? import <nixpkgs> {},
   gcc ? pkgs.gcc ,
   gfortran ? pkgs.gfortran,
   numerics_only ? false,
   enable_python ? true,
-  python ? pkgs.python36,
+  pythonX ? pkgs.python36,
   blas_name ? "openblas",
   blas_implem ? pkgs.openblas.override { blas64 = false; },
+  boost ? pkgs.boost,
+  gmp ? pkgs.gmp,
+  version ? "4.1.0",
+  enable_cxx ? true,
   }:
   
 with pkgs;
+with stdenv.lib;
 
-let  version = "1.0.0" ; in
- stdenv.mkDerivation rec {
+let
+	pythonenv = pythonX.withPackages (ps: with ps; [pip numpy ipython h5py matplotlib lxml scipy pytest]);
 
+
+in
+
+stdenv.mkDerivation rec {
+ inherit version;
  name = "siconos-${version}";
- buildInputs = [ cmake pkgconfig gcc gfortran python blas_implem pkgs.gmp pkgs.boost]
- 	     ++ stdenv.lib.optional (numerics_only == false)[pkgs.boost];	     
- cmakeFlags = [ " " ]
-    ++ stdenv.lib.optional (numerics_only == true) [ "-DCOMPONENTS=externals;numerics -DWITH_CXX=OFF" ]
-    ++ stdenv.lib.optional (enable_python != true) [ "-DWITH_PYTHON_WRAPPER=OFF" ]
-    ++ stdenv.lib.optional (enable_python != true) [ "-DWITH_BLAS=${blas_name}" ];
-    
-    
+ 
+ enableParallelBuilding = true;	
 
-hardeningDisable = [ "format" ];
+ nativeBuildInputs = [
+    pkgconfig
+    pythonX
+   ]  
+  ++ optional enable_python [pythonX.pkgs.wrapPython];
+ 
+ buildInputs = [
+    gcc
+    blas_implem
+    gmp
+    boost
+    cppunit
+    ]
+  ++ optional (numerics_only != true) [ boost ];
+  
+  propagatedNativeBuildInputs = with pythonX.pkgs;
+    if enable_python then [ cmake swig gfortran pythonenv]
+    else [cmake gfortran];
+
+ cmakeFlags = [ "-DWITH_BLAS=${blas_name}" ]
+    ++ optional (numerics_only == true) [ "-DCOMPONENTS=externals;numerics -DWITH_CXX=OFF" ]
+    ++ optional (enable_python != true) [ "-DWITH_PYTHON_WRAPPER=OFF" ]
+    ++ optional (enable_python == true) [ "-Dsiconos_python_install=prefix" ];
+    
+    
+ hardeningDisable = [ "format" ];
  src = ./.;
-    }
+
+
+
+ 
+  postFixup = ''
+    echo "Create links in bin ..."
+    if test -e $out/nix-support/propagated-native-build-inputs; then
+        ln -s $out/nix-support/propagated-native-build-inputs $out/nix-support/propagated-user-env-packages
+    fi
+    echo "siconos" > $out/${pythonX.sitePackages}/${name}.pth
+  '';
+
+ 
+  meta = with stdenv.lib; {
+    homepage = http://siconos.gforge.inria.fr/;
+    description = "Nonsmooth dynamical systems simulator";
+    license = licenses.asl20;
+    platforms = platforms.all;
+  };
+
+
+}
