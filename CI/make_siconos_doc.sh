4aaa9c8e9f429b9f9e3f1aba717c70c4c4742c96
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-05-22 09:28:54 +0200

    [docs] doxy2swig dislikes '\n' in doxygen.

--- CI/make_siconos_doc.sh
+++ CI/make_siconos_doc.sh
@@ -1,6 +1,6 @@
 #!bin/bash
-apt-get update -qq && apt-get install -y -qq cmake git-core wget make \
-			      cmake libboost-dev libgmp-dev swig gcc gfortran g++ liblapack-dev libatlas-base-dev \
+apt update -qq && apt install -y -qq cmake git-core wget make \
+			      libboost-dev libgmp-dev swig gcc gfortran g++ liblapack-dev libatlas-base-dev \
 			      lp-solve liblpsolve55-dev python3-dev libpython3-dev bash swig doxygen python3-dev python3-pip graphviz
 
 pip3 install -r ./docs/requirements.txt
