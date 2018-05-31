44a3d505fd08e72a6d71a31cfec7ed0a688dc608
Olivier Huber <ohuber2@wisc.edu>
2018-05-31 17:44:38 -0500

    [CI] new cdash likes https

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,7 +9,7 @@
 set(CTEST_PROJECT_NAME "Siconos")
 set(CTEST_NIGHTLY_START_TIME "20:00:00 CET")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "cdash-tripop.inrialpes.fr")
 set(CTEST_DROP_LOCATION "/submit.php?project=Siconos")
 set(CTEST_DROP_SITE_CDASH TRUE)
