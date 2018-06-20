6e00d8160ee22d818796a2602fdf9b9e6a7fe214
Maurice Bremond <Maurice.Bremond@inria.fr>
2018-06-20 11:15:30 +0200

    [CI] some fedora fixes

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,7 +9,14 @@
 set(CTEST_PROJECT_NAME "Siconos")
 set(CTEST_NIGHTLY_START_TIME "20:00:00 CET")
 
-set(CTEST_DROP_METHOD "https")
+# fedora fails with https  
+#    Error message was: libcurl was built with SSL disabled, https: not supported!
+#    Problems when submitting via HTTP
+# CMake Warning at /home/maurice/src/git/siconos/CI/machinery/CTestDriver.cmake:172 (message):
+#    *** submission failure ***
+# Note: option --system-curl for cmake build is not sufficient.
+
+set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "cdash-tripop.inrialpes.fr")
 set(CTEST_DROP_LOCATION "/submit.php?project=Siconos")
 set(CTEST_DROP_SITE_CDASH TRUE)
