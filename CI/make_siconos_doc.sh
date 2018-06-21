949977522d9842930a8d8c8edc1d0ca57830eb54
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2018-06-21 09:30:02 +0200

    [docs/CI] fix python packages install

--- CI/make_siconos_doc.sh
+++ CI/make_siconos_doc.sh
@@ -3,7 +3,7 @@ apt update -qq && apt install -y -qq cmake git-core wget make \
 			      libboost-dev libgmp-dev swig gcc gfortran g++ liblapack-dev libatlas-base-dev \
 			      lp-solve liblpsolve55-dev python3-dev libpython3-dev bash swig doxygen python3-dev python3-pip graphviz
 
-pip3 install -r ./docs/requirements.txt
+pip3 install -U -r ./docs/requirements.txt
 pip3 install git+https://github.com/sphinx-contrib/youtube.git
 mkdir build
 cd build
