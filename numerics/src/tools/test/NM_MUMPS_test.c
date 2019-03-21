d96ddbac51ff3fef6af37bac7e4f1e65f2c46381
Maurice Bremond <Maurice.Bremond@inria.fr>
2019-03-21 15:05:21 +0100

    [numerics] add a MUMPS interface test (solve a 2x2 system & check)

---
+++ numerics/src/tools/test/NM_MUMPS_test.c
@@ -0,0 +1,82 @@
+#include "SiconosNumerics.h"
+#include "NumericsMatrix.h"
+#include "NumericsSparseMatrix.h"
+#include "siconos/NM_MUMPS.h"
+#include "siconos/NM_MPI.h"
+#define SIZE 2
+#include <stdlib.h>
+#include <stdio.h>
+#ifdef HAVE_MPI
+#include <mpi.h>
+#endif
+#include <math.h>
+int main(int argc, char *argv[])
+{
+  double b[SIZE];
+
+#ifdef HAVE_MPI
+  MPI_Init(&argc, &argv);
+#endif
+
+  int rank;
+
+#ifdef HAVE_MPI
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+#else
+  rank = 0;
+#endif
+
+  NumericsMatrix* M = NM_create(NM_SPARSE, SIZE, SIZE);
+
+  NM_MPI_set_comm(M, MPI_COMM_WORLD);
+  NM_MUMPS_set_control_params(M);
+  NM_triplet_alloc(M, 0);
+  M->matrix2->origin = NSM_TRIPLET;
+
+  NM_MUMPS(M, -1);
+  if(rank > 0)
+  {
+#ifdef HAVE_MPI
+    MPI_Finalize();
+    NM_free(M);
+#endif
+    exit(0);
+  }
+
+  assert (rank == 0);
+  /*
+     2*x - y = 1
+     x   + y = 1
+  */
+  /* solution x: 2/3, y: 1/3 */
+
+  NM_zentry(M, 0, 0, 2.);
+  NM_zentry(M, 0, 1, -1.);
+  NM_zentry(M, 1, 0, 1.);
+  NM_zentry(M, 1, 1, 1.);
+
+  for(unsigned int i=0; i<SIZE; ++i)
+  {
+    b[i] = 1.;
+  }
+
+  NM_MUMPS_set_problem(M, b);
+  NM_MUMPS(M, 6);
+  NM_MUMPS(M, -2);
+  NM_MUMPS(M, 0);
+
+  for (unsigned int i=0; i<SIZE; ++i)
+  {
+    printf("solution b[%d] = %g\n", i, b[i]);
+  }
+
+#ifdef HAVE_MPI
+  MPI_Finalize();
+#endif
+  NM_free(M);
+
+  if (fabs(b[0] - 2./3.) > 1e-7) return(1);
+  if (fabs(b[1] - 1./3.) > 1e-7) return(1);
+
+  return(0);
+}
