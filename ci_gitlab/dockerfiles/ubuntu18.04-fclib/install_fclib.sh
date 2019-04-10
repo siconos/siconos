a7cc28e79fe98c6c71607ec52227d2d029c0651f
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2019-04-10 16:18:06 +0200

    [docker-build] fix fclib docker image

--- ci_gitlab/dockerfiles/ubuntu18.04-fclib/install_fclib.sh
+++ ci_gitlab/dockerfiles/ubuntu18.04-fclib/install_fclib.sh
@@ -1,17 +1,16 @@
 #!bin/bash
 #
 # Check if CI_PROJECT_DIR is set AND not empty
-: ${CI_PROJECT_DIR:?"Please set environment variable CI_PROJECT_DIR with 'siconos-tutorials' repository (absolute) path."}
 
 # -- Fclib download --
-cd $CI_PROJECT_DIR
+cd $HOME
 git clone https://github.com/FrictionalContactLibrary/fclib.git
 
 # Creates a directory to build libs.
-mkdir -p $CI_PROJECT_DIR/build
-cd $CI_PROJECT_DIR/build
+mkdir -p $HOME/build
+cd $HOME/build
 
 # Configure and build fclib
-cmake $CI_PROJECT_DIR/fclib -DFCLIB_HEADER_ONLY=OFF
+cmake $HOME/fclib -DFCLIB_HEADER_ONLY=OFF
 make
 make install
