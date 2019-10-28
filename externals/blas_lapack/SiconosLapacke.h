3c582a3c88e081bdaffa88e4f0b461b4c710c3bb
Vincent ACARY <vincent.acary@inria.fr>
2019-10-28 18:17:54 +0100

    [numerics] add wrapper for other blas/Lapack distribution

--- externals/blas_lapack/SiconosLapacke.h
+++ externals/blas_lapack/SiconosLapacke.h
@@ -27,13 +27,13 @@
 
 // -------- Headers and routines naming conventions for the different Lapack implementations --------
 
-// --- Intel MKL Header --- 
-#if defined(HAS_MKL_LAPACKE) 
+// --- Intel MKL Header ---
+#if defined(HAS_MKL_LAPACKE)
 #include <mkl_lapacke.h>
-#else 
+#else
 // Standard lapacke header
 #include <lapacke.h>
-#endif 
+#endif
 
 // Name of the routines
 #define LAPACK_NAME(N) LAPACKE_##N
@@ -61,15 +61,24 @@
 #define WRAP_DGESVD(F,A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,INFO)      \
   fprintf(stderr, "Your lapack version misses dgesvd function.\n");
 #endif
-  
+
 // --- DGETRS ---
 #define WRAP_DGETRS(F,A1,A2,A3,A4,A5,A6,A7,A8,INFO)   \
   INFO = F(CblasColMajor,A1,A2,A3,A4,A5,A6,A7,A8)
-     
+
+// --- DPOTRS ---
+#define WRAP_DPOTRS(F,A1,A2,A3,A4,A5,A6,A7,INFO) \
+  INFO = F(CblasColMajor,A1,A2,A3,A4,A5,A6,A7)
+
+
 // --- DGESV ---
 #define WRAP_DGESV(F,A1,A2,A3,A4,A5,A6,A7,INFO)   \
   INFO = F(CblasColMajor,A1,A2,A3,A4,A5,A6,A7)
-     
+
+// --- DPOSV ---
+#define WRAP_DPOSV(F,A1,A2,A3,A4,A5,A6,A7,INFO) \
+  INFO = F(CblasColMajor,A1,A2,A3,A4,A5,A6,A7)
+
 // --- DGELS ---
 #if defined(HAS_LAPACK_dgels)
 #define WRAP_DGELS(F,A1,A2,A3,A4,A5,A6,A7,A8,INFO)    \
@@ -90,7 +99,8 @@
 // --- DGETRF ---
 #define WRAP_DGETRF(F,A1,A2,A3,A4,A5,INFO)  \
   INFO = F(CblasColMajor,A1,A2,A3,A4,A5)
- 
+
+
 // --- DTRTRS ---
 #if defined(HAS_LAPACK_dtrtrs)
 #define WRAP_DTRTRS(F,A1,A2,A3,A4,A5,A6,A7,A8,A9,INFO)  \
