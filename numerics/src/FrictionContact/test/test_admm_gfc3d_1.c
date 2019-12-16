acab555d3e0bf378352982fc4b355280a1f96003
Vincent ACARY <vincent.acary@inria.fr>
2019-12-16 16:40:37 +0100

    [numerics] put again update s variable and all tests

--- numerics/src/FrictionContact/test/test_admm_gfc3d_1.c
+++ numerics/src/FrictionContact/test/test_admm_gfc3d_1.c
@@ -30,97 +30,97 @@ char *** test_collection(int n_data_1, char ** data_collection_1)
   }
 
   int n =0;
-  /* for ( int d =0; d <n_data_1; d++) */
-  /* { */
-  /*   int e=0; */
-  /*   test[n][e++] = data_collection_1[d]; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_GLOBAL_FRICTION_3D_NSGS_WR); */
-  /*   test[n][e++] = "1e-05"; */
-  /*   test[n][e++] = "10000"; */
-  /*   test[n][e++] = "---"; */
-  /*   n++; */
-  /* } */
-  /* for ( int d =0; d <n_data_1; d++) */
-  /* { */
-  /*   int e=0; */
-  /*   test[n][e++] = data_collection_1[d]; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_GLOBAL_FRICTION_3D_ADMM); */
-  /*   test[n][e++] = "1e-05"; */
-  /*   test[n][e++] = "1000"; */
-  /*   test[n][e++] = "---"; */
-  /*   n++; */
-  /* } */
+  for ( int d =0; d <n_data_1; d++)
+  {
+    int e=0;
+    test[n][e++] = data_collection_1[d];
+    test[n][e++] = "0";
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_GLOBAL_FRICTION_3D_NSGS_WR);
+    test[n][e++] = "1e-05";
+    test[n][e++] = "10000";
+    test[n][e++] = "---";
+    n++;
+  }
+  for ( int d =0; d <n_data_1; d++)
+  {
+    int e=0;
+    test[n][e++] = data_collection_1[d];
+    test[n][e++] = "0";
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_GLOBAL_FRICTION_3D_ADMM);
+    test[n][e++] = "1e-05";
+    test[n][e++] = "1000";
+    test[n][e++] = "---";
+    n++;
+  }
 
-  /* for ( int d =0; d <n_data_1; d++) */
-  /* { */
-  /*   int e=0; */
-  /*   test[n][e++] = data_collection_1[d]; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_GLOBAL_FRICTION_3D_ADMM); */
-  /*   test[n][e++] = "1e-05"; */
-  /*   test[n][e++] = "10000"; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e++] = "iparam"; */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_IPARAM_RHO_STRATEGY ); */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_RHO_STRATEGY_RESIDUAL_BALANCING); */
-  /*   test[n][e++] = "---"; */
-  /*   n++; */
-  /* } */
-  /* for ( int d =0; d <n_data_1; d++) */
-  /* { */
-  /*   int e=0; */
-  /*   test[n][e++] = data_collection_1[d]; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_GLOBAL_FRICTION_3D_ADMM); */
-  /*   test[n][e++] = "1e-05"; */
-  /*   test[n][e++] = "10000"; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e++] = "iparam"; */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_IPARAM_RHO_STRATEGY); */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_RHO_STRATEGY_SCALED_RESIDUAL_BALANCING); */
-  /*   test[n][e++] = "---"; */
-  /*   n++; */
-  /* } */
+  for ( int d =0; d <n_data_1; d++)
+  {
+    int e=0;
+    test[n][e++] = data_collection_1[d];
+    test[n][e++] = "0";
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_GLOBAL_FRICTION_3D_ADMM);
+    test[n][e++] = "1e-05";
+    test[n][e++] = "10000";
+    test[n][e++] = "0";
+    test[n][e++] = "0";
+    test[n][e++] = "0";
+    test[n][e++] = "iparam";
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_IPARAM_RHO_STRATEGY );
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_RHO_STRATEGY_RESIDUAL_BALANCING);
+    test[n][e++] = "---";
+    n++;
+  }
+  for ( int d =0; d <n_data_1; d++)
+  {
+    int e=0;
+    test[n][e++] = data_collection_1[d];
+    test[n][e++] = "0";
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_GLOBAL_FRICTION_3D_ADMM);
+    test[n][e++] = "1e-05";
+    test[n][e++] = "10000";
+    test[n][e++] = "0";
+    test[n][e++] = "0";
+    test[n][e++] = "0";
+    test[n][e++] = "iparam";
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_IPARAM_RHO_STRATEGY);
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_RHO_STRATEGY_SCALED_RESIDUAL_BALANCING);
+    test[n][e++] = "---";
+    n++;
+  }
 
-  /* for ( int d =0; d <n_data_1; d++) */
-  /* { */
-  /*   int e=0; */
-  /*   test[n][e++] = data_collection_1[d]; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_GLOBAL_FRICTION_3D_ADMM); */
-  /*   test[n][e++] = "1e-05"; */
-  /*   test[n][e++] = "30000"; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e++] = "0"; */
-  /*   test[n][e++] = "iparam"; */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_IPARAM_RHO_STRATEGY ); */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_RHO_STRATEGY_RESIDUAL_BALANCING); */
-  /*   test[n][e++] = "iparam"; */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_IPARAM_RESCALING); */
-  /*   test[n][e] = (char *)malloc(50*sizeof(char)); */
-  /*   sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_RESCALING_YES); */
-  /*   test[n][e++] = "---"; */
-  /*   n++; */
-  /* } */
+  for ( int d =0; d <n_data_1; d++)
+  {
+    int e=0;
+    test[n][e++] = data_collection_1[d];
+    test[n][e++] = "0";
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_GLOBAL_FRICTION_3D_ADMM);
+    test[n][e++] = "1e-05";
+    test[n][e++] = "30000";
+    test[n][e++] = "0";
+    test[n][e++] = "0";
+    test[n][e++] = "0";
+    test[n][e++] = "iparam";
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_IPARAM_RHO_STRATEGY );
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_ADMM_RHO_STRATEGY_RESIDUAL_BALANCING);
+    test[n][e++] = "iparam";
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_IPARAM_RESCALING);
+    test[n][e] = (char *)malloc(50*sizeof(char));
+    sprintf(test[n][e++], "%d", SICONOS_FRICTION_3D_RESCALING_YES);
+    test[n][e++] = "---";
+    n++;
+  }
   for ( int d =0; d <n_data_1; d++)
   {
     int e=0;
