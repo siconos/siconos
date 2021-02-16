df2acabb1c92a9b3402b6874d9a8258991fa22fa
Vincent ACARY <vincent.acary@inria.fr>
2021-02-16 18:26:00 +0100

    [numerics] fix memory error in balancing

--- numerics/src/FrictionContact/gfc3d_balancing.c
+++ numerics/src/FrictionContact/gfc3d_balancing.c
@@ -24,6 +24,10 @@
 #include "SiconosBlas.h"                         // for cblas_dcopy, cblas_dscal
 #include "CSparseMatrix_internal.h"                // for NSM_TRIPLET ...
 #include "gfc3d_balancing.h"
+
+
+/* #define DEWBUG_STDOUT */
+/* #define DEBUG_MESSAGES */
 #include "siconos_debug.h"                         // for DEBUG_EXPR
 #ifdef  DEBUG_MESSAGES
 #include "NumericsVector.h"
@@ -68,8 +72,6 @@ void gfc3d_balancing_MHHT(
   NM_triplet_alloc(M_tmp, n);
   NM_gemm(1.0,problem->M,B_for_M->D2,0.0, M_tmp) ; // M * D2M
   NM_gemm(1.0,B_for_M->D2,M_tmp, 0.0, problem->M); // D1M *M  * D2M
-  //NM_gemm(1.0,B_for_M->D1,problem->M, 0.0, M_tmp);
-  //NM_copy(M_tmp, problem->M);
 
   /* scaling of H */
   /* Warning the matrix H must be scaled such
@@ -79,20 +81,18 @@ void gfc3d_balancing_MHHT(
     /* A choice among other */
     NM_triplet(B_for_H->D2)->x[i+1] = NM_triplet(B_for_H->D2)->x[i];
     NM_triplet(B_for_H->D2)->x[i+2] = NM_triplet(B_for_H->D2)->x[i];
-    NM_triplet(B_for_H->D1)->x[i+1] = NM_triplet(B_for_H->D2)->x[i];
-    NM_triplet(B_for_H->D1)->x[i+2] = NM_triplet(B_for_H->D2)->x[i];
   }
   NumericsMatrix* H_tmp = NM_create(NM_SPARSE, n, m);
   NM_triplet_alloc(H_tmp, n);
   NM_gemm(1.0,problem->H,B_for_H->D2,0.0, H_tmp); // H * D2H
-  NM_gemm(1.0,B_for_M->D1,H_tmp, 0.0, problem->H); // D1M * D2H
+  NM_gemm(1.0,B_for_M->D1,H_tmp, 0.0, problem->H); // D1M * H* D2H
   //NM_gemm(1.0,B_for_M->D1,problem->M, 0.0, M_tmp);
   //NM_copy(M_tmp, problem->M);
 
   /* scaling of q */
   double * q_tmp = (double *) malloc(n*sizeof(double));
   NM_gemv(1.0, B_for_M->D2, problem->q, 0.0, q_tmp);
-  cblas_dcopy(m, q_tmp, 1, problem->q, 1);
+  cblas_dcopy(n, q_tmp, 1, problem->q, 1);
 
   /* scaling of b */
   double * b_tmp = (double *) malloc(m*sizeof(double));
@@ -191,11 +191,11 @@ GlobalFrictionContactProblem*  gfc3d_balancing_problem(GlobalFrictionContactProb
   BalancingMatrices * B_for_M = NULL;
   BalancingMatrices * B_for_H = NULL;
 
-  NumericsMatrix *Htrans =  NM_transpose(H);
-  /* Compute M + rho H H^T (storage in W)*/
-  NumericsMatrix *W = NM_create(NM_SPARSE,n,n);
-  NM_triplet_alloc(W, n);
-  W->matrix2->origin = NSM_TRIPLET;
+  /* NumericsMatrix *Htrans =  NM_transpose(H); */
+  /* /\* Compute M + rho H H^T (storage in W)*\/ */
+  /* NumericsMatrix *W = NM_create(NM_SPARSE,n,n); */
+  /* NM_triplet_alloc(W, n); */
+  /* W->matrix2->origin = NSM_TRIPLET; */
 
 
   if(options->iparam[SICONOS_FRICTION_3D_IPARAM_RESCALING]==SICONOS_FRICTION_3D_RESCALING_SCALAR)
@@ -215,7 +215,7 @@ GlobalFrictionContactProblem*  gfc3d_balancing_problem(GlobalFrictionContactProb
   {
     numerics_printf_verbose(1,"---- GFC3D - BALANCING - Rescaling of the problem by balancing M");
     data->B_for_M  = NM_BalancingMatrices_new(problem->M);
-    //NM_compute_balancing_matrices(problem->M, 1e-2, 5, data->B_for_M);
+    NM_compute_balancing_matrices(problem->M, 1e-2, 5, data->B_for_M);
     gfc3d_balancing_M(rescaled_problem, data->B_for_M);
   }
   /* else if (options->iparam[SICONOS_FRICTION_3D_IPARAM_RESCALING]==SICONOS_FRICTION_3D_RESCALING_BALANCING_H) */
@@ -229,6 +229,7 @@ GlobalFrictionContactProblem*  gfc3d_balancing_problem(GlobalFrictionContactProb
 
     numerics_printf_verbose(1,"---- GFC3D - BALANCING - Rescaling of the problem by balancing MHHT");
 
+    /****** compute balancing matrices **************************************/
     NumericsMatrix * MHHT = NM_create(NM_SPARSE, n+m, n+m);
     NM_triplet_alloc(MHHT, n+m);
     MHHT->matrix2->origin = NSM_TRIPLET;
@@ -239,7 +240,9 @@ GlobalFrictionContactProblem*  gfc3d_balancing_problem(GlobalFrictionContactProb
 
     //NM_display(MHHT);
     BalancingMatrices * B_for_MHHT = NM_BalancingMatrices_new(MHHT);
-    //NM_compute_balancing_matrices(MHHT, 1e-2, 5, B_for_MHHT);
+    NM_compute_balancing_matrices(MHHT, 1e-2, 5, B_for_MHHT);
+    DEBUG_EXPR(NM_display(B_for_MHHT->D1););
+    DEBUG_EXPR(NM_display(B_for_MHHT->D2););
 
     /* to simplify the use of the balancing matrices, we split it */
     data->B_for_M = NM_BalancingMatrices_new(problem->M); // lazy mode
@@ -247,15 +250,23 @@ GlobalFrictionContactProblem*  gfc3d_balancing_problem(GlobalFrictionContactProb
 
     for(size_t i =0; i < n ; i++)
     {
-       NM_triplet(data->B_for_M->D2)->x[i] = NM_triplet(B_for_MHHT->D2)->x[i];
-       NM_triplet(data->B_for_M->D1)->x[i] = NM_triplet(B_for_MHHT->D1)->x[i];
-       NM_triplet(data->B_for_H->D1)->x[i] = NM_triplet(B_for_MHHT->D1)->x[i+n];
+      NM_triplet(data->B_for_M->D1)->x[i] = NM_triplet(B_for_MHHT->D1)->x[i]; // D1M
+      NM_triplet(data->B_for_M->D2)->x[i] = NM_triplet(B_for_MHHT->D2)->x[i]; // D2M
     }
+
+    for(size_t i =0; i < n ; i++)
+    {
+      NM_triplet(data->B_for_H->D1)->x[i] = NM_triplet(B_for_MHHT->D1)->x[i]; // D1M
+    }
+
     for(size_t i =0; i < m ; i++)
     {
-       NM_triplet(data->B_for_H->D2)->x[i] = NM_triplet(B_for_MHHT->D2)->x[i+m];
-
+      NM_triplet(data->B_for_H->D2)->x[i] = NM_triplet(B_for_MHHT->D2)->x[i+n]; //D2H
     }
+
+
+
+    /****** balanced problem          ***************************************/
     gfc3d_balancing_MHHT(rescaled_problem, data->B_for_M, data->B_for_H);
   }
   else
