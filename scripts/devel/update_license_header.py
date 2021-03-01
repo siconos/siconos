a6131ad2a8e3de73027bbac81790da0a35e3d8c7
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2021-03-01 16:25:16 +0100

    [skip CI] script to update license header in siconos-tutorials

--- scripts/devel/update_license_header.py
+++ scripts/devel/update_license_header.py
@@ -1,25 +1,26 @@
 
 import os
 import fileinput
-rootpath = './sico-2021'
+rootpath = './siconos-tutorials'
 flist = []
 for root, directories, files in os.walk(rootpath):
     for name in files:
         flist.append(os.path.join(root, name))
 
-    exclude = ['.png', '.eps', '.hdf5', '.h5','.jpg', '.gz','.pdf', '.npz']
+    exclude = ['.png', '.eps', '.hdf5', '.h5','.jpg', '.gz','.pdf', '.npz', '.mat', '.zip', '.prt.1', '.dox','.txt','.maple','.sxd','.sce','.TXT','.sci','.stp']
 for ext in exclude:
     flist[:] = [d for d in flist if not d.endswith(ext) and '.git' not in d]
 
-# for f in flist:
-#     with open(f) as ff:
-#         try:
-#             ff.readline()
-#         except:
-#             print(f)
+for f in flist:
+    with open(f) as ff:
+        #try:
+        print(f)
+        ff.readline()
+        #except:
+        #    print(f)
 
 
-instring ='Copyright 2020 INRIA'
+instring ='Copyright 2018 INRIA'
 #instring ='Copyright (C) 2005, 2018 by INRIA'
 #instring ='Siconos-Numerics, Copyright INRIA 2005-2015'
 #instring = 'Siconos, Copyright INRIA 2005-2016'
