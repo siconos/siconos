ed71f8b101881be7481960549c893703c8fe6650
Minh Nguyen <hoang-minh.nguyen@inria.fr>
2023-01-28 15:46:20 +0100

    [numerics] create new nonsmooth test

--- numerics/numerics_tests.cmake
+++ numerics/numerics_tests.cmake
@@ -304,6 +304,11 @@ if(WITH_TESTING)
       EXTRA_SOURCES data_collection_gfc3d_fclib.c test_ipm_gfc3d_1.c DEPS FCLIB::fclib
       HDF5 ON
       )
+    new_tests_collection(
+      DRIVER gfc3d_test_collection.c.in  FORMULATION gfc3d_nonsmooth COLLECTION TEST_IPM_COLLECTION_FCLIB
+      EXTRA_SOURCES data_collection_gfc3d_fclib.c test_ipm_gfc3d_1.c DEPS FCLIB::fclib
+      HDF5 ON
+      )
 
     # ---------------------------------------------------
     # --- Rolling friction contact problem formulation ---
