76ae9d56fe6627234e7f8c08842c54b62c9cd044
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2023-02-03 16:41:07 +0100

    Add missing files

---
+++ kernel/src/utils/SiconosAlgebra/SiconosMatrixOp.hpp
@@ -0,0 +1,217 @@
+/* Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ *
+ * Copyright 2022 INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*! \file SiconosMatrixOp.hpp
+  \brief Toolbox for operators acting on matrices
+  For matrix-vector op, see SiconosMatrixVectorOp.hpp
+*/
+#ifndef SICOMAT_OPH
+#define SICOMAT_OPH
+#include <memory>
+
+namespace siconos::algebra {
+
+class SiconosMatrix;
+class SimpleMatrix;
+class BlockMatrix;
+
+/** multiply the current matrix with a scalar
+ *
+ *  \param m the matrix to operate on
+ *  \param s the scalar
+ *  \return SiconosMatrix&
+ */
+SiconosMatrix& operator*=(SiconosMatrix& m, const double& s);
+
+SiconosMatrix& operator*=(SiconosMatrix& m, const double& s);
+
+SiconosMatrix& operator/=(SiconosMatrix& m, const double& s);
+const SimpleMatrix operator/(const SiconosMatrix& A, double a);
+void operator+=(std::shared_ptr<SiconosMatrix> A, std::shared_ptr<SimpleMatrix> B);
+
+// /** multiplication of a matrix by a double
+//  *  \param A a SiconosMatrix
+//  *  \param a a double
+//  *  \return a SimpleMatrix
+//  */
+// SimpleMatrix operator * (const SiconosMatrix& A, double a);
+
+/** multiplication of a matrix by a double
+ *  \param a a double
+ *  \param A a SiconosMatrix
+ *  \return a SimpleMatrix
+ */
+SimpleMatrix operator*(double a, const SiconosMatrix& A);
+
+const SimpleMatrix operator*(const SiconosMatrix&, double);
+
+// /** multiplication of a SimpleMatrix by a SimpleMatrix
+//  *  \param a const SiconosMatrix&
+//  *  \param a const SimpleMatrix&
+//  *  \return a const SimpleMatrix
+//  */
+// //  const SimpleMatrix operator * (const SimpleMatrix&,const SimpleMatrix&);
+
+std::shared_ptr<SimpleMatrix> operator*(const std::shared_ptr<SimpleMatrix>,
+                                        const std::shared_ptr<SimpleMatrix>);
+
+const SimpleMatrix operator/(const SiconosMatrix& A, double a);
+
+// /** multiplication of a std::shared_ptr<SimpleMatrix> by a std::shared_ptr<SimpleMatrix>
+//  *  \param A a std::shared_ptr<SiconosMatrix>
+//  *  \param B a std::shared_ptr<SimpleMatrix>
+//  *  \return a std::shared_ptr<SimpleMatrix>
+//  */
+// std::shared_ptr<SimpleMatrix> operator * (const std::shared_ptr<SimpleMatrix> A, const
+// std::shared_ptr<SimpleMatrix> B);
+
+/** operator += add B to A
+ *  \param[in,out] A a std::shared_ptr<SiconosMatrix>
+ *  \param B a std::shared_ptr<SiconosMatrix>
+ */
+// void operator +=(std::shared_ptr<SiconosMatrix> A, std::shared_ptr<SimpleMatrix> B);
+
+// /** division of the matrix by a double
+//  *  \param A a SiconosMatrix
+//  *  \param a a double
+//  *  \return a SimpleMatrix
+//  */
+// const SimpleMatrix operator /(const SiconosMatrix& A, double a);
+
+/** Addition of two matrices, C = A+B
+ * \param A a SiconosMatrix
+ * \param B a SiconosMatrix
+ * \return a SimpleMatrix C
+ */
+const SimpleMatrix operator+(const SiconosMatrix& A, const SiconosMatrix& B);
+
+/** Addition of two matrices, C = A+B
+ * \param A a std::shared_ptr<SiconosMatrix>
+ * \param B a std::shared_ptr<SiconosMatrix>
+ * \return a std::shared_ptr<SimpleMatrix>
+ */
+std::shared_ptr<SimpleMatrix> operator+(const std::shared_ptr<SimpleMatrix> A,
+                                        const std::shared_ptr<SimpleMatrix> B);
+
+// SimpleMatrix operator+(const SimpleMatrix& A, const SimpleMatrix& B);
+
+/** Addition of two matrices C = A+B
+ *  \param A a SiconosMatrix
+ *  \param B a SiconosMatrix
+ *  \param[in,out] C a SiconosMatrix
+ */
+void add(const SiconosMatrix& A, const SiconosMatrix& B, SiconosMatrix& C);
+
+/** Subtraction of two matrices, C = A-B
+ * \param A a SiconosMatrix
+ * \param B a SiconosMatrix
+ * \return a SimpleMatrix
+ */
+const SimpleMatrix operator-(const SiconosMatrix& A, const SiconosMatrix& B);
+//  const SimpleMatrix operator -(const SimpleMatrix&,const SimpleMatrix&);
+
+/** Subtraction of two matrices C = A-B
+ *  \param A a SiconosMatrix
+ *  \param B a SiconosMatrix
+ *  \param[in,out] C a SiconosMatrix
+ */
+void sub(const SiconosMatrix& A, const SiconosMatrix& B, SiconosMatrix& C);
+
+/**: A==B when (A-B).normInf()<tolerance
+   \param A a SiconosMatrix
+   \param B a SiconosMatrix
+   \return a boolean
+ */
+bool operator==(const SiconosMatrix& A, const SiconosMatrix& B);
+
+/**: A!=B when (A-B).normInf()>tolerance
+ * \param A a SiconosMatrix
+ * \param B a SiconosMatrix
+ * \return a boolean
+ */
+bool operator!=(const SiconosMatrix& A, const SiconosMatrix& B);
+
+/** multiplication of a matrix by a scalar, B = a*A (init = true) or B += a*A (init = false)
+ *  \param a a double
+ *  \param A a SiconosMatrix
+ *  \param[in,out] B a SiconosMatrix
+ *  \param init a bool
+ */
+void scal(double a, const SiconosMatrix& A, SiconosMatrix& B, bool = true);
+
+/** prod(A, B, C) computes C = A*B in an optimal way, or C += AB if init = false.
+  \param A a SiconosMatrix
+  \param B a SiconosMatrix
+  \param[in,out] C a SiconosMatrix
+  \param init a bool (default = true)
+  */
+void prod(const SiconosMatrix& A, const SiconosMatrix& B, SiconosMatrix& C, bool init = true);
+
+/** axpy_prod(A, B, C) computes C = A*B in an optimal way (if init = true, else +=).
+  \param A a SiconosMatrix
+  \param B a SiconosMatrix
+  \param[in,out] C a SiconosMatrix
+  \param init a bool (default = true)
+  */
+void axpy_prod(const SiconosMatrix&, const SiconosMatrix&, SiconosMatrix&, bool);
+
+/** product of two matrices, C = A*B
+  \param A a SiconosMatrix
+  \param B a SiconosMatrix
+  \return C a SimpleMatrix
+  */
+const SimpleMatrix prod(const SiconosMatrix& A, const SiconosMatrix& B);
+
+/** send data of the matrix to an ostream
+ * \param os An output stream
+ * \param bm a BlockMatrix
+ * \return The same output stream
+ */
+std::ostream& operator<<(std::ostream& os, const SiconosMatrix& sm);
+std::ostream& operator<<(std::ostream& os, const SimpleMatrix& sm);
+std::ostream& operator<<(std::ostream& os, const BlockMatrix& sm);
+
+/** Compute the matrix exponential Exp = exp(A) for general matrices,
+  using scaling and Padé approximation. See expm.hpp.
+  \param A : input matrix
+  \param Exp : result = exp(A)
+  \param computeAndAdd : if true, result = result + exp(A)
+**/
+void expm(SiconosMatrix& A, SiconosMatrix& Exp, bool computeAndAdd = false);
+
+/** Copy a subBlock of MIn into a sub-block of MOut - Dim and positions of the sub-block are
+ *  given in dim and start.
+ *  \param MIn a SPC::SiconosMatrix \param[in,out] MOut a std::shared_ptr<SiconosMatrix>
+ *  \param dim an Index, dim[0], dim[1]: number of rows and columns of the sub-block
+ *  \param start an Index, start[0], start[1]: position (row, column) of the first
+ *  element of the sub-block in MIn start[2], start[3]: position (row, column) of the first
+ *  element of the sub-block in MOut.
+ */
+void setBlock(const SiconosMatrix& MIn, std::shared_ptr<SiconosMatrix> MOut,
+              const std::vector<std::size_t>& dim, const std::vector<std::size_t>& start);
+
+/** test if two matrices have the same number of blocks with
+    blocks of the same dimension when at the same position
+    \param v1 first matrix to compare with
+    \param v2 second matrix to compare with
+*/
+bool isComparableTo(const SiconosMatrix& m1, const SiconosMatrix& m2);
+
+}  // namespace siconos::algebra
+
+#endif
---
+++ kernel/src/utils/SiconosAlgebra/SiconosMatrixVectorOp.hpp
@@ -0,0 +1,143 @@
+/* Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ *
+ * Copyright 2022 INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*! \file SiconosMatrixVectorOp.hpp
+  \brief Toolbox for operators acting on matrices and vectors
+*/
+
+#ifndef SICOMATVEC_OPH
+#define SICOMATVEC_OPH
+
+#include <boost/numeric/ublas/fwd.hpp>
+#include <complex>
+
+namespace siconos::algebra {
+
+class BlockVector;
+class SiconosVector;
+class SiconosMatrix;
+class SimpleMatrix;
+
+/** compute the product m1 * trans(m2)
+ *  \param 2 SiconosVectors
+ *  \return a SimpleMatrix
+ */
+SimpleMatrix outer_prod(const SiconosVector&, const SiconosVector&);
+
+/** prod(A, x, y, init) computes y = A*x or y += A*x if init = false
+  \param A a SiconosMatrix
+  \param x a SiconosVector
+  \param[in,out] y a SiconosVector
+  \param init a bool (default = true)
+  */
+void prod(const SiconosMatrix& A, const SiconosVector& x, SiconosVector& y, bool init = true);
+
+void prod(const SiconosMatrix& A, const BlockVector& x, SiconosVector& y, bool init = true);
+
+// NO NEED TO BE FRIEND
+/** prod(A, x, y, init) computes y = A*x or y += A*x if init = false
+  \param A a SiconosMatrix
+  \param x a SiconosVector
+  \param[in,out] y a SiconosVector
+  \param init a bool (default = true)
+  */
+void prod(const SiconosMatrix& A, const SiconosVector& x, BlockVector& y, bool init = true);
+
+/** prod(x, A, y) computes y = trans(A)*x (init = true) or y += trans(A)*x (init = false)
+  \param x a SiconosVector
+  \param A a SiconosMatrix
+  \param[in,out] y a SiconosVector
+  \param init a bool (default = true)
+  */
+void prod(const SiconosVector& x, const SiconosMatrix& A, SiconosVector& y, bool init = true);
+
+void prod(const SiconosVector& x, const SiconosMatrix& A, BlockVector& y, bool init = true);
+
+/** prod(A, x) returns the product Ax
+  \param A a SiconosMatrix
+  \param x a SiconosVector
+  \return a SiconosVector
+  */
+SiconosVector prod(const SiconosMatrix& A, const SiconosVector& x);
+
+/** prod(a, A, x, y, init) computes y = a*A*x or y += a*A*x if init = false
+  \param a a double
+  \param A a SiconosMatrix
+  \param x a SiconosVector
+  \param[in,out] y a SiconosVector
+  \param init a bool (default = true)
+  */
+void prod(double a, const SiconosMatrix& A, const SiconosVector& x, SiconosVector& y,
+          bool init = true);
+
+/** subprod(A, x, y) computes sub_y = sub_A*sub_x or sub_y += sub_A*sub_x if init = false
+  \param A a SiconosMatrix
+  \param x a SiconosVector
+  \param[in,out] y a SiconosVector
+  \param coord an std::vector<std::size_t> = [r0A r1A c0A c1A r0x r1x r0y r1y];
+  subA is the sub-matrix of A, for row numbers between r0A and r1A-1 and columns between c0A
+  and c1A-1; The same for x and y with rix and riy. \param init a bool (default = true)
+  */
+void subprod(const SiconosMatrix& A, const SiconosVector& x, SiconosVector& y,
+             const std::vector<std::size_t>& coord, bool init = true);
+
+void subprod(const SiconosMatrix& A, const BlockVector& x, SiconosVector& y,
+             const std::vector<std::size_t>& coord, bool init = true);
+
+/** computes y += sub(transpose(A)) x (only = if init = true) where
+subA is a sub-matrix of A.
+  \param A a SiconosMatrix
+  \param x a SiconosVector
+  \param[in,out] y a SiconosVector
+  \param startRow row index of the first element considered in tA (to get sub(tA))
+  \param startCol col index of the first element considered in tA (to get sub(tA))
+  \param init if true, start with y = 0, else add subA.x to current y.
+*/
+void taxpy(const SiconosVector& x, const SiconosMatrix& A, unsigned int startRow,
+           unsigned int startCol, std::shared_ptr<SiconosVector> y, bool init = true);
+
+/** Compute eigenvalues and eigenvectors of a real symmetric matrix A
+ *  See examples of use in test/EigenProblemsTest.cpp.
+ *  \param[in,out] eigenval : eigenvalues of the matrix
+ *  \param[in,out] eigenvec : input matrix A, replace with eigenvectors (columns) in output.
+ *  \param[in] withVect : true if eigenvectors are to be computed (default = true).
+ *  \return int : return value from lapack routine. 0 if successful.
+ */
+int syev(SiconosVector& eigenval, SiconosMatrix& eigenvec, bool withVect = true);
+
+/** Compute eigenvalues and eigenvectors of a nonsymmetrix complex matrix
+ *  See examples of use in test/EigenProblemsTest.cpp.
+ *  \param[in,out] input_mat SiconosMatrix : input matrix.
+ *  \param[in,out] eigenval complex_vector : eigenvalues of the matrix
+ *  \param[in,out] left_eigenvec complex_matrix : matrix of the left eigenvectors
+ *  \param[in, out] right_eigenvec  complex_matrix : matrix of the right eigenvectors
+ *  \param[in] withLeft : true if left eigenvectors are to be computed (default = false).
+ *  \param[in] withRight : true if right  eigenvectors are to be computed (default = true).
+ *  \return int : return value from lapack routine. 0 if succesful.
+ */
+int geev(SiconosMatrix& input_mat,
+         boost::numeric::ublas::vector<std::complex<double>>& eigenval,
+         boost::numeric::ublas::matrix<std::complex<double>,
+                                       boost::numeric::ublas::column_major>& left_eigenvec,
+         boost::numeric::ublas::matrix<std::complex<double>,
+                                       boost::numeric::ublas::column_major>& right_eigenvec,
+         bool withLeft = false, bool withRight = true);
+
+}  // namespace siconos::algebra
+
+#endif
