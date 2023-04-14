596336a21827743774b0b73c59614102e0793c31
Minh Nguyen <hoang-minh.nguyen@inria.fr>
2023-04-14 17:02:11 +0200

    [numerics] create new semismooth test (related to general model with 4 vars: v,u,r,s)

--- numerics/numerics_tests.cmake
+++ numerics/numerics_tests.cmake
@@ -233,6 +233,9 @@ if(WITH_TESTING)
     DRIVER gfc3d_test_collection.c.in FORMULATION gfc3d COLLECTION TEST_IPM_COLLECTION_1
     EXTRA_SOURCES data_collection_gfc3d_1.c test_ipm_gfc3d_1.c )
   new_tests_collection(
+    DRIVER gfc3d_test_collection.c.in FORMULATION gfc3d_nonsmooth COLLECTION TEST_IPM_COLLECTION_1
+    EXTRA_SOURCES data_collection_gfc3d_1.c test_ipm_gfc3d_1.c )
+  new_tests_collection(
     DRIVER gfc3d_test_collection.c.in FORMULATION gfc3d COLLECTION TEST_ADMM_COLLECTION_1
     EXTRA_SOURCES data_collection_gfc3d_1.c test_admm_gfc3d_1.c )
 
@@ -306,6 +309,11 @@ if(WITH_TESTING)
       )
     new_tests_collection(
       DRIVER gfc3d_test_collection.c.in  FORMULATION gfc3d_nonsmooth COLLECTION TEST_IPM_COLLECTION_FCLIB
+      EXTRA_SOURCES data_collection_gfc3d_fclib.c test_ipm_gfc3d_1.c DEPS FCLIB::fclib
+      HDF5 ON
+      )
+    new_tests_collection(
+      DRIVER gfc3d_test_collection.c.in  FORMULATION gfc3d_semismooth COLLECTION TEST_IPM_COLLECTION_FCLIB
       EXTRA_SOURCES data_collection_gfc3d_fclib.c test_ipm_gfc3d_1.c DEPS FCLIB::fclib
       HDF5 ON
       )
