1a8f9f8c6f491f3b6cc87ed9dcd25f7f9062fa33
Samuel Heidmann <samuel.heidmann@inria.fr>
2023-06-15 09:58:09 +0200

    add eigeninclude

---
+++ kernel/src/utils/SiconosAlgebra/EigenInclude.hpp
@@ -0,0 +1,10 @@
+#ifndef EIGENINCLUDE_HPP
+#define EIGENINCLUDE_HPP
+
+
+#define EIGEN_MATRIXBASE_PLUGIN "/home/sam/dev/nonsmooth/siconos/kernel/src/utils/SiconosAlgebra/MatrixBaseAddons.hpp"
+
+#define EIGEN_PLAINOBJECTBASE_PLUGIN "/home/sam/dev/nonsmooth/siconos/kernel/src/utils/SiconosAlgebra/PlainObjectAddons.hpp"
+
+
+#endif
