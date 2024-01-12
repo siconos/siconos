7216731026dc0201b44e83424a7b07ab6dd7ffbb
Franck Pérignon <franck.perignon@univ-grenoble-alpes.fr>
2024-01-12 16:50:59 +0100

    merge master

--- scripts/devel/update_license_header.py
+++ scripts/devel/update_license_header.py
@@ -34,12 +34,8 @@ for f in flist:
         # except:
         # print(f)
 
+instring = 'Copyright 2023 INRIA'
 
-<<<<<<< HEAD
-instring = 'Copyright 2024 INRIA'
-=======
-instring = 'Copyright 2020 INRIA'
->>>>>>> master
 # instring ='Copyright (C) 2005, 2018 by INRIA'
 # instring ='Siconos-Numerics, Copyright INRIA 2005-2015'
 # instring = 'Siconos, Copyright INRIA 2005-2016'
