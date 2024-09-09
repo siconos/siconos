6e655bf6f323f8f64f57f2fe293003affba906d9
Sam <samuel.heidmann@gmail.com>
2024-09-09 11:03:38 +0200

    [skip CI] Add readMatrixFromFile bindings for tests

---
+++ kernel/python/io/io.cpp
@@ -0,0 +1,36 @@
+/* Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ *
+ * Copyright 2024 INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+
+#include "SiconosMatrix.hpp"
+
+
+namespace py = pybind11;
+
+
+PYBIND11_MODULE(io, m)
+{
+    m.doc() = "Siconos nonsmooth io module";
+
+    m.def("readMatrixFromFile", &siconos::algebra::readMatrixFromFile, py::arg("filename"), py::arg("ascii") = true,
+          "Lire une matrice à partir d'un fichier et retourner une SiconosMatrix.");
+}
