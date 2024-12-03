c7fd9769749340591c02b82a5b09e0d6bc85179e
Sam <samuel.heidmann@gmail.com>
2024-12-03 16:03:24 +0100

    Fix declaration signature

--- kernel/src/simulationTools/EulerMoreauOSI.hpp
+++ kernel/src/simulationTools/EulerMoreauOSI.hpp
@@ -312,7 +312,7 @@ class EulerMoreauOSI : public OneStepIntegrator {
                 const siconos::graphs::DynamicalSystemsGraph::VDescriptor &dsv,
                 siconos::algebra::SiconosMatrix &W);
 
-  void computeKhat(siconos::modeling::Interaction &inter, siconos::algebra::MapType &m,
+  void computeKhat(siconos::modeling::Interaction &inter, siconos::algebra::SiconosMatrix &m,
                    std::vector<std::shared_ptr<siconos::algebra::SiconosMatrix>> &workM,
                    double h) const;
 
