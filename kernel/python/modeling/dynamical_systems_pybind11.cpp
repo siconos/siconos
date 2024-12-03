d31314397457e582a7f0bb0eba60459780fa06ed
Sam <samuel.heidmann@gmail.com>
2024-12-03 16:04:10 +0100

    Fix constructors bindings

--- kernel/python/modeling/dynamical_systems_pybind11.cpp
+++ kernel/python/modeling/dynamical_systems_pybind11.cpp
@@ -49,9 +49,12 @@ void wrap_dynamical_systems(py::module_ &m) {
 
   py::class_<siconos::modeling::FirstOrderLinearDS, siconos::modeling::FirstOrderNonLinearDS,
              std::shared_ptr<siconos::modeling::FirstOrderLinearDS>>(m, "FirstOrderLinearDS")
+      .def(py::init<Eigen::Ref<siconos::algebra::SiconosVector> &>(),
+           py::keep_alive<1, 2>(), py::arg("x0"))
       .def(py::init<Eigen::Ref<siconos::algebra::SiconosVector> &,
-                    Eigen::Ref<siconos::algebra::SiconosMatrix> &>(),
-           py::keep_alive<1, 2>(), py::keep_alive<1, 3>(), py::arg("x0"), py::arg("A"));
+                    Eigen::Ref<siconos::algebra::SiconosMatrix> &,
+                    Eigen::Ref<siconos::algebra::SiconosVector> &>(),
+           py::keep_alive<1, 2>(), py::keep_alive<1, 3>(), py::keep_alive<1, 4>(), py::arg("x0"), py::arg("A"), py::arg("b"));
 
   // ============================== SECOND ORDER DS ==============================
 
