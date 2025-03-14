a65bae1b1f5eb80f47aee209e4c595f641c26cb7
olivier-goury <olivier.goury@inria.fr>
2025-03-14 17:39:36 +0100

    Add test for SolidLinearTIDS

---
+++ mechanics/src/fem/native/test/SolidLinearTIDSTest.cpp
@@ -0,0 +1,59 @@
+/* Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ *
+ * Copyright 2024 INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "SolidLinearTIDSTest.hpp"
+
+#include "BlockMatrix.hpp"
+#include "SiconosMatrixOp.hpp"
+#include "SiconosMatrixVectorOp.hpp"
+#include "SiconosVector.hpp"
+#include "SimpleMatrix.hpp"
+#include "MeshUtils.hpp"
+#include "FENode.hpp"
+#include "FiniteElementModel.hpp"
+#include "FiniteElement.hpp"
+#include "Material.hpp"
+
+
+#define CPPUNIT_ASSERT_NOT_EQUAL(message, alpha, omega) \
+  if ((alpha) == (omega)) CPPUNIT_FAIL(message);
+
+// test suite registration
+CPPUNIT_TEST_SUITE_REGISTRATION(SolidLinearTIDSTest);
+
+void SolidLinearTIDSTest::setUp() {
+
+  mesh = siconos::mechanics::fem::createMeshFromGMSH2("square_6.msh");
+  int bulk_material_tag = 1;
+  double density = 7800.;
+  auto mat1 = std::make_shared<siconos::mechanics::fem::Material>(density, 210e9, 1 / 3.);
+  materials = {{bulk_material_tag, mat1}};
+
+}
+
+void SolidLinearTIDSTest::tearDown() {}
+
+// Mass, K, C
+void SolidLinearTIDSTest::testBuildSolidLinearTIDS1() {
+  std::cout << "--> Test: constructor 1." << std::endl;
+  auto FEsolid = std::make_shared<siconos::mechanics::fem::SolidLinearTIDS>(
+      mesh, materials, siconos::algebra::UblasType::SPARSE);
+
+  CPPUNIT_ASSERT_EQUAL_MESSAGE("testBuildSolidLinearTIDS1 : ", FEsolid->FEModel()->elements().size() == 6,
+                               true);
+  std::cout << "--> Constructor 1 test ended with success." << std::endl;
+}
---
+++ mechanics/src/fem/native/test/SolidLinearTIDSTest.hpp
@@ -0,0 +1,60 @@
+/* Siconos is a program dedicated to modeling, simulation and control
+ * of non smooth dynamical systems.
+ *
+ * Copyright 2024 INRIA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+#ifndef __SolidLinearTIDSTest__
+#define __SolidLinearTIDSTest__
+
+#include <cppunit/TestCase.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include "SolidLinearTIDS.hpp"
+#include "SiconosException.hpp"
+
+class SolidLinearTIDSTest : public CppUnit::TestFixture
+{
+
+private:
+  
+  ACCEPT_SERIALIZATION(SolidLinearTIDSTest);
+
+
+  // Name of the tests suite
+  CPPUNIT_TEST_SUITE(SolidLinearTIDSTest);
+
+  // tests to be done ...
+
+  CPPUNIT_TEST(testBuildSolidLinearTIDS1);
+  CPPUNIT_TEST_SUITE_END();
+
+  // \todo exception test
+
+  void testBuildSolidLinearTIDS1();
+
+  // Members
+
+  std::shared_ptr<siconos::mechanics::fem::Mesh> mesh;
+  std::map<unsigned int, std::shared_ptr<siconos::mechanics::fem::Material>> materials;
+public:
+  void setUp();
+  void tearDown();
+
+};
+
+#endif
+
+
+
+
