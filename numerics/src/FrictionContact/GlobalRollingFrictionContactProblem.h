b587581b9856d73bb9660120eb4b973638862318
Vincent ACARY <vincent.acary@inria.fr>
2026-03-04 14:54:46 +0100

    [numerics] remove pragma message

--- numerics/src/FrictionContact/GlobalRollingFrictionContactProblem.h
+++ numerics/src/FrictionContact/GlobalRollingFrictionContactProblem.h
@@ -2,8 +2,6 @@
 #ifndef GLOBAL_ROLLING_FRICTION_CONTACT_PROBLEM_BACKWARD_COMPAT_H
 #define GLOBAL_ROLLING_FRICTION_CONTACT_PROBLEM_BACKWARD_COMPAT_H
 
-#pragma message("Warning: GlobalRollingFrictionContactProblem.h has moved to RollingFrictionContact/GlobalRollingFrictionContactProblem.h")
-
 #include "RollingFrictionContact/GlobalRollingFrictionContactProblem.h"
 
 #endif
--- numerics/src/FrictionContact/RollingFrictionContactProblem.h
+++ numerics/src/FrictionContact/RollingFrictionContactProblem.h
@@ -2,8 +2,6 @@
 #ifndef ROLLING_FRICTION_CONTACT_PROBLEM_BACKWARD_COMPAT_H
 #define ROLLING_FRICTION_CONTACT_PROBLEM_BACKWARD_COMPAT_H
 
-#pragma message("Warning: RollingFrictionContactProblem.h has moved to RollingFrictionContact/RollingFrictionContactProblem.h")
-
 #include "RollingFrictionContact/RollingFrictionContactProblem.h"
 
 #endif
