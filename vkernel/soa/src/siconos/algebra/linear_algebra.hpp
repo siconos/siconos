48de2c1599ab9e6098d7093ad729e2b9b3261490
Maurice Bremond <Maurice.Bremond@inria.fr>
2026-06-11 12:05:51 +0200

    [vkernel] linear_algebra: remove range v3

--- vkernel/soa/src/siconos/algebra/linear_algebra.hpp
+++ vkernel/soa/src/siconos/algebra/linear_algebra.hpp
@@ -3,12 +3,7 @@
 #include "siconos/storage/pattern/pattern.hpp"
 #include "siconos/storage/traits/traits.hpp"
 
-#include <range/v3/view/join.hpp>
-#include <range/v3/view/stride.hpp>
-#include <range/v3/view/zip.hpp>
 #include <type_traits>
 
-
 namespace siconos {
-
 }
